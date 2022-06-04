4d0580b1fc46c806ce8cc481826875be6b224d71
jung su young <j9972@naver.com>
2022-06-04 14:44:52 +0900

    음료수

--- DFS:BFS-practice/1.음료수얼려먹기.py
+++ DFS:BFS-practice/1.음료수얼려먹기.py
@@ -6,14 +6,14 @@ for i in range(n):
 
 
 def dfs(x, y):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
+    if x <= - 1 or x >= n or y <= -1 or y >= m:
         return False
     if graph[x][y] == 0:
         graph[x][y] = 1
-        dfs(x-1, y)
         dfs(x+1, y)
-        dfs(x, y-1)
+        dfs(x-1, y)
         dfs(x, y+1)
+        dfs(x, y-1)
         return True
     return False
 
@@ -21,6 +21,6 @@ def dfs(x, y):
 res = 0
 for i in range(n):
     for j in range(m):
-        if dfs(i, j) == True:
+        if dfs(i, j) == 1:
             res += 1
 print(res)
