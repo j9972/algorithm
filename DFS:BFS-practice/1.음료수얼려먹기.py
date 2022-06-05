f7fc2af5b78dd6b07820c1217212ce37054f79a1
jung su young <j9972@naver.com>
2022-06-05 15:09:31 +0900

    음료 복습

--- DFS:BFS-practice/1.음료수얼려먹기.py
+++ DFS:BFS-practice/1.음료수얼려먹기.py
@@ -1,12 +1,11 @@
 n, m = map(int, input().split())
-
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
 
 def dfs(x, y):
-    if x <= - 1 or x >= n or y <= -1 or y >= m:
+    if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
     if graph[x][y] == 0:
         graph[x][y] = 1
@@ -23,4 +22,5 @@ for i in range(n):
     for j in range(m):
         if dfs(i, j) == 1:
             res += 1
+
 print(res)
