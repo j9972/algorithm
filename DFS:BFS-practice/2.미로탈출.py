30bfa5369c17d95e167db12cd2fdcedff4c3920f
jung su young <j9972@naver.com>
2022-06-06 19:49:30 +0900

    again

--- DFS:BFS-practice/2.미로탈출.py
+++ DFS:BFS-practice/2.미로탈출.py
@@ -1,13 +1,15 @@
+
+
 from collections import deque
 
 n, m = map(int, input().split())
-
 graph = []
+
 for i in range(n):
     graph.append(list(map(int, input())))
 
 dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [0, 0, -1, 1]
 
 
 def bfs(x, y):
@@ -16,7 +18,6 @@ def bfs(x, y):
 
     while queue:
         x, y = queue.popleft()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
