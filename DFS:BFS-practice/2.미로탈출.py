19e95225394fa0a4ed3997077d92b97ada2ba870
jung su young <j9972@naver.com>
2022-06-25 16:22:04 +0900

    solve and comment

--- DFS:BFS-practice/2.미로탈출.py
+++ DFS:BFS-practice/2.미로탈출.py
@@ -2,9 +2,9 @@ from collections import deque
 
 n, m = map(int, input().split())
 
-g = []
+graph = []
 for i in range(n):
-    g.append(list(map(int, input())))
+    graph.append(list(map(int, input())))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -23,12 +23,13 @@ def bfs(x, y):
 
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if g[nx][ny] == 0:
+            if graph[nx][ny] == 0:
                 continue
-            if g[nx][ny] == 1:
-                g[nx][ny] = g[x][y] + 1
+            if graph[nx][ny] == 1:
+                # 우리가 구하고자하는게 거리의 수를 알아야 하니까 필요하다
+                graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
-    return g[n-1][m-1]
+    return graph[n-1][m-1]
 
 
 print(bfs(0, 0))
