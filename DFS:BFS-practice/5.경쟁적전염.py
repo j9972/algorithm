8660cb2362c2b0add8d9503b92f550e89074f8ff
jung su young <j9972@naver.com>
2022-06-25 17:50:53 +0900

    solve and comment

--- DFS:BFS-practice/5.경쟁적전염.py
+++ DFS:BFS-practice/5.경쟁적전염.py
@@ -2,32 +2,32 @@ from collections import deque
 
 n, k = map(int, input().split())
 
-g = []
-d = []
-
+data = []
+graph = []
 for i in range(n):
-    g.append(list(map(int, input().split())))
+    graph.append(list(map(int, input().split())))
     for j in range(n):
-        if g[i][j] != 0:
-            d.append((g[i][j], 0, i, j))
+        if graph[i][j] != 0:
+            data.append((graph[i][j], 0, i, j))
 
 tarS, tarX, tarY = map(int, input().split())
 
+data.sort()
+queue = deque(data)
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
-d.sort()
-queue = deque(d)
 
 while queue:
     virus, s, x, y = queue.popleft()
     if s == tarS:
         break
+
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if nx >= 0 and nx < n and ny >= 0 and ny < n:
-            if g[nx][ny] == 0:
-                g[nx][ny] = virus
+        if 0 <= nx < n and 0 <= ny < n:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = virus
                 queue.append((virus, s+1, nx, ny))
-print(g[tarX-1][tarY-1])
+print(graph[tarX-1][tarY-1])
