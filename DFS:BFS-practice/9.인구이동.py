bef393d95714e56526d30564fe1b225031adce4c
jung su young <j9972@naver.com>
2022-06-26 21:28:36 +0900

    solve and comment

--- DFS:BFS-practice/9.인구이동.py
+++ DFS:BFS-practice/9.인구이동.py
@@ -9,6 +9,7 @@ for i in range(n):
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+# point
 res = 0
 
 
@@ -16,12 +17,13 @@ def process(x, y, index):
     united = []
     united.append((x, y))
 
+    # point
     queue = deque()
     queue.append((x, y))
 
-    union[x][y] = index  # 연합의 번호 할당
-    summary = board[x][y]  # 연합의 인구수
-    count = 1  # 연합 국가 수
+    union[x][y] = index
+    summary = board[x][y]
+    count = 1
 
     while queue:
         x, y = queue.popleft()
@@ -32,16 +34,19 @@ def process(x, y, index):
             if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
                 if l <= abs(board[nx][ny] - board[x][y]) <= r:
                     queue.append((nx, ny))
+                    # point
                     union[nx][ny] = index
-                    count += 1
                     summary += board[nx][ny]
+                    count += 1
                     united.append((nx, ny))
+
+    # point
     for i, j in united:
         board[i][j] = summary // count
     return count
 
 
-totalCount = 0
+total = 0
 
 while True:
     union = [[-1] * n for _ in range(n)]
@@ -52,8 +57,9 @@ while True:
                 process(i, j, index)
                 index += 1
 
-    if index == n * n:
+    if index == n*n:
         break
-    totalCount += 1
+    total += 1
 
-print(totalCount)
+
+print(total)
