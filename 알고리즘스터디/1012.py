0163df11a2c0f7e7f9d5e13585e3ff9db77af4c2
jung su young <j9972@naver.com>
2022-07-11 15:26:52 +0900

    2문제..

--- 알고리즘스터디/1012.py
+++ 알고리즘스터디/1012.py
@@ -1,8 +1,51 @@
-# dfs 문제
+# # dfs 문제
+# import sys
+# sys.setrecursionlimit(10**9)
+# input = sys.stdin.readline
+# # 런타임 에러때문에 써줘야함
+
+# t = int(input())
+
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+
+# def dfs(x, y):
+#     for i in range(4):
+#         nx = x + dx[i]
+#         ny = y + dy[i]
+
+#         if 0 <= nx < n and 0 <= ny < m:
+#             if graph[nx][ny] == 1:
+#                 # 여기서 graph[nx][ny]이 값을 0이하로 바꿔줘야함
+#                 # 맨 아래 for 문에서 graph[i][j] 가 0이 아닐때 count 를 증가하므로
+#                 graph[nx][ny] = 0
+#                 dfs(nx, ny)
+
+
+# for i in range(t):
+#     m, n, k = map(int, input().split())
+#     count = 0
+#     리프리헨션 쓸때 가로 세로 체크 제대로 하기 -> 가로 세로 순서
+#     graph = [[0]*m for _ in range(n)]
+
+#     for _ in range(k):
+#         x, y = map(int, input().split())
+#         x,y는 위치 값 ( 2차원 배열속의 위치 값인데, x는 세로 y는 가로이기 때문에 y x 순서)
+#         graph[y][x] = 1
+
+#     for i in range(n):
+#         for j in range(m):
+#             if graph[i][j] != 0:
+#                 count += 1
+#                 # 함수를 재귀적으로 호출, 다른 방법으로는 이코테에 음료수 얼려먹기 방법도 있다
+#                 dfs(i, j)
+
+#     # print(count) 이거 선언하는 위치 조심하기
+#     print(count)
 import sys
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit
 input = sys.stdin.readline
-# 런타임 에러때문에 써줘야함
 
 t = int(input())
 
@@ -16,29 +59,30 @@ def dfs(x, y):
         ny = y + dy[i]
 
         if 0 <= nx < n and 0 <= ny < m:
-            if graph[nx][ny] == 1:
-                # 여기서 graph[nx][ny]이 값을 0이하로 바꿔줘야함
-                # 맨 아래 for 문에서 graph[i][j] 가 0이 아닐때 count 를 증가하므로
-                graph[nx][ny] = 0
+            if board[nx][ny] == 1:
+                board[nx][ny] = 0
                 dfs(nx, ny)
 
 
 for i in range(t):
     m, n, k = map(int, input().split())
-    count = 0
-    graph = [[0]*m for _ in range(n)]
+    # 컴프리 헨션은 가로 세로 순서로 해야함을 잊지말자
+    board = [[0] * m for _ in range(n)]
 
-    for _ in range(k):
+    # 지렁이의 갯수
+    count = 0
+
+    for i in range(k):
+        # x,y는 배추의 위치를 의미한다. 근데 범위가 x는 m-1 -> m이 가로를 의미!!
         x, y = map(int, input().split())
-        # x의 범위가 0 부터 m-1까지 이므로 2번째에 오는게 맞다 ( 같은 원리로 y는 첫번째 )
-        graph[y][x] = 1
+        # 여기서 x는 가로를 의미, y는 세로를 의미 ( 가로의미는 2번째 인덱스를 의미 )
+        board[y][x] = 1
 
     for i in range(n):
         for j in range(m):
-            if graph[i][j] != 0:
+            # dfs 함수가 0일때는 배추가 없어서 재귀함수를 일으키지 못하므로
+            if board[i][j] != 0:
                 count += 1
-                # 함수를 재귀적으로 호출, 다른 방법으로는 이코테에 음료수 얼려먹기 방법도 있다
                 dfs(i, j)
 
-    # print(count) 이거 선언하는 위치 조심하기
     print(count)
--- 알고리즘스터디/1260.py
+++ 알고리즘스터디/1260.py
@@ -1,53 +1,104 @@
-# DFS BFS 문제
+# # DFS BFS 문제
+# from collections import deque
+
+# # dfs
+# n, m, v = map(int, input().split())
+
+# # 예제 입력을 받기 위해서 2차원 배열 형태로 밭고 n+1인 이유는 0부터 시작하니까 0인 부분을 나두고 1부터 n까지 받으려고하는것
+# graph = [[] * (n+1) for _ in range(n+1)]
+
+# # 배열의 입력을 잘 2개씩 받기 위함
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+#     graph[b].append(a)
+
+# # 기본적으로 false로 초기화를 시키고 false를 true로 바꾸면서 문제를 실행함
+# visitied = [False] * (n+1)
+# # visited를 공유하면 뒤의 함수가 실행이 잘 되지 않는다.
+# visitiedS = [False] * (n+1)
+
+
+# def dfs(now):
+#     visitied[now] = True
+#     print(now, end=' ')
+
+#     for i in graph[now]:
+#         if not visitied[i]:
+#             dfs(i)
+
+
+# def bfs(now):
+#     queue = deque([now])
+#     visitiedS[now] = True
+
+#     while queue:
+#         v = queue.popleft()
+#         print(v, end=' ')
+
+#         for i in graph[v]:
+#             if not visitiedS[i]:
+#                 queue.append(i)
+#                 visitiedS[i] = True
+
+
+# # graph를 정렬시켜줘야 bfs문제가 통과된다. ( 이유는 우선순위가 낮은순서로 되기때문이다)
+# # range 범위가 1부터 n+1인 이유는 0번째 인덱스는 시작 간선을 체크하기때문
+# for i in range(1, n+1):
+#     graph[i].sort()
+
+# # print(dfs(v)) 이런식으로 하면 마지막에 None이 발생하는데 이를 조심
+# dfs(v)
+# print()
+# bfs(v)
+
 from collections import deque
 
-# dfs
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+
 n, m, v = map(int, input().split())
 
-# 예제 입력을 받기 위해서 2차원 배열 형태로 밭고 n+1인 이유는 0부터 시작하니까 0인 부분을 나두고 1부터 n까지 받으려고하는것
-graph = [[] * (n+1) for _ in range(n+1)]
+# 정점의 개수보다는 많은 수가 필요 하다
+visited = [False] * (n+1)
+visitedA = [False] * (n+1)
 
-# 배열의 입력을 잘 2개씩 받기 위함
-for _ in range(m):
+# 비워져 있는 것을 만들기 n or n+1
+board = [[] * (n+1) for _ in range(n+1)]
+for i in range(m):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-# 기본적으로 false로 초기화를 시키고 false를 true로 바꾸면서 문제를 실행함
-visitied = [False] * (n+1)
-# visited를 공유하면 뒤의 함수가 실행이 잘 되지 않는다.
-visitiedS = [False] * (n+1)
+    board[a].append(b)
+    board[b].append(a)
 
 
 def dfs(now):
-    visitied[now] = True
+    visited[now] = True
     print(now, end=' ')
 
-    for i in graph[now]:
-        if not visitied[i]:
+    for i in board[now]:
+        if not visited[i]:
             dfs(i)
 
 
 def bfs(now):
     queue = deque([now])
-    visitiedS[now] = True
+    visitedA[now] = True
 
     while queue:
         v = queue.popleft()
         print(v, end=' ')
 
-        for i in graph[v]:
-            if not visitiedS[i]:
+        for i in board[v]:
+            if not visitedA[i]:
                 queue.append(i)
-                visitiedS[i] = True
+                visitedA[i] = True
 
 
-# graph를 정렬시켜줘야 bfs문제가 통과된다. ( 이유는 우선순위가 낮은순서로 되기때문이다)
-# range 범위가 1부터 n+1인 이유는 0번째 인덱스는 시작 간선을 체크하기때문
+# for은 왜? range 범위?
 for i in range(1, n+1):
-    graph[i].sort()
+    board[i].sort()
 
-# print(dfs(v)) 이런식으로 하면 마지막에 None이 발생하는데 이를 조심
 dfs(v)
 print()
 bfs(v)
