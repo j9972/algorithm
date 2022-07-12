e769b938bd466f77a442bacf131db8537cc050ab
jung su young <j9972@naver.com>
2022-07-12 22:08:35 +0900

    2178

--- 알고리즘스터디/2178.py
+++ 알고리즘스터디/2178.py
@@ -1,10 +1,11 @@
+# bfs 문제 - 미로탈출  ( 최소 칸 수 )
 from collections import deque
 
 n, m = map(int, input().split())
 
-graph = []
+board = []
 for i in range(n):
-    graph.append(list(map(int, input())))
+    board.append(list(map(int, input())))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -13,19 +14,17 @@ dy = [0, 0, -1, 1]
 def bfs(x, y):
     queue = deque()
     queue.append((x, y))
+
     while queue:
         x, y = queue.popleft()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 1:
-                    graph[nx][ny] = graph[x][y] + 1
-                    queue.append((nx, ny))
-
-    return graph[n-1][m-1]
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1:
+                board[nx][ny] = board[x][y] + 1
+                queue.append((nx, ny))
+    return board[n-1][m-1]
 
 
 print(bfs(0, 0))
