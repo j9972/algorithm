c5ac530258cefaa829991720ba70d8a4babf60e5
jung su young <j9972@naver.com>
2022-07-15 23:48:03 +0900

    기타줄

--- 알고리즘스터디그리디/1049.py
+++ 알고리즘스터디그리디/1049.py
@@ -1,33 +1,34 @@
+# 여기서 진짜 중요한 점은 2가지 이상의 브랜드를 섞어서 기타줄을 살수 있다는 점이다
+# 또 다른 좀은 6개까지 패키지를 샀을때 n 보다 커도 최솟값이면 상관없다 ( 3번째 예제 )
+
 import sys
 sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-brand = []
+pack = []
+single = []
+
+# 브랜드의 개수 만큼 for문을 돌려서 리스트 박스에 넣어준다
 for i in range(m):
-    brand.append(list(map(int, input().split())))
+    p, s = map(int, input().split())
+    pack.append(p)
+    single.append(s)
 
-money = []
+mPack = min(pack)
+mSingle = min(single)
 
-for i in range(len(brand)):
-    res = 0
-    if n // 6 == 0:
-        if brand[i][0] > n * brand[i][1]:
-            res = n * brand[i][1]
-            money.append(res)
-        else:
-            res = brand[i][0]
-            money.append(res)
+res = 0
+while n > 0:
+    if n >= 6:
+        mSingle *= 6
+        n -= 6
     else:
-        if (n//6) * brand[i][0] + (n % 6) * brand[i][1] > n * brand[i][1]:
-            res = n * brand[i][1]
-            money.append(res)
-        else:
-            res += (n // 6) * brand[i][0]
-            res += (n % 6) * brand[i][1]
-            money.append(res)
-
-print("money: {}".format(money))
-
-print(min(money))
+        mSingle *= n
+        n -= n
+    if mSingle < mPack:
+        res += mSingle
+    else:
+        res += mPack
+print(res)
