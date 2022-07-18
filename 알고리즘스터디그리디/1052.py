4aa69d79007c1dc6e9eba33959a4670ac0ba24cd
jung su young <j9972@naver.com>
2022-07-18 22:11:04 +0900

    물병

--- 알고리즘스터디그리디/1052.py
+++ 알고리즘스터디그리디/1052.py
@@ -3,3 +3,12 @@ sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
+
+
+minValue = 0
+ans = False
+
+if ans == False:
+    print(-1)
+else:
+    print(minValue)
