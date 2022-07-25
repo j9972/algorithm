580cebd9a46c80175dfdceb64ff0beaa76a011a2
jung su young <j9972@naver.com>
2022-07-25 23:18:33 +0900

    숫자 카드 게임

--- 이코테/이코테그리드/반복용.py
+++ 이코테/이코테그리드/반복용.py
@@ -1,19 +1,12 @@
-n, m, k = map(int, input().split())
+import sys
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
 
-data = list(map(int, input().split()))
+n, m = map(int, input().split())
 
-data.sort(reverse=True)
-
-f = data[0]
-s = data[1]
-
-# 가장 큰수가 더해지는 횟수 계산 -> k만큼 반복 될수있으니까 곱해야한다
-count = int(m/(k+1)) * k + m%(k+1)
-
-res = 0
-res += count * f
-res += (m-count) * s
-
-print(res)
-
-
+board = []
+v = []
+for i in range(n):
+    board = list(map(int, input().split()))
+    v.append(min(board))
+print(max(v))
--- 이코테/이코테그리드/숫자 카드 게임.py
+++ 이코테/이코테그리드/숫자 카드 게임.py
@@ -12,3 +12,14 @@ for i in range(n):
     # 가장 작은 수중에서 가장 큰 값 찾기
     res = max(res, min_va)
 print(res)
+
+
+# new way -> 게임 전략을 잘 파악 해야한다
+# n, m = map(int, input().split())
+
+# board = []
+# v = []
+# for i in range(n):
+#     board = list(map(int, input().split()))
+#     v.append(min(board))
+# print(max(v))
