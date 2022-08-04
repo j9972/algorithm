8210daf921eb0409ab88fc0b16246e314c36e2dc
jung su young <j9972@naver.com>
2022-08-04 14:17:15 +0900

    특정거리 - board[].append() 할때는 board에 대한 구조를 만들어 놔야한다.

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,31 +1,28 @@
 from collections import deque
 
-n, m = map(int, input().split())
+n, m, k, x = map(int, input().split())
 
-board = []
-for i in range(n):
-    board.append(list(map(int, input())))
+board = [[] for _ in range(n+1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    board[a].append(b)
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+distance = [-1] * (n+1)
+distance[x] = 0
 
+queue = deque([x])
 
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
+while queue:
+    now = queue.popleft()
+    for next in board[now]:
+        if distance[next] == -1:
+            distance[next] = distance[now] + 1
+            queue.append(next)
 
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < m:
-                if board[nx][ny] == 1:
-                    board[nx][ny] = board[x][y] + 1
-                    queue.append((nx, ny))
-    return board[n-1][m-1]
-
-
-print(bfs(0, 0))
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        print(i)
+    check = True
+if check == False:
+    print(-1)
