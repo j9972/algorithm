427452dd8ec5363a186348f5f04ec2e086da63ac
jung su young <j9972@naver.com>
2022-08-04 15:01:46 +0900

    연구소 2차원 배열 데이터를 새로운 2차원 배열에 넣기

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,28 +1,59 @@
-from collections import deque
+n, m = map(int, input().split())
 
-n, m, k, x = map(int, input().split())
+board = [[0]*m for _ in range(n)]
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
 
-board = [[] for _ in range(n+1)]
-for i in range(m):
-    a, b = map(int, input().split())
-    board[a].append(b)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-distance = [-1] * (n+1)
-distance[x] = 0
 
-queue = deque([x])
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-while queue:
-    now = queue.popleft()
-    for next in board[now]:
-        if distance[next] == -1:
-            distance[next] = distance[now] + 1
-            queue.append(next)
+        if 0 <= nx < n and 0 <= ny < m:
+            if board[nx][ny] == 0:
+                board[nx][ny] = 2
+                virus(nx, ny)
 
-check = False
-for i in range(1, n+1):
-    if distance[i] == k:
-        print(i)
-    check = True
-if check == False:
-    print(-1)
+
+def score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 0:
+                score += 1
+    return score
+
+
+res = 0
+
+
+def dfs(count):
+    global res
+
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                board[i][j] = graph[i][j]
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == 2:
+                    virus(i, j)
+        res = max(res, score())
+        return
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                count += 1
+                dfs(count)
+                graph[i][j] = 0
+                count -= 1
+
+
+dfs(0)
+print(res)
