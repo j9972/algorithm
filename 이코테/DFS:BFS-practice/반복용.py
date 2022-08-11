83d4a4365fadaf8741f6525b4b32d232f1cf33df
jung su young <j9972@naver.com>
2022-08-11 11:32:39 +0900

    part1

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,54 +1,33 @@
 from collections import deque
 
+n, m, k, x = map(int, input().split())
 
-def solution(board):
-    n = len(board)
-    newBoard = [[1] * (n+2) for _ in range(n+2)]
-    for i in range(n):
-        for j in range(n):
-            newBoard[i+1][j+1] = board[i][j]
-    queue = deque()
-    visited = []
-    pos = {(0, 0), (0, 1)}
-    queue.append((pos, 0))
-    visited.append(pos)
+board = [[] for _ in range(n+1)]
 
-    while queue:
-        pos, cost = queue.popleft()
-        if (n, n) in pos:
-            return cost
-        for next in get_next(pos, newBoard):
-            if next not in visited:
-                queue.append((next, cost+1))
-                visited.append(next)
-    return 0
+for i in range(m):
+    a, b = map(int, input().split())
+    board[a].append(b)
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def get_next(pos, board):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+queue = deque([x])
 
-    next_pos = []
-    pos = list(pos)
-    pos1_x, pos1_y, pos2_x, pos2_y = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
+distance = [-1] * (n+1)
+distance[x] = 0
 
-    for i in range(4):
-        post1_next_x, post1_next_y = pos1_x+dx[i], pos1_y+dy[i]
-        post2_next_x, post2_next_y = pos2_x+dx[i], pos2_y+dy[i]
+while queue:
+    now = queue.popleft()
 
-        if board[post1_next_x][post1_next_y] == 0 and board[post2_next_x][post2_next_y] == 0:
-            next_pos.append({(post1_next_x, post1_next_y),
-                            (post2_next_x, post2_next_y)})
+    for next in board[now]:
+        if distance[next] == -1:
+            distance[next] = distance[now] + 1
+            queue.append(next)
 
-    if pos1_x == pos2_x:
-        for i in [-1, 1]:
-            if board[pos1_x+i][pos1_y] == 0 and board[pos2_x + i][pos1_y] == 0:
-                next_pos.append({(pos1_x, pos1_y), (pos1_x+i, pos1_y)})
-                next_pos.append({(pos2_x, pos2_y), (pos2_x+i, pos2_y)})
-
-    elif pos1_y == pos2_y:
-        for i in [-1, 1]:
-            if board[pos1_x][pos1_y+i] == 0 and board[pos2_x][pos1_y + i] == 0:
-                next_pos.append({(pos1_x, pos1_y), (pos1_x, pos1_y + i)})
-                next_pos.append({(pos2_x, pos2_y), (pos2_x, pos2_y + i)})
-    return next
+find = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        find = True
+        print(i)
+if find == False:
+    print(-1)
