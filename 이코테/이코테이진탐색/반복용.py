6cba04bb2f14d9ad35fd7e5aba8d991116860655
jung su young <j9972@naver.com>
2022-08-15 15:27:03 +0900

    정렬된 배열에서 특정 수이 개수 구하기 - login :정렬후 x값의 시작점과 끝점을 찾기

--- 이코테/이코테이진탐색/반복용.py
+++ 이코테/이코테이진탐색/반복용.py
@@ -1,25 +1,19 @@
-import sys
-input = sys.stdin.readline
+# 범위의 시작과 끝을 체
 
-n, m = map(int, input().split())
+from bisect import bisect_left, bisect_right
+n, x = map(int, input().split())
 array = list(map(int, input().split()))
+array.sort()
 
-start = 0
-end = max(array)
-res = 0
 
-while start <= end:
-    mid = (start + end) // 2
-    total = 0  # 잘린 떡 조각 길이의 합
+def count_by_range(array, left_value, right_value):
+    left_index = bisect_left(array, left_value)
+    right_index = bisect_right(array, right_value)
+    return right_index - left_index
 
-    for sliceArray in array:
-        if sliceArray > mid:
-            total += sliceArray - mid
 
-    if total < m:
-        end = mid - 1
-    else:
-        res = mid
-        start = mid + 1
-
-print(res)
+count = count_by_range(array, x, x)
+if count == 0:
+    print(-1)
+else:
+    print(count)
