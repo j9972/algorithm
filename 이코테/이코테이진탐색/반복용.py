f8506ece1225256b8cbbf6ef38b441a0154cd76e
jung su young <j9972@naver.com>
2022-08-16 12:22:06 +0900

    정렬된 배열에서 특정 수의 개수 구하기

--- 이코테/이코테이진탐색/반복용.py
+++ 이코테/이코테이진탐색/반복용.py
@@ -1,44 +1,22 @@
+from bisect import bisect_left, bisect_right
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n, x = map(int, input().split())
 array = list(map(int, input().split()))
 
 array.sort()
 
-start = 0
-end = max(array)
-res = 0
 
-while start <= end:
-    mid = (start + end) // 2
-    total = 0
-
-    for x in array:
-        if x > mid:
-            total += x - mid
-    if total > m:
-        start = mid + 1
-    else:
-        res = mid
-        end = mid - 1
-print(res)
-
-# def search_binary(array, target, start, end):
-#     while start <= end:
-#         mid = (start+end) // 2
-#         if array[mid] == target:
-#             return mid
-#         elif array[mid] < target:
-#             start = mid + 1
-#         else:
-#             end = mid - 1
-#     return None
+def count_by_range(array, left_value, right_value):
+    left_index = bisect_left(array, left_value)
+    right_index = bisect_right(array, right_value)
+    return right_index - left_index
 
 
-# for i in m_array:
-#     res = search_binary(array, i, 0, n-1)
-#     if res != None:
-#         print('YES', end=' ')
-#     else:
-#         print('NO', end=' ')
+res = count_by_range(array, x, x)
+
+if res:
+    print(res)
+else:
+    print(-1)
