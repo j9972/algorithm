f9e554286f51db268b9ea73991960a6d5d4a732f
jung su young <j9972@naver.com>
2022-08-16 11:42:01 +0900

    실전문제 2문제

--- 이코테/이코테이진탐색/반복용.py
+++ 이코테/이코테이진탐색/반복용.py
@@ -1,34 +1,44 @@
-from bisect import bisect_left, bisect_right
 import sys
 input = sys.stdin.readline
 
+n, m = map(int, input().split())
+array = list(map(int, input().split()))
 
-def count_by_range(a, left_value, right_value):
-    left_index = bisect_left(a, left_value)
-    right_index = bisect_right(a, right_value)
-    return right_index-left_index
+array.sort()
+
+start = 0
+end = max(array)
+res = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    total = 0
+
+    for x in array:
+        if x > mid:
+            total += x - mid
+    if total > m:
+        start = mid + 1
+    else:
+        res = mid
+        end = mid - 1
+print(res)
+
+# def search_binary(array, target, start, end):
+#     while start <= end:
+#         mid = (start+end) // 2
+#         if array[mid] == target:
+#             return mid
+#         elif array[mid] < target:
+#             start = mid + 1
+#         else:
+#             end = mid - 1
+#     return None
 
 
-def solution(words, queries):
-    answer = []
-
-    array = [[] for _ in range(10001)]
-    reverseArray = [[] for _ in range(10001)]
-
-    for word in words:
-        array[len(word)].append(word)
-        reverseArray[len(word)].append(word[::-1])
-
-    for i in range(10001):
-        array[i].sort()
-        reverseArray[i].sort()
-
-    for q in queries:
-        if q[0] != '?':
-            res = count_by_range(array[len(q)], q.replace(
-                '?', 'a'), q.replace('?', 'z'))
-        else:
-            res = count_by_range(reverseArray[len(q)], q[::-1].replace(
-                '?', 'a'), q[::-1].replace('?', 'z'))
-        answer.append(res)
-    return answer
+# for i in m_array:
+#     res = search_binary(array, i, 0, n-1)
+#     if res != None:
+#         print('YES', end=' ')
+#     else:
+#         print('NO', end=' ')
