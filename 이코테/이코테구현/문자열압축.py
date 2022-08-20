f82a42d34e4cc7d4a82677160c69d5949addd635
jung su young <j9972@naver.com>
2022-08-20 21:48:07 +0900

    문자열 압축

---
+++ 이코테/이코테구현/문자열압축.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = len(s)
+    for step in range(1, len(s)//2+1):
+        sentense = ""
+        count = 1
+        prev = s[0:step]
+
+        for j in range(step, len(s), step):
+            if prev == s[j:j+step]:
+                count += 1
+            else:
+                sentense += str(count) + prev if count >= 2 else prev
+                prev = s[j:j+step]
+                count = 1
+        sentense += str(count) + prev if count >= 2 else prev
+        answer = min(answer, len(sentense))
+
+    return answer
