13dbb521b32f4a6d468062f79b182c0a70d96168
jung su young <j9972@naver.com>
2022-09-16 16:30:00 +0900

    try

--- 이코테/이코테구현/상하좌우.py
+++ 이코테/이코테구현/상하좌우.py
@@ -1,17 +1,10 @@
 n = int(input())
 x, y = 1, 1
-plans = input().split()
+plans = ['L', 'R', 'U', 'D']
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-moves = ['L', 'R', 'U', 'D']
-
-for plan in plans:
-    if 1 <= nx <= n and if 1 <= ny <= n:
-        for i in range(len(moves)):
-            if plan == moves[i]:
-                nx = x + dx[i]
-                ny = y + dy[i]
-        x, y = nx, ny
-print(x, y)
+for i in range(4):
+    for plan in plans:
+        if plan ==
