7ac5e2c924b7da9d03efda38214cdb2a13138934
jung su young <j9972@naver.com>
2022-09-17 19:56:01 +0900

    연산자

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,45 +1,40 @@
-def balance(p):
-    count = 0
-    for i in range(len(p)):
-        if p[i] == '(':
-            count += 1
-        else:
-            count -= 1
-        if count == 0:
-            return i
+n = int(input())
 
-def proper(p):
-    count = 0
-    for i in p:
-        if i == '(':
-            count += 1
-        else:
-            if count == 0:
-                return False
-            else:
-                count -= 1
-        return True
+data = list(map(int, input().split()))
 
-def solution(p):
-    ans = ''
-    if p == '':
-        return ans
-    idx = balance(p)
-    u = p[:idx+1]
-    v = p[idx+1:]
+plus, minus, multi, div = map(int, input().split())
 
-    if proper(p):
-        ans = u + solution(v)
+max_value = -10e9
+min_value = 10e9
+
+
+# i는 순서, now 지금 값
+def dfs(i, now):
+    global plus, minus, multi, div, max_value, min_value
+
+    if i == n:
+        min_value = min(min_value, now)
+        max_value = max(max_value, now)
     else:
-        ans += '('
-        ans += u + solution(v)
-        ans += ')'
-        u = list(u[1:-1])
-        for i in range(len(u)):
-            if u[i] == '(':
-                u[i] = ')'
-            else:
-                u[i] = '('
-        ans += ''.join(u)
+        if plus > 0:
+            plus -= 1
+            dfs(i+1, now+data[i])
+            plus += 1
+        if minus > 0:
+            minus -= 1
+            dfs(i+1, now-data[i])
+            minus += 1
+        if multi > 0:
+            multi -= 1
+            dfs(i+1, now*data[i])
+            multi += 1
+        if div > 0:
+            div -= 1
+            dfs(i+1, int(now/data[i]))
+            div += 1
 
-    return ans
+
+dfs(1, data[0])
+
+print(max_value)
+print(min_value)
