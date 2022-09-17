1c0db9453e08d557c7cd96865d00ca3b073c0b5f
jung su young <j9972@naver.com>
2022-09-17 13:45:56 +0900

    음료수 얼려먹기

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,55 +1,25 @@
-from collections import deque
+n, m = map(int, input().split())
+
+board = []
+for i in range(n):
+    board.append(list(map(int, input())))
 
 
-def next_get_pos(pos, board):
-    next_pos = []
-    pos = list(pos)
-    pos1_x, pos1_y, pos2_x, pos2_y = pos[0][0], pos[0][1], pos[1][0], pos[1][1],
-
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-
-    for i in range(4):
-        pos1_next_x, pos1_next_y = pos1_x + dx[i], pos1_y + dy[i]
-        pos2_next_x, pos2_next_y = pos2_x + dx[i], pos2_y + dy[i]
-
-        if board[pos1_next_x][pos1_next_y] == 0 and board[pos2_next_x][pos2_next_y] == 0:
-            next_pos.append({(pos1_next_x, pos1_next_y),
-                            (pos2_next_x, pos2_next_y)})
-
-    if pos1_x == pos2_x:
-        for i in [-1, 1]:
-            if board[pos1_x + i][pos1_y] == 0 and board[pos2_x + i][pos2_y] == 0:
-                next_pos.append({(pos1_x, pos1_y), (pos1_x + i, pos1_y)})
-                next_pos.append({(pos2_x, pos2_y), (pos2_x + i, pos2_y)})
-
-    elif pos1_y == pos2_y:
-        for i in [-1, 1]:
-            if board[pos1_x][pos1_y + i] == 0 and board[pos2_x][pos2_y + i] == 0:
-                next_pos.append({(pos1_x, pos1_y), (pos1_x, pos1_y + i)})
-                next_pos.append({(pos2_x, pos2_y), (pos2_x, pos2_y + i)})
-    return next
+def dfs(x, y):
+    if 0 <= x < n and 0 <= y < m:
+        if board[x][y] == 0:
+            board[x][y] = 1
+            dfs(x-1, y)
+            dfs(x+1, y)
+            dfs(x, y-1)
+            dfs(x, y+1)
+            return True
+    return False
 
 
-def solution(board):
-    n = len(board)
-    newBoard = [[1]*(n+2) for _ in range(n+2)]
-    for i in range(n):
-        for j in range(n):
-            newBoard[i+1][j+1] = board[i][j]
-
-    visited = []
-    queue = deque()
-    pos = {(1, 1), (1, 2)}
-    visited.append(pos)
-    queue.append((pos, 0))
-
-    while queue:
-        pos, cost = queue.popleft()
-        if (n, n) in pos:
-            return cost
-        for next in next_get_pos(pos, newBoard):
-            if next not in visited:
-                queue.append((next, cost + 1))
-                visited.append(next)
-    return 0
+check = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            check += 1
+print(check)
