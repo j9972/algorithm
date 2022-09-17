fa8ba8c77a46b652d7050cf19537353ad9b0c927
jung su young <j9972@naver.com>
2022-09-17 15:06:25 +0900

    bfs에선 처음에 시작하는 위치를 마지막에 return 해줘야 한다

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,25 +1,29 @@
+from collections import deque
+
 n, m = map(int, input().split())
 
 board = []
 for i in range(n):
     board.append(list(map(int, input())))
 
-
-def dfs(x, y):
-    if 0 <= x < n and 0 <= y < m:
-        if board[x][y] == 0:
-            board[x][y] = 1
-            dfs(x-1, y)
-            dfs(x+1, y)
-            dfs(x, y-1)
-            dfs(x, y+1)
-            return True
-    return False
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
-check = 0
-for i in range(n):
-    for j in range(m):
-        if dfs(i, j) == True:
-            check += 1
-print(check)
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1:
+                board[nx][ny] = board[x][y] + 1
+                queue.append((nx, ny))
+    return board[n-1][m-1]
+
+
+print(bfs(0, 0))
