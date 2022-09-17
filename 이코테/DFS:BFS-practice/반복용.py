65116feecf0d7ca7bce0bcfbc559fab6b12cb4a7
jung su young <j9972@naver.com>
2022-09-17 16:37:02 +0900

    특정거리찾기

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,29 +1,33 @@
 from collections import deque
 
-n, m = map(int, input().split())
-
-board = []
-for i in range(n):
-    board.append(list(map(int, input())))
-
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+n, m, k, x = list(map(int, input().split()))
 
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
+board = [[] for _ in range(n+1)]
 
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+for i in range(m):
+    a, b = map(int, input().split())
+    board[a].append(b)
 
-            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1:
-                board[nx][ny] = board[x][y] + 1
-                queue.append((nx, ny))
-    return board[n-1][m-1]
+distance = [-1] * (n+1)
+distance[x] = 0
 
+queue = deque([x])
 
-print(bfs(0, 0))
+while queue:
+    now = queue.popleft()
+
+    for next in board[now]:
+        if distance[next] == -1:
+            distance[next] = distance[now] + 1
+            queue.append(next)
+
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        check = True
+        print(i)
+if check == False:
+    print(-1)
