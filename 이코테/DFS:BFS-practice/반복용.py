fab2ec8a6f06a9b771085ca1603d29298469f87f
jung su young <j9972@naver.com>
2022-09-17 17:48:55 +0900

    범위체크잘하기

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,63 +1,37 @@
-from re import L
+from collections import deque
 
-
-n, m = map(int, input().split())
-
-graph = [[0] * m for _ in range(n)]
-
-board = []
-for i in range(n):
-    board.append(list(map(int, input().split())))
+n, k = map(int, input().split())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+board = []
+graph = []
 
-def virus(x, y):
+for i in range(n):
+    board.append(list(map(int, input().split())))
+    for j in range(n):
+        if board[i][j] != 0:
+            graph.append((board[i][j], 0, i, j))
+
+tarS, tarX, tarY = map(int, input().split())
+
+graph.sort()
+queue = deque(graph)
+
+while queue:
+    virus, s, x, y = queue.popleft()
+
+    if s == tarS:
+        break
+
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < n and 0 <= ny < m:
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = 2
-                virus(nx, ny)
+        if 0 <= nx < n and 0 <= ny < n:
+            if board[nx][ny] == 0:
+                board[nx][ny] = virus
+                queue.append((virus, s+1, nx, ny))
 
-
-def score():
-    score = 0
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 0:
-                score += 1
-    return score
-
-
-res = 0
-
-
-def dfs(count):
-    global res
-    if count == 3:
-        for i in range(n):
-            for j in range(m):
-                graph[i][j] = board[i][j]
-        for i in range(n):
-            for j in range(m):
-                if graph[i][j] == 2:
-                    virus(i, j)
-        res = max(res, score())
-        return
-
-    for i in range(n):
-        for j in range(m):
-            if board[i][j] == 0:
-                board[i][j] = 1
-                count += 1
-                dfs(count)
-                board[i][j] = 0
-                count -= 1
-
-
-dfs(0)
-print(res)
+print(board[tarX-1][tarY-1])
