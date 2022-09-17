b88c6b5c5b01492635d069ff220fb06a8904be99
jung su young <j9972@naver.com>
2022-09-17 17:24:41 +0900

    virus -> 여분의 배열을 만들어서 보기

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,33 +1,63 @@
-from collections import deque
+from re import L
+
+
+n, m = map(int, input().split())
+
+graph = [[0] * m for _ in range(n)]
+
+board = []
+for i in range(n):
+    board.append(list(map(int, input().split())))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-n, m, k, x = list(map(int, input().split()))
 
-board = [[] for _ in range(n+1)]
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-for i in range(m):
-    a, b = map(int, input().split())
-    board[a].append(b)
+        if 0 <= nx < n and 0 <= ny < m:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = 2
+                virus(nx, ny)
 
-distance = [-1] * (n+1)
-distance[x] = 0
 
-queue = deque([x])
+def score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                score += 1
+    return score
 
-while queue:
-    now = queue.popleft()
 
-    for next in board[now]:
-        if distance[next] == -1:
-            distance[next] = distance[now] + 1
-            queue.append(next)
+res = 0
 
-check = False
-for i in range(1, n+1):
-    if distance[i] == k:
-        check = True
-        print(i)
-if check == False:
-    print(-1)
+
+def dfs(count):
+    global res
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                graph[i][j] = board[i][j]
+        for i in range(n):
+            for j in range(m):
+                if graph[i][j] == 2:
+                    virus(i, j)
+        res = max(res, score())
+        return
+
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 0:
+                board[i][j] = 1
+                count += 1
+                dfs(count)
+                board[i][j] = 0
+                count -= 1
+
+
+dfs(0)
+print(res)
