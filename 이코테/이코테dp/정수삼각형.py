dcd6810caa08eb68c454a6675d4ef44e8cfeefff
jung su young <j9972@naver.com>
2022-09-22 15:42:45 +0900

    금광이랑 비슷한 문제

---
+++ 이코테/이코테dp/정수삼각형.py
@@ -0,0 +1,24 @@
+from turtle import left
+
+
+n = int(input())
+
+dp = []
+
+for i in range(n):
+    dp.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            left_up = 0
+        else:
+            left_up = dp[i-1][j-1]
+
+        if j == i:
+            up = 0
+        else:
+            up = dp[i-1][j]
+
+        dp[i][j] += max(up, left_up)
+print(max(dp[n-1]))
