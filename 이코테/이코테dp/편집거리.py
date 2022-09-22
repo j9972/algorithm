7c54328f3bec034304ee8b855d3a5e82119fd04c
jung su young <j9972@naver.com>
2022-09-22 17:42:54 +0900

    편집거리

---
+++ 이코테/이코테dp/편집거리.py
@@ -0,0 +1,26 @@
+a = input()
+b = input()
+
+
+def edit(a, b):
+    n = len(a)
+    m = len(b)
+
+    dp = [[0]*(m+1) for _ in range(n+1)]
+
+    for i in range(1, n+1):
+        dp[i][0] = i
+    for j in range(1, m+1):
+        dp[0][j] = j
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if a[i-1] == b[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1])
+
+    return dp[n][m]
+
+
+print(edit(a, b))
