2f8b616bf451c656813999e5637e75d4d0b6efc5
jung su young <j9972@naver.com>
2022-09-25 15:41:27 +0900

    similar question

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,29 +1,20 @@
-for tc in range(int(input())):
-    n, m = map(int, input().split())
-    data = list(map(int, input().split()))
+n = int(input())
 
-    dp = []
-    idx = 0
-    for i in range(n):
-        dp.append(data[idx:idx+m])
-        idx += m
-
-    for j in range(1, m):
-        for i in range(n):
-            if i == 0:
-                left_up = 0
-            else:
-                left_up = dp[i-1][j-1]
-            if i == n-1:
-                left_down = 0
-            else:
-                left_down = dp[i+1][j-1]
-
-            left = dp[i][j-1]
-            dp[i][j] += max(left_down, left_up, left)
-
-res = 0
+dp = []
 for i in range(n):
-    res = max(res, dp[i][m-1])
+    dp.append(list(map(int, input().split())))
 
-print(res)
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            left_up = 0
+        else:
+            left_up = dp[i-1][j-1]
+        if j == i:
+            up = 0
+        else:
+            up = dp[i-1][j]
+
+        dp[i][j] += max(left_up, up)
+
+print(max(dp[n-1]))
