d7872e0e98fdc97f3cab94c61a1ed3be9b1c7c18
jung su young <j9972@naver.com>
2022-09-25 13:18:39 +0900

    얻어걸림

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,12 +1,12 @@
 n = int(input())
-data = list(map(int, input().split()))
 
-dp = [0] * 100
+dp = [0] * 1000
 
-dp[0] = data[0]
-dp[1] = max(data[0], data[1])
+dp[1] = 1
+dp[2] = 3
+dp[3] = 5
 
-for i in range(2, n):
-    dp[i] = max(dp[i-1], dp[i-2] + data[i])
+for i in range(3, n):
+    dp[i] = (dp[i-1] + dp[i-2] * 2) % 796796
 
-print(dp[n-1])
+print(dp[n])
