b2f07cc0a2f3ffd1c1315cb9ff8817aad1943c7c
jung su young <j9972@naver.com>
2022-09-25 15:16:27 +0900

    금광

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,19 +1,29 @@
-# 효율적인 화폐 구성
-n, m = map(int, input().split())
+for tc in range(int(input())):
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
 
-data = []
+    dp = []
+    idx = 0
+    for i in range(n):
+        dp.append(data[idx:idx+m])
+        idx += m
+
+    for j in range(1, m):
+        for i in range(n):
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i-1][j-1]
+            if i == n-1:
+                left_down = 0
+            else:
+                left_down = dp[i+1][j-1]
+
+            left = dp[i][j-1]
+            dp[i][j] += max(left_down, left_up, left)
+
+res = 0
 for i in range(n):
-    data.append(int(input()))
+    res = max(res, dp[i][m-1])
 
-dp = [10001] * (m+1)
-dp[0] = 0
-
-for i in range(n):
-    for j in range(data[i], m+1):
-        if dp[j-data[i]] != 10001:
-            dp[j] = min(dp[j], dp[j-data[i]] + 1)
-
-if dp[m] != 10001:
-    print(dp[m])
-else:
-    print(-1)
+print(res)
