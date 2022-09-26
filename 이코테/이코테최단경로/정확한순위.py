ab725db879d0ba47f217b078ce1121084124da5c
jung su young <j9972@naver.com>
2022-09-26 20:08:35 +0900

    두학생의  성적비교 이므로 플로이드

---
+++ 이코테/이코테최단경로/정확한순위.py
@@ -0,0 +1,33 @@
+# 정확한 순위는 학생 a,b를 서로서로 비교를 하는 성적 비교이기에 플로이드이다
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+INF = int(1e9)
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 1
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+res = 0
+for i in range(1, n+1):
+    count = 0
+    for j in range(1, n+1):
+        if graph[i][j] != INF and graph[j][i] != INF:
+            count += 1
+    if count == n:
+        res += 1
+print(count)
---
+++ 이코테/이코테최단경로/플로이드.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+INF = int(1e9)
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+# 최소 거리 단선만 구하면 되기때문에 크기 비교를 해야한다
+for i in range(m):
+    a, b, c = map(int, input().split())
+    if c < graph[a][b]:
+        graph[a][b] = c
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[a][b], end=' ')
+
+    print()
