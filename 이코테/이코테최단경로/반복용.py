1e784a0b9c4169fb8ce7f1c6853ed3b209fa542f
jung su young <j9972@naver.com>
2022-09-27 11:31:52 +0900

    정확한순위

--- 이코테/이코테최단경로/반복용.py
+++ 이코테/이코테최단경로/반복용.py
@@ -1,18 +1,16 @@
-# 플로이드 - 플로이드
+# 정확한순위 - 플로이드
 import sys
 import heapq
 input = sys.stdin.readline
 
-n = int(input())
-m = int(input())
+n, m = map(int, input().split())
 INF = int(1e9)
 
 graph = [[INF] * (n+1) for _ in range(n+1)]
 
 for _ in range(m):
-    a, b, c = map(int, input().split())
-    if c < graph[a][b]:
-        graph[a][b] = c
+    a, b = map(int, input().split())
+    graph[a][b] = 1
 
 for a in range(1, n+1):
     for b in range(1, n+1):
@@ -23,11 +21,12 @@ for k in range(1, n+1):
     for a in range(1, n+1):
         for b in range(1, n+1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-
-for a in range(1, n+1):
-    for b in range(1, n+1):
-        if graph[a][b] == INF:
-            print(0, end=' ')
-        else:
-            print(graph[a][b], end=' ')
-    print()
+res = 0
+for i in range(1, n+1):
+    count = 0
+    for j in range(1, n+1):
+        if graph[i][j] != INF and graph[j][i] != INF:
+            count += 1
+    if count == n:
+        res += 1
+print(res)
