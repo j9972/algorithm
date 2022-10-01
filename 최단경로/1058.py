b0f2e0e5c89f9eae52d3d7509d483b9f7aea580b
jung su young <j9972@naver.com>
2022-10-01 21:01:46 +0900

    n의 범위

--- 최단경로/1058.py
+++ 최단경로/1058.py
@@ -3,20 +3,20 @@ input = sys.stdin.readline
 
 n = int(input())
 
-graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
-friend = [list(input()) for _ in range(n+1)]
+graph = [[0 for _ in range(n)] for _ in range(n)]
+friend = [list(input()) for _ in range(n)]
 
 
-count = 0
+res = 0
 
-for k in range(1, n+1):
-    for a in range(1, n+1):
-        for b in range(1, n+1):
+for k in range(n):
+    for a in range(n):
+        for b in range(n):
             if a == b:
                 continue
 
             if friend[a][b] == 'Y' or (friend[a][k] == 'Y' and friend[k][b] == 'Y'):
-                friend[a][b] = 1
+                graph[a][b] = 1
 
 for i in graph:
     res = max(res, sum(i))
