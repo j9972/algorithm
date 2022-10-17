9609f632cb4a894ed2fb84aeab1c1d84eba9bf81
jung su young <j9972@naver.com>
2022-10-18 00:35:28 +0900

    각각의 행에 대해서 어떤 수를 고르고 그 수들을 비교하는 거면 데이터를 받아올때 각 행마다의 데이터를 비교하는 방법도 있다

--- 이코테/이코테그리드/반복용.py
+++ 이코테/이코테그리드/반복용.py
@@ -1,28 +1,16 @@
-# 큰 수의 법칙
+# 숫자 카드 게임
 import sys
 input = sys.stdin.readline
 
-n, m, k = map(int, input().split())
-
-data = list(map(int, input().split()))
-data = sorted(data, reverse=True)
-count = 0
+n, m = map(int, input().split())
 
 ans = 0
 
-# 가장 큰수가 2개일때 -> m * 큰수
-if data[0] == data[1]:
-    ans += m * data[0]
+for i in range(n):
+    data = map(int, input().split())
 
-# 가장 큰수가 1개일때 ->  while ( k * 큰수 + 2번째 큰수 <= m )
-if data[0] != data[1]:
-    while m > 0:
-        ans += data[0]
-        m -= 1
-        count += 1
-        if count == k:
-            ans += data[1]
-            m -= 1
-            count = 0
+    minValue = min(data)
+
+    ans = max(ans, minValue)
 
 print(ans)
