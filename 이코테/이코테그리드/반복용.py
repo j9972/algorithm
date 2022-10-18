205fc2e36bd54ad433fc929c180c4f8332ad14de
jung su young <j9972@naver.com>
2022-10-18 15:24:05 +0900

    코인을 추가하는 방식으로 생각하기

--- 이코테/이코테그리드/반복용.py
+++ 이코테/이코테그리드/반복용.py
@@ -1,26 +1,15 @@
-# 문자열 뒤집기
-
+# 만들 수 없는 금액
 import sys
 input = sys.stdin.readline
 
-data = input()
+n = int(input())
+data = list(map(int, input().split()))
 
+data.sort()
 
-zeroCount = 0
-oneCount = 0
-
-data_list = list(data)
-
-if data[0] == '1':
-    oneCount += 1
-else:
-    zeroCount += 1
-
-
-for i in range(len(data_list)-1):
-    if data[i] != data[i+1]:  # i+1 때문에 처음 시작하는 데이터의 값을 알아야 한다.
-        if data[i+1] == '1':
-            oneCount += 1
-        else:
-            zeroCount += 1
-print(min(zeroCount, oneCount))
+t = 1
+for i in data:
+    if t < i:  # i 가 새롭게 추가된 동전이라는 의미
+        break
+    t += i
+print(t)
