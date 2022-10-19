2482761c4acf34e6e02709284faf0437e3571e2f
jung su young <j9972@naver.com>
2022-10-20 00:53:51 +0900

    오른쪽으로 간다니까 왼쪽에서 오는거를 체크하기

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,24 +1,37 @@
-# 효율적인 화폐 구성
+# 금광
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-data = []
-for i in range(n):
-    data.append(int(input()))
+for tc in range(int(input())):
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
 
-# 결국에 최소한의 개수의 화폐로 m원을 맞춰야 하므로 이렇게 초기화 하는 것이 맞다
-d = [10001] * (m+1)
+    d = []
 
-# 0원을 만드는데는 0번이면 된다
-d[0] = 0
-for i in range(n):
-    for j in range(data[i], m+1):
-        # m원을 맞출 수 있다면
-        if d[j-data[i]] != 10001:
-            d[j] = min(d[j], d[j-data[i]] + 1)
+    idx = 0
+    for i in range(m):
+        d.append(data[idx:idx+m])
+        idx += m
 
-if d[m] == 10001:
-    print(-1)
-else:
-    print(d[m])
+    for j in range(1, m):
+        for i in range(n):
+
+            # left up
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = d[i-1][j-1]
+            # left down
+            if i == n-1:
+                left_down = 0
+            else:
+                left_down = d[i+1][j-1]
+
+            left = d[i][j-1]
+            d[i][j] += max(left, left_down, left_up)
+
+    res = 0
+    for i in range(n):
+        res = max(res, d[i][m-1])
+
+    print(res)
