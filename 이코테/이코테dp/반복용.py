8861b613930aa3eb73205054a0ee093ac329f8a1
jung su young <j9972@naver.com>
2022-10-19 23:38:41 +0900

    효율적인 화폐구성

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,17 +1,24 @@
-# 1로 만들기
+# 효율적인 화폐 구성
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+n, m = map(int, input().split())
+data = []
+for i in range(n):
+    data.append(int(input()))
 
-d = [0] * 1001
+# 결국에 최소한의 개수의 화폐로 m원을 맞춰야 하므로 이렇게 초기화 하는 것이 맞다
+d = [10001] * (m+1)
 
+# 0원을 만드는데는 0번이면 된다
 d[0] = 0
-d[1] = 1
-d[2] = 3
-#d[3] = 5
+for i in range(n):
+    for j in range(data[i], m+1):
+        # m원을 맞출 수 있다면
+        if d[j-data[i]] != 10001:
+            d[j] = min(d[j], d[j-data[i]] + 1)
 
-for i in range(3, n+1):
-    d[i] = d[i-1] + (d[i-2] * 2) % 796796
-
-print(d[n])
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
