a87ced8ed82908ad604b4835ed5cccfcd1bc478d
jung su young <j9972@naver.com>
2022-10-19 23:08:20 +0900

    n의 범위를 잘 체크해야한다

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -2,20 +2,14 @@
 import sys
 input = sys.stdin.readline
 
-x = int(input())
+n = int(input())
+data = list(map(int, input().split()))
 
-d = [0] * 30001
+d = [0] * 100
+d[0] = data[0]
+d[1] = max(d[0], data[1])
 
-#d[1] = 0
-# d[2] = 1 = d[1] + 1
+for i in range(2, n):
+    d[i] = max(d[i-1], d[i-2]+data[i])
 
-for i in range(2, x+1):
-    d[i] = d[i-1] + 1
-    if i % 5 == 0:
-        d[i] = min(d[i], d[i//5]+1)
-    elif i % 3 == 0:
-        d[i] = min(d[i], d[i//3]+1)
-    elif i % 2 == 0:
-        d[i] = min(d[i], d[i//2]+1)
-
-print(d[x])
+print(d[n-1])
