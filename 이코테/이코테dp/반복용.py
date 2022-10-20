9505f078a63c0558f7d0411aa1e312c068c31735
jung su young <j9972@naver.com>
2022-10-20 15:35:22 +0900

    퇴사

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,37 +1,25 @@
-# 금광
+# 퇴사
 import sys
 input = sys.stdin.readline
 
-for tc in range(int(input())):
-    n, m = map(int, input().split())
-    data = list(map(int, input().split()))
+n = int(input())
 
-    d = []
+dp = [0] * (n+1)
 
-    idx = 0
-    for i in range(m):
-        d.append(data[idx:idx+m])
-        idx += m
+t = []
+p = []
 
-    for j in range(1, m):
-        for i in range(n):
+for i in range(n):
+    time, price = map(int, input().split())
+    t.append(time)
+    p.append(price)
 
-            # left up
-            if i == 0:
-                left_up = 0
-            else:
-                left_up = d[i-1][j-1]
-            # left down
-            if i == n-1:
-                left_down = 0
-            else:
-                left_down = d[i+1][j-1]
-
-            left = d[i][j-1]
-            d[i][j] += max(left, left_down, left_up)
-
-    res = 0
-    for i in range(n):
-        res = max(res, d[i][m-1])
-
-    print(res)
+mv = 0
+for i in range(n-1, -1, -1):
+    time = t[i] + i
+    if time <= n:
+        dp[i] = max(dp[time] + p[i], mv)
+        mv = dp[i]
+    else:
+        dp[i] = mv
+print(mv)
