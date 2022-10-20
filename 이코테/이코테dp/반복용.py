508ea98b2738a38e90f7280f395834e86aea059d
jung su young <j9972@naver.com>
2022-10-20 16:17:59 +0900

    편집거리

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,28 +1,30 @@
-# 못생긴수
+# 편집거리
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+str1 = input()
+str2 = input()
 
-dp = [0] * n  # 1차원
-dp[0] = 1
 
-idx2 = idx3 = idx5 = 0
+def edit(a, b):
+    n = len(a)
+    m = len(b)
 
-# 곱셈값 초기화
-next2, next3, next5 = 2, 3, 5
+    dp = [[0]*(m+1) for _ in range(n+1)]
 
-for i in range(1, n):
-    dp[i] = min(next2, next3, next5)
+    for i in range(1, n+1):
+        dp[i][0] = i
+    for i in range(1, m+1):
+        dp[0][i] = i
 
-    if dp[i] == next2:
-        idx2 += 1
-        next2 = dp[idx2] * 2
-    if dp[i] == next3:
-        idx3 += 1
-        next3 = dp[idx3] * 3
-    if dp[i] == next5:
-        idx5 += 1
-        next5 = dp[idx5] * 5
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if a[i-1] == b[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = 1 + min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j])
 
-print(dp[n-1])
+    return dp[n][m]
+
+
+print(edit(str1, str2))
