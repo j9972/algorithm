2b75476cc9e56f337faefc6a6ef7db00a536c146
jung su young <j9972@naver.com>
2022-10-22 17:32:13 +0900

    comment

--- dp/1446.py
+++ dp/1446.py
@@ -14,6 +14,7 @@ for i in data:
     s, e, sD = i
     if e <= d:
         D[e] = min(D[s]+sD, D[e])
+    # 지름길 업데이트 해주는 코드 e부터 d까지 1씩 전진하면서 최소값을 업데이터 한다
     for k in range(s, d+1):
         D[k] = min(D[k], D[k-1]+1)
 
