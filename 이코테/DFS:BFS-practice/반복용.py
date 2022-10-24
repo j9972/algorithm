9ad7633726012b501af6b7ecc8ca451406215695
jung su young <j9972@naver.com>
2022-10-24 20:14:05 +0900

    경쟁적 전염

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -2,62 +2,38 @@ from collections import deque
 import sys
 sys.setrecursionlimit(10**9)
 
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-resetArray = [[0]*(m+1) for _ in range(n+1)]
+otherArray = []
 
 data = []
 for i in range(n):
     data.append(list(map(int, input().split())))
+    for j in range(n):
+        if data[i][j] != 0:
+            otherArray.append((data[i][j], 0, i, j))
 
-res = 0
+otherArray.sort()
+queue = deque(otherArray)
 
+s, x, y = map(int, input().split())
 
-def virus(x, y):
+while queue:
+    virus, target_s, target_x, target_y = queue.popleft()
+
+    if s == target_s:
+        break
+
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+        nx = target_x + dx[i]
+        ny = target_y + dy[i]
 
-        if 0 <= nx < n and 0 <= ny < m:
-            if resetArray[nx][ny] == 0:
-                resetArray[nx][ny] = 2
-                virus(nx, ny)
+        if 0 <= nx < n and 0 <= ny < n:
+            if data[nx][ny] == 0:
+                data[nx][ny] = virus
+                queue.append((virus, target_s+1, nx, ny))
 
-
-def getScore():
-    score = 0
-    for i in range(n):
-        for j in range(n):
-            if resetArray[i][j] == 0:
-                score += 1
-    return score
-
-
-def dfs(count):
-    global res
-    if count == 3:
-        for i in range(n):
-            for j in range(m):
-                resetArray[i][j] = data[i][j]
-        for i in range(n):
-            for j in range(m):
-                if resetArray[i][j] == 2:
-                    virus(i, j)
-        res = max(res, getScore())
-        return
-
-    for i in range(n):
-        for j in range(m):
-            if data[i][j] == 0:
-                data[i][j] = 1
-                count += 1
-                dfs(count)
-                data[i][j] = 0
-                count -= 1
-
-
-dfs(0)
-print(res)
+print(data[x-1][y-1])
