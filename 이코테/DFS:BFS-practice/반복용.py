2aba086f131d97939101185c1d340d5b3373416e
jung su young <j9972@naver.com>
2022-10-24 20:28:37 +0900

    괄호 변환

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -2,38 +2,50 @@ from collections import deque
 import sys
 sys.setrecursionlimit(10**9)
 
-n, k = map(int, input().split())
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+def balance(p):
+    count = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            return i
 
-otherArray = []
 
-data = []
-for i in range(n):
-    data.append(list(map(int, input().split())))
-    for j in range(n):
-        if data[i][j] != 0:
-            otherArray.append((data[i][j], 0, i, j))
+def proper(p):
+    count = 0
+    for i in p:
+        if i == '(':
+            count += 1
+        else:
+            if count == 0:
+                return False
+            else:
+                count -= 1
+        return True
 
-otherArray.sort()
-queue = deque(otherArray)
 
-s, x, y = map(int, input().split())
+def solution(p):
+    ans = ''
+    if p == '':
+        print(ans)
+    idx = balance(p)
+    u = p[:idx+1]
+    v = p[idx+1:]
 
-while queue:
-    virus, target_s, target_x, target_y = queue.popleft()
-
-    if s == target_s:
-        break
-
-    for i in range(4):
-        nx = target_x + dx[i]
-        ny = target_y + dy[i]
-
-        if 0 <= nx < n and 0 <= ny < n:
-            if data[nx][ny] == 0:
-                data[nx][ny] = virus
-                queue.append((virus, target_s+1, nx, ny))
-
-print(data[x-1][y-1])
+    if proper(p):
+        ans = u + solution(v)
+    else:
+        ans += '('
+        ans += u + solution(v)
+        ans += ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == ')':
+                u[i] = '('
+            else:
+                u[i] = ')'
+        ans += ''.join(u)
+    return ans
