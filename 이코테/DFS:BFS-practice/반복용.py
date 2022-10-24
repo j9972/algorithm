b3f4ca928dfe9a5ff712c3117998a3eee5b97323
jung su young <j9972@naver.com>
2022-10-24 20:04:53 +0900

    연구소 dfs -> 3 types of function

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -2,36 +2,62 @@ from collections import deque
 import sys
 sys.setrecursionlimit(10**9)
 
-n, m, k, x = map(int, input().split())
-
-data = [[] for _ in range(n+1)]
-
-for i in range(m):
-    a, b = map(int, input().split())
-    data[a].append(b)
-
-queue = deque([x])
+n, m = map(int, input().split())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-distance = [-1] * (n+1)
-distance[x] = 0
+resetArray = [[0]*(m+1) for _ in range(n+1)]
 
-while queue:
-    now = queue.popleft()
+data = []
+for i in range(n):
+    data.append(list(map(int, input().split())))
 
-    for next in data[now]:
-        if distance[next] == -1:
-            distance[next] = distance[now] + 1
-            queue.append(next)
+res = 0
 
-check = False
-# 1부터 n번까지의 도시를 이동하기에 range 범위가 이렇게 된다.
-for i in range(1, n+1):
-    if distance[i] == k:
-        print(i)
-        check = True
 
-if check == False:
-    print(-1)
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < m:
+            if resetArray[nx][ny] == 0:
+                resetArray[nx][ny] = 2
+                virus(nx, ny)
+
+
+def getScore():
+    score = 0
+    for i in range(n):
+        for j in range(n):
+            if resetArray[i][j] == 0:
+                score += 1
+    return score
+
+
+def dfs(count):
+    global res
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                resetArray[i][j] = data[i][j]
+        for i in range(n):
+            for j in range(m):
+                if resetArray[i][j] == 2:
+                    virus(i, j)
+        res = max(res, getScore())
+        return
+
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 0:
+                data[i][j] = 1
+                count += 1
+                dfs(count)
+                data[i][j] = 0
+                count -= 1
+
+
+dfs(0)
+print(res)
