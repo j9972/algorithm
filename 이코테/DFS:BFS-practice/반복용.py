400343cd74da280481d45cd52729a7e5389a4f17
jung su young <j9972@naver.com>
2022-10-26 01:36:32 +0900

    블록이동

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,55 +1,56 @@
 from collections import deque
 
-n, l, r = map(int, input().split())
 
-data = []
-for i in range(n):
-    data.append(list(map(int, input().split())))
+def nextPos(pos, board):
+    nextPos = []
+    pos = list(pos)
+    pos1_x, pos1_y, pos2_x, pos2_y = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
+    for i in range(4):
+        pos1_next_x, pos1_next_y, pos2_next_x, pos2_next_y = pos1_x + \
+            dx[i], pos1_y+dy[i], pos2_x+dx[i], pos2_y+dy[i]
 
-def bfs(x, y, index):
-    united = []
-    united.append((x, y))
+        if board[pos1_next_x][pos1_next_y] == 0 and board[pos2_next_x][pos2_next_y] == 0:
+            nextPos.append({(pos1_next_x, pos1_next_y),
+                           (pos2_next_x, pos2_next_y)})
 
-    queue = deque()
-    queue.append((x, y))
+    if pos1_x == pos2_x:
+        for i in [-1, 1]:
+            if board[pos1_x+i][pos1_y] == 0 and board[pos2_x+i][pos2_y] == 0:
+                nextPos.append({(pos1_x, pos1_y), (pos1_x+i, pos1_y)})
+                nextPos.append({(pos2_x, pos2_y), (pos2_x+i, pos2_y)})
+    if pos1_y == pos2_y:
+        for i in [-1, 1]:
+            if board[pos1_x][pos1_y+i] == 0 and board[pos2_x][pos2_y+i] == 0:
+                nextPos.append({(pos1_x, pos1_y), (pos1_x, pos1_y)+i})
+                nextPos.append({(pos2_x, pos2_y), (pos2_x, pos2_y)+i})
 
-    union[x][y] = index
-    summary = data[x][y]
-    count = 1
-
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
-                if l <= abs(data[nx][ny] - data[x][y]) <= r:
-                    united.append((nx, ny))
-                    queue.append((nx, ny))
-                    union[nx][ny] = index
-                    summary += data[nx][ny]
-                    count += 1
-    for i, j in united:
-        data[i][j] = summary // count
-    return count
+    return next
 
 
-total = 0
-while True:
-    union = [[-1]*n for _ in range(n)]
-    index = 0
+def solution(board):
+    n = len(board)
+    newboard = [[1]*(n+2) for _ in range(n+2)]
     for i in range(n):
         for j in range(n):
-            if union[i][j] == -1:
-                bfs(i, j, index)
-                index += 1
-    if index == n*n:
-        break
-    total += 1
-print(total)
+            newboard[i+1][j+1] = board[i][j]
+
+    queue = deque()
+    visited = []
+    pos = {(1, 1), (1, 2)}
+    queue.append((pos, 0))
+    visited.append(pos)
+
+    while queue:
+        pos, cost = queue.popleft()
+
+        if (n, n) in pos:
+            return pos
+        for next in nextPos(pos, newboard):
+            if next not in visited:
+                queue.append((next, cost+1))
+                visited.append(next)
+    return 0
