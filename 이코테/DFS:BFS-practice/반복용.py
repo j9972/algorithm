212f1ecc39db1a79c677de0566a316faa3d530b8
jung su young <j9972@naver.com>
2022-10-25 15:42:03 +0900

    감시피하기

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,43 +1,79 @@
 from collections import deque
+from itertools import combinations
 import sys
 sys.setrecursionlimit(10**9)
 
 n = int(input())
 
-data = list(map(int, input().split()))
+teacher = []
+space = []
+data = []
 
-plus, minus, multi, div = map(int, input().split())
-
-minvalue = 10e9
-maxValue = -10e9
-
-
-def dfs(i, now):
-    global plus, minus, multi, div, maxValue, minvalue
-
-    if i == n:
-        minvalue = min(minvalue, now)
-        maxValue = max(maxValue, now)
-    else:
-        if plus > 0:
-            plus -= 1
-            dfs(i+1, now + data[i])
-            plus += 1
-        if minus > 0:
-            minus -= 1
-            dfs(i+1, now - data[i])
-            minus += 1
-        if multi > 0:
-            multi -= 1
-            dfs(i+1, now * data[i])
-            multi += 1
-        if div > 0:
-            div -= 1
-            dfs(i+1, now / int(data[i]))
-            div += 1
+for i in range(n):
+    data.append(list(map(str, input().split())))
+    for j in range(n):
+        if data[i][j] == 'T':
+            teacher.append((i, j))
+        elif data[i][j] == 'X':
+            space.append((i, j))
 
 
-dfs(1, data[0])
+def watch(x, y, direction):
+    # left -> right -> up -> down 순서
+    if direction == 0:
+        while y >= 0:
+            if data[x][y] == 'T':
+                return False
+            elif data[x][y] == 'S':
+                return True
+            y -= 1
+    if direction == 1:
+        while y < n:
+            if data[x][y] == 'T':
+                return False
+            elif data[x][y] == 'S':
+                return True
+            y += 1
+    if direction == 2:
+        while x >= 0:
+            if data[x][y] == 'T':
+                return False
+            elif data[x][y] == 'S':
+                return True
+            x -= 1
+    if direction == 3:
+        while x < n:
+            if data[x][y] == 'T':
+                return False
+            elif data[x][y] == 'S':
+                return True
+            x += 1
+    return False
 
-print(maxValue)
-print(minvalue)
+
+def process():
+    for x, y in teacher:
+        for i in range(4):
+            if watch(x, y, i) == True:
+                return True
+    return False
+
+
+find = True
+
+for d in combinations(data, 3):
+    for x, y in d:
+        data[x][y] == 'O'
+
+    if process():
+        find = False
+        break
+
+    for x, y in d:
+        data[x][y] = 'X'
+
+
+if find == True:
+    print('YES')
+else:
+    print('NO')
