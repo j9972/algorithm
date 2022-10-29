c185a27bf92408696f4c8323f37a4a9ce2dd00db
jung su young <j9972@naver.com>
2022-10-29 18:45:52 +0900

    개선가능한 다익스트라

--- 이코테/이코테최단경로/반복용.py
+++ 이코테/이코테최단경로/반복용.py
@@ -1,33 +1,44 @@
-# 미래도시
+# 전보
 import heapq
+from itertools import count
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n, m, start = map(int, input().split())  # 도시개수, 간선개수, 메시지를 보내고자 하는 도시
 INF = int(1e9)
 
-graph = [[INF]*(n+1) for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
 
 for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if i == j:
-            graph[i][j] = 0
 
-x, k = map(int, input().split())
+def dij(start):
+    q = []
+    distance[start] = 0
+    heapq.heappush(q, (0, start))
 
-for k in range(1, n+1):
-    for a in range(1, n+1):
-        for b in range(1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+    while q:
+        dist, now = heapq.heappop(q)
+        if dist > distance[now]:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-distance = graph[1][k] + graph[k][x]
 
-if distance >= INF:
-    print("-1")
-else:
-    print(distance)
+dij(start)
+
+count = 0
+maxD = 0
+
+for d in distance:
+    if d != INF:
+        count += 1
+        maxD = max(maxD, d)
+
+print(count-1, maxD)
