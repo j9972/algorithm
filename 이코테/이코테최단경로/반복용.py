54eb737373d4730b23a14998a413a4072f87bda4
jung su young <j9972@naver.com>
2022-10-29 18:21:11 +0900

    ㅁㅣ래도시

--- 이코테/이코테최단경로/반복용.py
+++ 이코테/이코테최단경로/반복용.py
@@ -1,50 +1,33 @@
-# 숨바꼭질
-import sys
+# 미래도시
 import heapq
+import sys
 input = sys.stdin.readline
 
+n, m = map(int, input().split())
 INF = int(1e9)
 
-n, m = map(int, input().split())
-start = 1
-graph = [[] for i in range(n+1)]
-distance = [INF] * (n+1)
+graph = [[INF]*(n+1) for _ in range(n+1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append((b, 1))
-    graph[b].append((a, 1))
-
-
-def dijkstra(start):
-    q = []
-    distance[start] = 0
-    heapq.heappush(q, (0, start))
-
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-
-        for i in graph[now]:
-            cost = dist + i[1]
-            if distance[i[0]] > cost:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-
-
-dijkstra(start)
-
-maxNode = 0
-maxDistance = 0
-res = []
+    graph[a][b] = 1
+    graph[b][a] = 1
 
 for i in range(1, n+1):
-    if distance[i] > maxDistance:
-        maxDistance = distance[i]
-        maxNode = i
-        res = [maxNode]
-    elif maxDistance == distance[i]:
-        res.append(i)
+    for j in range(1, n+1):
+        if i == j:
+            graph[i][j] = 0
 
-print(maxNode, maxDistance, len(res))
+x, k = map(int, input().split())
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+distance = graph[1][k] + graph[k][x]
+
+if distance >= INF:
+    print("-1")
+else:
+    print(distance)
