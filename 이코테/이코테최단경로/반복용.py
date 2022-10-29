dc463b952b00575d6dddc25b679695f9008e515c
jung su young <j9972@naver.com>
2022-10-29 19:48:21 +0900

    다익스트라를 이용한 숨바꼭질

--- 이코테/이코테최단경로/반복용.py
+++ 이코테/이코테최단경로/반복용.py
@@ -8,31 +8,49 @@ dy = [0, 1, 0, -1]
 
 INF = int(1e9)
 
-for tc in range(int(input())):
-    n = int(input())
 
-    graph = [[INF]*n for _ in range(n)]
+n, m = map(int, input().split())
 
-    data = []
-    for i in range(n):
-        data.append(list(map(int, input().split())))
+graph = [[] for _ in range(n+1)]
+distance = [INF] * (n+1)
+start = 1
 
-    x, y = 0, 0
-    q = [(data[x][y], x, y)]
-    graph[x][y] = data[x][y]
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append((b, 1))
+    graph[b].append((a, 1))
+
+
+def dij(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
 
     while q:
-        dist, x, y = heapq.heappop(q)
+        dist, now = heapq.heappop(q)
 
-        if dist > graph[x][y]:
+        if dist > distance[now]:
             continue
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-            if 0 <= nx < n and 0 <= ny < n:
-                cost = dist + data[nx][ny]
-                if cost < graph[nx][ny]:
-                    graph[nx][ny] = cost
-                    heapq.heappush(q, (cost, nx, ny))
-    print(graph[n-1][n-1])
+
+dij(start)
+
+maxNode = 0
+maxDistance = 0
+
+res = []
+
+for i in range(1, n+1):
+    if maxDistance < distance[i]:
+        maxNode = i
+        maxDistance = distance[i]
+        res = [maxNode]
+    elif maxDistance == distance[i]:
+        res.append(i)
+
+print(maxNode, maxDistance, len(res))
