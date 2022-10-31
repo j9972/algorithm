3c087fc52762c6d1649c3d1d54405379e499f50c
jung su young <j9972@naver.com>
2022-10-31 16:06:10 +0900

    공유기 사이 거리 체크

--- 이코테/이코테이진탐색/반복용.py
+++ 이코테/이코테이진탐색/반복용.py
@@ -1,11 +1,20 @@
 # 고정점 찾기
+from array import array
 import sys
 from bisect import bisect_left, bisect_right
 
 input = sys.stdin.readline
 
-n = int(input())
-array = list(map(int, input().split()))
+n, c = map(int, input().split())
+data = []
+for i in range(n):
+    data.append(int(input()))
+
+data.sort()
+
+start = 1
+end = data[-1] - data[0]
+res = 0
 
 
 def count_range(a, left_value, right_value):
@@ -14,21 +23,30 @@ def count_range(a, left_value, right_value):
     return right_idx - left_idx
 
 
-def binary_search(a, start, end):
-    if start > end:
-        return None
-    mid = (start + end) // 2
-    if a[mid] == mid:
-        return mid
-    elif a[mid] > mid:
-        return binary_search(a, start, mid - 1)
+# def binary_search(a, start, end):
+#     if start > end:
+#         return None
+#     mid = (start + end) // 2
+#     if a[mid] == mid:
+#         return mid
+#     elif a[mid] > mid:
+#         return binary_search(a, start, mid - 1)
+#     else:
+#         return binary_search(a, mid+1, end)
+
+while start <= end:
+    count = 1
+    val = data[0]
+    mid = (start+end)//2
+
+    for i in range(1, n):
+        if data[i] >= val + mid:
+            val = data[i]
+            count += 1
+    if count >= c:
+        start = mid + 1
+        res = mid
     else:
-        return binary_search(a, mid+1, end)
+        end = mid - 1
 
-
-idx = binary_search(array, 0, n-1)
-
-if idx == None:
-    print(-1)
-else:
-    print(idx)
+print(res)
