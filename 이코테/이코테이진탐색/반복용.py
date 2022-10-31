2180eaa81b3a9ed66d336e32b5c33902669572dc
jung su young <j9972@naver.com>
2022-10-31 15:40:57 +0900

    특정 개수 찾기 -> count range def 사용

--- 이코테/이코테이진탐색/반복용.py
+++ 이코테/이코테이진탐색/반복용.py
@@ -4,32 +4,21 @@ from bisect import bisect_left, bisect_right
 
 input = sys.stdin.readline
 
-n, target = map(int, input().split())
+n, x = map(int, input().split())
 array = list(map(int, input().split()))
-
-start = 0
-end = max(array)
-
-res = 0
-
-while start <= end:
-    total = 0
-    mid = (start + end) // 2
-    for x in array:
-        if x > mid:
-            total += x - mid
-    if total < target:
-        end = mid - 1
-    else:
-        start = mid + 1
-        res = mid
-print(res)
 
 
 def count_range(a, left_value, right_value):
     right_idx = bisect_right(a, right_value)
     left_idx = bisect_left(a, left_value)
     return right_idx - left_idx
+
+
+res = count_range(array, x, x)
+if res != 0:
+    print(res)
+else:
+    print(-1)
 
 
 # def binary_search(a, target, start, end):
