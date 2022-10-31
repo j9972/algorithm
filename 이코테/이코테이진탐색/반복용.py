1fa1de2dfdf6724782f953d199a843c0387e7cca
jung su young <j9972@naver.com>
2022-10-31 15:54:32 +0900

    target 대신에 mid 써서 체크

--- 이코테/이코테이진탐색/반복용.py
+++ 이코테/이코테이진탐색/반복용.py
@@ -1,10 +1,10 @@
-# 떡볶이 떡
+# 고정점 찾기
 import sys
 from bisect import bisect_left, bisect_right
 
 input = sys.stdin.readline
 
-n, x = map(int, input().split())
+n = int(input())
 array = list(map(int, input().split()))
 
 
@@ -14,30 +14,21 @@ def count_range(a, left_value, right_value):
     return right_idx - left_idx
 
 
-res = count_range(array, x, x)
-if res != 0:
-    print(res)
-else:
+def binary_search(a, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if a[mid] == mid:
+        return mid
+    elif a[mid] > mid:
+        return binary_search(a, start, mid - 1)
+    else:
+        return binary_search(a, mid+1, end)
+
+
+idx = binary_search(array, 0, n-1)
+
+if idx == None:
     print(-1)
-
-
-# def binary_search(a, target, start, end):
-#     if start > end:
-#         return None
-#     mid = (start + end) // 2
-#     if a[mid] == target:
-#         return mid
-#     elif a[mid] > target:
-#         return binary_search(a, target, start, mid - 1)
-#     else:
-#         return binary_search(a, target, mid+1, end)
-
-    # while start <= end:
-    #     mid = (start + end) // 2
-    #     if a[mid] == target:
-    #         return mid
-    #     elif a[mid] > target:
-    #         end = mid - 1
-    #     else:
-    #         start = mid + 1
-    #     return None
+else:
+    print(idx)
