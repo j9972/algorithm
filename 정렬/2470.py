6191aa3349c3e8a7e3004fd98513bb9b4a2c040b
jung su young <j9972@naver.com>
2022-11-06 13:37:20 +0900

    두가지를 섞어서 특정 값에 가까이 가기

--- 정렬/2470.py
+++ 정렬/2470.py
@@ -4,3 +4,30 @@ input = sys.stdin.readline
 n = int(input())
 data = list(map(int, input().split()))
 data.sort()
+
+# 절댓값은 작을수록 0에 가깝다는것을 이용해서 문제 풀기
+
+l = 0
+r = n - 1
+
+res = abs(data[l]+data[r])
+final = [data[l], data[r]]
+
+while l < r:
+    lv = data[l]
+    rv = data[r]
+
+    sumv = lv + rv
+
+    if abs(sumv) < res:
+        res = abs(sumv)
+        final = [lv, rv]
+
+        if res == 0:
+            break
+
+    if sumv < 0:
+        l += 1
+    else:
+        r -= 1
+print(final[0], final[1])
