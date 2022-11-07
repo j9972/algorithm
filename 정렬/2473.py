c4ead2a1f5f4afbec6669cf453345f5eafc4fa2c
jung su young <j9972@naver.com>
2022-11-07 20:32:54 +0900

    h-index

--- 정렬/2473.py
+++ 정렬/2473.py
@@ -1,1 +1,56 @@
-#  세 용액
+# #  세 용액
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# data = list(map(int, input().split()))
+# data.sort()
+
+# # 절댓값은 작을수록 0에 가깝다는것을 이용해서 문제 풀기
+
+
+# for i in range(n-2):
+#     l = 0
+#     r = n - 1
+
+#     res = abs(data[l]+data[i]+data[r])
+#     final = [data[l], data[i], data[r]]
+
+#     while l < r:
+#         lv = data[l]
+#         rv = data[r]
+#         mv = data[i]
+
+#         sumv = lv + rv + mv
+
+#         if abs(sumv) < res:
+#             res = abs(sumv)
+#             final = [lv, mv, rv]
+
+#             if res == 0:
+#                 print(final[0], final[1], final[2])
+#                 break
+
+#         if sumv < 0:
+#             l += 1
+#         else:
+#             r -= 1
+
+# print(final[0], final[1], final[2])
+
+from itertools import permutations
+
+numbers = [3, 30, 34, 5, 9]
+
+
+def solution(numbers):
+    res = list(permutations(numbers, len(numbers)))
+    ans = []
+
+    for i in range(len(res)):
+        ans.append(''.join(str(s) for s in res[i]))
+
+    print(max(ans))
+
+
+solution(numbers)
--- 정렬/가장큰수.py
+++ 정렬/가장큰수.py
@@ -1,8 +1,34 @@
+from itertools import permutations
+
 # 프로그래머스
 # numbers는 주어지는 리스트 값들
+
+
 def solution(numbers):
     return str(int(''.join(sorted(list(map(str, numbers)), key=lambda x: x*3, reverse=True))))
     # x*3의 의미는 만약에 6, 10, 2  -> 666, 101010, 222
     # str(int()) 를 쓴 이유는 '0000' -> '0' 으로 변경을 위함
 # 문자열 비교연산의 경우엔 첫번째 인덱스인 666[0]인 6과 101010[0]인 1과 222[0]인 2를 ascii숫자로 바꿔서 비교합니다.
 # #물론 같으면, 다음 인덱스도 비교합니다. 비교한 결과 [6, 2, 10]의 순으로 정렬됩니다
+
+# 시간 초과가 뜨지만 새로운 방법
+
+
+def solution(numbers):
+    res = list(permutations(numbers, len(numbers)))
+    ans = []
+    answer = ''
+
+    for i in range(len(res)):
+        ans.append(''.join(str(s) for s in res[i]))
+
+    return max(ans)
+
+#  처음과 같은 방법
+
+
+def solution(numbers):
+    num = [str(i) for i in numbers]
+    num.sort(key=lambda x: x*3, reverse=True)
+
+    return str(int(''.join(num)))
