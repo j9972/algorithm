770c62691c9d089ab4872f0b3146f0c1eebe014f
jung su young <j9972@naver.com>
2022-11-08 12:10:35 +0900

    게임개발

--- 이코테/이코테구현/반복용.py
+++ 이코테/이코테구현/반복용.py
@@ -2,17 +2,54 @@
 import sys
 input = sys.stdin.readline
 
-data = input()
-# a3, a는 열(col), 3은 행(row)
-col = int(data[1])
-row = int(ord(data[0])) - int(ord('a')) + 1
+n, m = map(int, input().split())
+x, y, direction = map(int, input().split())
 
-step = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
-count = 0
+# 방문 처리
+visited = [[0] * m for _ in range(n)]
+visited[x][y] = 1
 
-for i in step:
-    next_col = col + i[1]
-    next_row = row + i[0]
-    if 1 <= next_col <= 8 and 1 <= next_row <= 8:
+data = []
+for i in range(n):
+    data.append(list(map(int, input().split())))
+
+# 북 동 남 서
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+def turn():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+
+count = 1
+turn_time = 0
+
+while True:
+    turn()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    if data[nx][ny] == 0 and visited[nx][ny] == 0:
+        visited[nx][ny] = 1
+        x, y = nx, ny
         count += 1
+        turn_time = 0
+        continue
+    else:
+        turn_time += 1
+
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있나?
+        if data[nx][ny] == 0:
+            x, y = nx, ny
+        else:
+            break
+        turn_time = 0
+
 print(count)
