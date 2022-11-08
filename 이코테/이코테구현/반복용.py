8a207f9c707dcd15aa223e160de9c62288a85381
jung su young <j9972@naver.com>
2022-11-08 19:51:25 +0900

    게임 개발

--- 이코테/이코테구현/반복용.py
+++ 이코테/이코테구현/반복용.py
@@ -1,21 +1,22 @@
-# 왕실의 나이트
+# 게임 개발
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 x, y, direction = map(int, input().split())
 
-# 방문 처리
-visited = [[0] * m for _ in range(n)]
-visited[x][y] = 1
-
 data = []
 for i in range(n):
     data.append(list(map(int, input().split())))
 
-# 북 동 남 서
+visited = [[0] * m for _ in range(n)]
+visited[x][y] = 1
+
+# 북 서 남 동
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
+
+# 왼쪽으로 90도 회전
 
 
 def turn():
@@ -35,21 +36,19 @@ while True:
 
     if data[nx][ny] == 0 and visited[nx][ny] == 0:
         visited[nx][ny] = 1
-        x, y = nx, ny
         count += 1
+        x, y = nx, ny
         turn_time = 0
-        continue
     else:
         turn_time += 1
 
     if turn_time == 4:
         nx = x - dx[direction]
         ny = y - dy[direction]
-        # 뒤로 갈 수 있나?
         if data[nx][ny] == 0:
             x, y = nx, ny
+            turn_time = 0
+
         else:
             break
-        turn_time = 0
-
 print(count)
