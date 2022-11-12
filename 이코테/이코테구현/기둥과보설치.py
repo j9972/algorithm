bda8c6f2e7c64c4b824c225f30cac99f20be95f3
jung su young <j9972@naver.com>
2022-11-12 22:43:17 +0900

    빅오가 M^^3 이므로 매번 함수로 체크를 해줘도 된다. 기둥과 보를 설치하는 것을 기준으로 두고 언제 설치할 수 있는지 체크하고 설치인지 제거인지를 따져보면 된다

---
+++ 이코테/이코테구현/기둥과보설치.py
@@ -0,0 +1,26 @@
+def possible(answer):
+    for x, y, stuff in answer:
+        if stuff == 0:
+            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
+                continue
+            return False
+        elif stuff == 1:
+            if ([x-1, y, 1] in answer and [x+1, y, 1] in answer) or [x, y-1, 0] in answer or [x+1, y-1, 0] in answer:
+                continue
+            return False
+    return True
+
+
+def solution(n, build_frame):
+    answer = []
+    for frame in build_frame:
+        x, y, stuff, operator = frame
+        if operator == 0:
+            answer.remove([x, y, stuff])
+            if not possible(answer):
+                answer.append([x, y, stuff])
+        elif operator == 1:
+            answer.append([x, y, stuff])
+            if not possible(answer):
+                answer.remove([x, y, stuff])
+    return sorted(answer)
