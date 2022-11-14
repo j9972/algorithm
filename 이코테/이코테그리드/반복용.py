06a6281fdc3acd67f970eb32307976cadba58a46
jung su young <j9972@naver.com>
2022-11-14 15:26:49 +0900

    마지막 \n 주의

--- 이코테/이코테그리드/반복용.py
+++ 이코테/이코테그리드/반복용.py
@@ -1,24 +1,16 @@
-# 모험가 길드
+# 곱하기 혹은 더하기
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-data = list(map(int, input().split()))
+n = input()
+s = list(n)
+print(s)
+res = 0
 
-data.sort()
-res = []
-totalCount = 0
+for i in range(len(s)-1):
+    if int(s[i]) == 0 or int(s[i]) == 1 or res == 0 or res == 1:
+        res += int(s[i])
+    else:
+        res *= int(s[i])
 
-for i in data:
-    res.append(i)
-    if i == res.count(i):
-        totalCount += 1
-
-
-print(totalCount)
-
-# for i in data:
-#     count += 1
-#     if count >= i:
-#         res += 1
-#         count = 0
+print(res)
