05c16c6794178ed558685295be5a16348977b7fe
jung su young <j9972@naver.com>
2022-11-14 14:47:27 +0900

    리팩토링

--- 이코테/이코테그리드/반복용.py
+++ 이코테/이코테그리드/반복용.py
@@ -8,18 +8,15 @@ data = list(map(int, input().split()))
 
 data.sort(reverse=True)
 
-res = 0
-count = 0
+ans = 0
 
-while True:
-    if m <= 0:
-        break
-    m -= 1
-    count += 1
-    if count <= k:
-        res += data[0]
-    else:
-        res += data[1]
-        count = 0
+# 나눠지는 경우 k+1은 수열의 길이, m을  k+1로 나눠지는 값은 덩어리의 개수, *k 를 하면서 총 큰값이 나오는 회수
+res = int(m/(k+1)) * k
 
-print(res)
+# 나눠지지 않는 경우
+res += m % (k+1)
+
+ans = res * data[0]
+ans += (m-res) * data[1]
+
+print(ans)
