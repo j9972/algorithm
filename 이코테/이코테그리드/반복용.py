65a2e936f06c2e6dba3f3d4c935bd08249d1ef96
jung su young <j9972@naver.com>
2022-11-14 14:58:24 +0900

    각 행에서 가장 큰 수 찾기

--- 이코테/이코테그리드/반복용.py
+++ 이코테/이코테그리드/반복용.py
@@ -1,22 +1,13 @@
-# 큰수의 법칙
+# 숫자 카드 게임
 import sys
 input = sys.stdin.readline
 
-n, m, k = map(int, input().split())
+n, m = map(int, input().split())
+data = []
+for i in range(n):
+    data.append(list(map(int, input().split())))
 
-data = list(map(int, input().split()))
-
-data.sort(reverse=True)
-
-ans = 0
-
-# 나눠지는 경우 k+1은 수열의 길이, m을  k+1로 나눠지는 값은 덩어리의 개수, *k 를 하면서 총 큰값이 나오는 회수
-res = int(m/(k+1)) * k
-
-# 나눠지지 않는 경우
-res += m % (k+1)
-
-ans = res * data[0]
-ans += (m-res) * data[1]
-
-print(ans)
+res = []
+for i in range(n):
+    res.append(min(data[i]))
+print(max(res))
