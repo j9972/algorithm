8f4f06452b1e9f69c7ffe3fc6917fa268d20461c
jung su young <j9972@naver.com>
2022-11-21 20:17:14 +0900

    바닥공사

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,17 +1,15 @@
-# 개미 전사
+# 바닥 공사
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-data = list(map(int, input().split()))
 
-d = [0] * 101
+d = [0] * 1000
 
-d[0] = data[0]
-d[1] = max(data[0], data[1])
+d[1] = 1
+d[2] = 3
 
+for i in range(3, n):
+    d[i] = ((d[i-2]*2) + d[i-1]) % 796796
 
-for i in range(2, n):
-    d[i] = max(d[i-1], d[i-2]+data[i])
-
-print(d[n-1])
+print(d[n])
