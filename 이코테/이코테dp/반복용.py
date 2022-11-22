258f0c2fc00440eaccb25586dabe7a45254f30a6
jung su young <j9972@naver.com>
2022-11-22 14:09:14 +0900

    이동 방향이랑 , 행/열애 집중하기

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -2,24 +2,25 @@
 import sys
 input = sys.stdin.readline
 
-
 for tc in range(int(input())):
     n, m = map(int, input().split())
-
     arr = list(map(int, input().split()))
-    dp = []
+
     index = 0
+    dp = []
     for i in range(n):
         dp.append(arr[index:index+m])
         index += m
 
     for j in range(1, m):
         for i in range(n):
+            # left up
             if i == 0:
                 left_up = 0
             else:
-                left_up = dp[i - 1][j - 1]
+                left_up = dp[i-1][j-1]
 
+            # left_down
             if i == n - 1:
                 left_down = 0
             else:
@@ -28,9 +29,7 @@ for tc in range(int(input())):
             left = dp[i][j - 1]
             dp[i][j] += max(left_up, left_down, left)
 
-    # print(dp[n-1][m-1])
-
-    result = 0
+    res = 0
     for i in dp:
-        result = max(result, max(i))
-    print(result)
+        res = max(res, max(i))
+    print(res)
