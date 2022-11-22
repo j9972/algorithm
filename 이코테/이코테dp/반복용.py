679bff70c03257113a4dfd826d97243be875497b
jung su young <j9972@naver.com>
2022-11-23 01:00:08 +0900

    LIS 를 이용해서 배치

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,35 +1,18 @@
-# 금광
+# 병사 배치하기
 import sys
 input = sys.stdin.readline
 
-for tc in range(int(input())):
-    n, m = map(int, input().split())
-    arr = list(map(int, input().split()))
+n = int(input())
+arr = list(map(int, input().split()))
 
-    index = 0
-    dp = []
-    for i in range(n):
-        dp.append(arr[index:index+m])
-        index += m
+arr.reverse()
 
-    for j in range(1, m):
-        for i in range(n):
-            # left up
-            if i == 0:
-                left_up = 0
-            else:
-                left_up = dp[i-1][j-1]
+# 1차원으로 리스트 초기화
+dp = [1] * n
 
-            # left_down
-            if i == n - 1:
-                left_down = 0
-            else:
-                left_down = dp[i + 1][j - 1]
+for i in range(1, n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j]+1)
 
-            left = dp[i][j - 1]
-            dp[i][j] += max(left_up, left_down, left)
-
-    res = 0
-    for i in dp:
-        res = max(res, max(i))
-    print(res)
+print(n-max(dp))
