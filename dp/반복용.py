8725e6eeb70381f352bdc6963f86c79470ca6fac
jung su young <j9972@naver.com>
2022-11-23 17:26:23 +0900

    dp 로 각자 쌓여가는 값중에 가장 작은값을 찾으면 된다

--- dp/반복용.py
+++ dp/반복용.py
@@ -1,24 +1,18 @@
-# 1010
-import math
+# 1149
 import sys
 input = sys.stdin.readline
 
+data = []
+n = int(input())
 
-def fact(n):
-    if n > 1:
-        return n * fact(n-1)
-    else:
-        return 1
+for i in range(n):
+    r, g, b = map(int, input().split())
+    data.append([r, g, b])
 
+# 1부터 시작한 이유는 2차원의 [0] 배열에 대한 최소 값을 갖기 위함 -> dp 방식으로 쌓여가는 것중에 가장 작은값출력
+for i in range(1, len(data)):
+    data[i][0] += min(data[i-1][1], data[i-1][2])
+    data[i][1] += min(data[i-1][0], data[i-1][2])
+    data[i][2] += min(data[i-1][1], data[i-1][0])
 
-for tc in range(int(input())):
-    n, m = map(int, input().split())
-
-    # up = math.factorial(m)
-    # down = math.factorial(m-n) * math.factorial(n)
-
-    up = fact(m)
-    down_first = fact(m-n)
-    down_second = fact(n)
-
-    print(up//(down_second*down_first))
+print(min(data[n-1][0], data[n-1][1], data[n-1][2]))
