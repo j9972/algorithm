34f981c150eb9aa6a4038c938c546aec5400718f
jung su young <j9972@naver.com>
2022-11-25 17:37:11 +0900

    useig heapq

--- 이코테/이코테그리드/반복용.py
+++ 이코테/이코테그리드/반복용.py
@@ -1,15 +1,4 @@
-# 만들 수  없는 금액
+# 무지의 먹방 라이브
 import heapq
 import sys
 input = sys.stdin.readline
-
-n = int(input())
-data = list(map(int, input().split()))
-data.sort()
-
-t = 1
-for i in data:
-    if t < i:
-        break
-    t += i
-print(t)
