3230f420d458b3cd902ec9d2b6464ba26d942171
jung su young <j9972@naver.com>
2022-11-25 16:01:53 +0900

    ez

--- 이코테/이코테그리드/반복용.py
+++ 이코테/이코테그리드/반복용.py
@@ -1,16 +1,25 @@
-# 무지의 먹방 라이브
+# 큰 수의 법칙
 import heapq
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
 
-res = list(combinations(data, 2))
+data.sort(reverse=True)
+first = data[0]
+second = data[1]
 
-count = 0
-for i in res:
-    if i[0] == i[1]:
-        count += 1
-
-print(len(res) - count)
+ans = 0
+while True:
+    if m == 0:
+        print(ans)
+        break
+    for i in range(k):
+        ans += first
+        m -= 1
+        if m <= 0:
+            print(ans)
+            break
+    ans += second
+    m -= 1
