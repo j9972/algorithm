140d167a4adf63f6167878e14f6de39a499d949e
jung su young <j9972@naver.com>
2022-11-25 15:15:34 +0900

    편집거리

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,25 +1,31 @@
-# 못생긴 수
+# 편집 거리
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+str1 = input()
+str2 = input()
 
-i2 = i3 = i5 = 0
-next_i2, next_i3, next_i5 = 2, 3, 5
 
-dp = [0] * n
-dp[0] = 1
+def edit(str1, str2):
+    n = len(str1)
+    m = len(str2)
 
-for i in range(1, n):
-    dp[i] = min(next_i2, next_i3, next_i5)
-    if dp[i] == next_i2:
-        i2 += 1
-        next_i2 = dp[i2] * 2
-    if dp[i] == next_i3:
-        i3 += 1
-        next_i3 = dp[i3] * 3
-    if dp[i] == next_i5:
-        i5 += 1
-        next_i5 = dp[i5] * 5
+    dp = [[0] * (m+1) for _ in range(n+1)]
 
-print(dp[n-1])
+    for i in range(1, n+1):
+        dp[i][0] = i
+
+    for j in range(1, m+1):
+        dp[0][j] = j
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
+
+    return dp[n-1][m-1]
+
+
+print(edit(str1, str2))
