bda601349b956f8fba61a1a739a844975a537ab2
jung su young <j9972@naver.com>
2022-11-25 16:20:20 +0900

    count check well

--- 이코테/이코테그리드/반복용.py
+++ 이코테/이코테그리드/반복용.py
@@ -1,18 +1,19 @@
-# 1이 될때까지
+# 모험가 길드
 import heapq
 import sys
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
+n = int(input())
+data = list(map(int, input().split()))
 
-count = 0
-while True:
-    if n == 1:
-        print(count)
-        break
+data.sort()
 
-    if n % k == 0:
-        n //= k
-    else:
-        n -= 1
-    count += 1
+g = 0
+c = 0
+for i in data:
+    c += 1
+    if c >= i:
+        g += 1
+        c = 0
+
+print(g)
