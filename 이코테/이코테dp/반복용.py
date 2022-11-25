2cd74f0d7eacdbea2e565479b28031db6413a68f
jung su young <j9972@naver.com>
2022-11-25 10:56:14 +0900

    배치

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,25 +1,17 @@
-# 금광
+# 병사 배치
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 
-dp = []
-for i in range(n):
-    dp.append(list(map(int, input().split())))
+arr = list(map(int, input().split()))
+arr.reverse()
 
+# LIS
+dp = [1]*n
 for i in range(1, n):
     for j in range(i):
-        if i == 0:
-            left = 0
-        else:
-            left = dp[i-1][j-1]
+        if arr[j] < arr[i]:  # j가 i보다 앞임
+            dp[i] += max(dp[i], dp[j]+1)
 
-        if i == j:
-            up = 0
-        else:
-            up = dp[i-1][j]
-
-        dp[i][j] += max(left, up)
-
-print(max(dp[n-1]))
+print(n-max(dp))
