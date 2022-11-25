9662512fa53b14315d799ef2e1768ede084937ef
jung su young <j9972@naver.com>
2022-11-25 16:28:03 +0900

    뒤집어 첫번째 원소 기준으로 다음 원소와 값이 다를때 다른 값을 뒤집어

--- 이코테/이코테그리드/반복용.py
+++ 이코테/이코테그리드/반복용.py
@@ -1,15 +1,22 @@
-# 곱하기 혹은 더하기
+# 문자열 뒤집기
 import heapq
 import sys
 input = sys.stdin.readline
 
 s = input()
 
-count = 0
-for i in range(len(s)-1):
-    if s[i] == '0' or count == 0 or s[i] == '1':
-        count += int(s[i])
-    else:
-        count *= int(s[i])
+zero = one = 0
 
-print(count)
+if s[0] == '0':
+    one += 1
+else:
+    zero += 1
+
+for i in range(len(s) - 1):
+    if s[i] == s[i+1]:
+        if s[i] == '0':
+            one += 1
+        else:
+            zero += 1
+
+print(min(one, zero))
