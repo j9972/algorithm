afe898414379f4773476489f12c78cb9865d6c74
jung su young <j9972@naver.com>
2022-11-29 20:26:39 +0900

    위상 정렬 사용한 DP

--- dp/1516.py
+++ dp/1516.py
@@ -1,36 +1,35 @@
-from collections import deque
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-
-seq = [[] for _ in range(n+1)]
-inDegree = [0 for _ in range(n+1)]
-dp = [0 for _ in range(n+1)]
-cost = [0 for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+inDegree = [0] * (n+1)
+cost = [0] * (n+1)
+dp = [0] * (n+1)
 
 for i in range(1, n+1):
     tmp = list(map(int, input().split()))[:-1]
     cost[i] = tmp[0]
     for j in tmp[1:]:
-        seq[j].append(i)
+        graph[j].append(i)
         inDegree[i] += 1
 
 q = deque()
 
 for i in range(1, n+1):
-    if inDegree[i] == 0:
+    if not inDegree[i]:
         q.append(i)
         dp[i] = cost[i]
 
 while q:
     n = q.popleft()
 
-    for i in seq[n]:
+    for i in graph[n]:
         inDegree[i] -= 1
         dp[i] = max(dp[i], dp[n] + cost[i])
 
-        if inDegree[i] == 0:
+        if not inDegree[i]:
             q.append(i)
 
 for i in range(1, n+1):
