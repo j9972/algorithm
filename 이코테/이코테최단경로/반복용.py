e115291f6a940e88cef2bca8ac69f56d0a8f1606
jung su young <j9972@naver.com>
2022-11-30 18:10:14 +0900

    개선된 우선순위 큐

--- 이코테/이코테최단경로/반복용.py
+++ 이코테/이코테최단경로/반복용.py
@@ -1,6 +1,7 @@
 # 전보
 import heapq
 import sys
+from turtle import distance
 input = sys.stdin.readline
 
 dx = [-1, 0, 1, 0]
@@ -8,28 +9,31 @@ dy = [0, 1, 0, -1]
 
 INF = int(1e9)
 
-
-n, m = map(int, input().split())
+n, m, start = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
 distance = [INF] * (n+1)
-start = 1
 
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a].append((b, 1))
-    graph[b].append((a, 1))
+# for a in range(1, n+1):
+#     for b in range(1, n+1):
+#         if a == b:
+#             graph[a][b] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
 
 
-def dij(start):
+def dik(start):
     q = []
+
     heapq.heappush(q, (0, start))
     distance[start] = 0
 
     while q:
         dist, now = heapq.heappop(q)
 
-        if dist > distance[now]:
+        if distance[now] < dist:
             continue
         for i in graph[now]:
             cost = dist + i[1]
@@ -38,19 +42,14 @@ def dij(start):
                 heapq.heappush(q, (cost, i[0]))
 
 
-dij(start)
+dik(start)
 
-maxNode = 0
+count = 0
 maxDistance = 0
 
-res = []
 
-for i in range(1, n+1):
-    if maxDistance < distance[i]:
-        maxNode = i
-        maxDistance = distance[i]
-        res = [maxNode]
-    elif maxDistance == distance[i]:
-        res.append(i)
-
-print(maxNode, maxDistance, len(res))
+for d in distance:
+    if d != INF:
+        count += 1
+        maxDistance = max(d, maxDistance)
+print(count - 1, maxDistance)
