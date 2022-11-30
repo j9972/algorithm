c5c1bf3d559f502c824ffbf0037ebac9cf2b3233
jung su young <j9972@naver.com>
2022-11-30 22:10:07 +0900

    성적 비교

--- 이코테/이코테최단경로/반복용.py
+++ 이코테/이코테최단경로/반복용.py
@@ -3,36 +3,31 @@ import heapq
 import sys
 input = sys.stdin.readline
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+n, m = map(int, input().split())
 
 INF = int(1e9)
-n = int(input())
-m = int(input())
-
-data = [[] for _ in range(n+1)]
-distance = [INF] * (n+1)
+data = [[INF] * (n+1) for _ in range(n+1)]
 
 for i in range(m):
-    a, b, c = map(int, input().split())
-    if c < data[a][b]:
-        data[a][b] = c
+    a, b = map(int, input().split())
+    data[a][b] = 1
 
 for i in range(1, n+1):
     for j in range(1, n+1):
         if i == j:
             data[i][j] = 0
 
-
 for k in range(1, n+1):
     for i in range(1, n+1):
         for j in range(1, n+1):
             data[i][j] = min(data[i][j], data[i][k]+data[k][j])
 
+res = 0
 for i in range(1, n+1):
+    count = 0
     for j in range(1, n+1):
-        if data[i][j] == INF:
-            print(0, end=' ')
-        else:
-            print(data[i][j], end=' ')
-    print()
+        if data[i][j] != INF or data[j][i] != INF:
+            count += 1
+    if count == n:
+        res += 1
+print(res)
