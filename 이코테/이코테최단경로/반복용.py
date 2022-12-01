b2bcd2ed84328bf39a5d63da4668fe08584b30a2
jung su young <j9972@naver.com>
2022-12-01 22:26:44 +0900

    플로이드

--- 이코테/이코테최단경로/반복용.py
+++ 이코테/이코테최단경로/반복용.py
@@ -1,6 +1,7 @@
 # 플로이드 - 플로이드 사용
 import heapq
 import sys
+from turtle import distance
 input = sys.stdin.readline
 
 dx = [-1, 1, 0, 0]
@@ -8,39 +9,28 @@ dy = [0, 0, -1, 1]
 
 INF = int(1e9)
 
-n, m, start = map(int, input().split())
+n = int(input())
+m = int(input())
 
-graph = [[] for _ in range(n+1)]
-distance = [INF] * (n+1)
+graph = [[INF] * (n+1) for _ in range(n+1)]
 
 for i in range(m):
     a, b, c = map(int, input().split())
-    graph[a].append((b, c))
+    if c < graph[a][b]:
+        graph[a][b] = c
 
+for i in range(1, n+1):
+    graph[i][i] = 0
 
-def dik(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
 
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-
-
-dik(start)
-
-count = 0
-maxDis = 0
-
-for d in distance:
-    if d != INF:
-        count += 1
-        maxDis = max(maxDis, d)
-print(count - 1, maxDis)
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] != INF:
+            print(graph[i][j], end=' ')
+        else:
+            print(0, end=' ')
+    print()
