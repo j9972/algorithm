172e5fe0374a2215fd83c83abf16474cad0da89c
jung su young <j9972@naver.com>
2022-12-01 21:56:46 +0900

    이코테 복습

--- 이코테/이코테최단경로/반복용.py
+++ 이코테/이코테최단경로/반복용.py
@@ -8,29 +8,39 @@ dy = [0, 0, -1, 1]
 
 INF = int(1e9)
 
-n, m = map(int, input().split())
+n, m, start = map(int, input().split())
 
-graph = [[INF] * (n+1) for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
 distance = [INF] * (n+1)
 
 for i in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
 
-x, k = map(int, input().split())
 
-for i in range(1, n+1):
-    graph[i][i] = 0
+def dik(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
 
-for k in range(1, n+1):
-    for a in range(1, n+1):
-        for b in range(1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-distance = graph[1][k] + graph[k][x]
 
-if distance >= INF:
-    print("-1")
-else:
-    print(distance)
+dik(start)
+
+count = 0
+maxDis = 0
+
+for d in distance:
+    if d != INF:
+        count += 1
+        maxDis = max(maxDis, d)
+print(count - 1, maxDis)
