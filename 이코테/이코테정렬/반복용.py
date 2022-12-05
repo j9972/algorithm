2112e5cb78a8d8e565d92c77b832a206dd357f60
jung su young <j9972@naver.com>
2022-12-05 19:24:59 +0900

    중간에 값을 다시 사용해야 하는 경우는는 heap 을 쓰는게 좋다

--- 이코테/이코테정렬/반복용.py
+++ 이코테/이코테정렬/반복용.py
@@ -1,14 +1,20 @@
-def solution(N, stages):
-    answer = []
-    stage = 1
-    fail = [0] * (N+1)
+import heapq
+import sys
+input = sys.stdin.readline
 
-    for i in range(1, N+1):  # 1부터인 이유는 stages의 최솟값이 1
-        nopeClear = stages.count(i)  # stages 의 값들의 갯수
-        reach = [i for i in stages if i >= stage]
-        fail = nopeClear / len(reach) if len(reach) != 0 else 0
-        stage += 1
-        answer.append((i, fail))
+n = int(input())
+heap = []
 
-    answer.sort(key=lambda x: -x[1])
-    return [i[0] for i in answer]
+for i in range(n):
+    data = int(input())
+    heapq.heappush(heap, data)
+
+res = 0
+
+while len(heap) != 1:
+    first = heapq.heappop(heap)
+    second = heapq.heappop(heap)
+    sumV = first + second
+    res += sumV
+    heapq.heappush(heap, sumV)
+print(res)
