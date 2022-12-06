0024a41c41c0e13384c139b2c9ee0d1edd1f1323
jung su young <j9972@naver.com>
2022-12-06 11:09:41 +0900

    부품 찾기

--- 이코테/이코테이진탐색/반복용.py
+++ 이코테/이코테이진탐색/반복용.py
@@ -1,34 +1,31 @@
-# 가사 검색
+# 부품 찾기
 from bisect import bisect_left, bisect_right
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+data_n = list(map(int, input().split()))
+m = int(input())
+data_m = list(map(int, input().split()))
+
+data_n.sort()
 
 
-def count(a, left, right):
-    leftIdx = bisect_left(a, left)
-    rightIdx = bisect_right(a, right)
-    return rightIdx - leftIdx
-
-
-def solution(words, queries):
-    answer = []
-
-    array = [[] for _ in range(10001)]
-    reverArray = [[] for _ in range(10001)]
-
-    for word in words:
-        array[len(word)].append(word)
-        reverArray[len(word)].append(word[::-1])
-
-    for i in range(10001):
-        array[i].sort()
-        reverArray[i].sort()
-
-    for q in queries:
-        if q[0] != '?':
-            res = count(array[len(q)], q.replace(
-                '?', 'a'), q.replace('?', 'z'))
+def binary(arr, tar, s, e):
+    while s <= e:
+        m = (s+e) // 2
+        if arr[m] == tar:
+            return m
+        elif arr[m] > tar:
+            e = m - 1
         else:
-            res = count(
-                reverArray[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
-        answer.append(res)
+            s = m + 1
+    return None
 
-    return answer
+
+for i in data_m:
+    res = binary(data_n, i, 0, n-1)
+    if res != None:
+        print("yes", end=' ')
+    else:
+        print("no", end=' ')
