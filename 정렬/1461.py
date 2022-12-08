0ef96072b2068d78c1829dbd3fb92380d747d8e4
jung su young <j9972@naver.com>
2022-12-08 17:12:53 +0900

    간단하게 생각해야하고, m 씩 묶을때는 for문의 마지막 자리를 사용하느 것을 잊지말자

---
+++ 정렬/1461.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n, gap = map(int, input().split())
+data = list(map(int, input().split()))
+
+p = []
+m = []
+res = []
+mv = 0
+
+for d in data:
+    if d > 0:
+        p.append(d)
+    else:
+        m.append(d)
+
+    if abs(d) > abs(mv):
+        mv = d
+
+p.sort(reverse=True)
+m.sort()
+
+for i in range(0, len(p), gap):
+    if p[i] != mv:
+        res.append(p[i])
+for i in range(0, len(m), gap):
+    if m[i] != mv:
+        res.append(m[i])
+
+cnt = abs(mv)
+for r in res:
+    cnt += abs(r) * 2
+print(cnt)
