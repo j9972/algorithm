1a5ec08cd77169f0ef66c6663a0093f63e0b66c6
jung su young <j9972@naver.com>
2022-12-11 15:39:47 +0900

    실패율

--- 이코테/이코테정렬/반복용.py
+++ 이코테/이코테정렬/반복용.py
@@ -1,10 +1,14 @@
-# 안테나
-import sys
-input = sys.stdin.readline
+def solution(N, stages):
+    answer = []
+    fail = [0] * (N+1)
+    stage = 1
 
-n = int(input())
-data = list(map(int, input().split()))
+    for i in range(1, N+1):
+        notClear = stages.count(i)
+        reach = [i for i in stages if i >= stage]
+        fail = notClear / len(reach) if len(reach) != 0 else 0
+        stage += 1
+        answer.append((i, fail))
 
-data.sort()
-
-print(data[(n-1) // 2])
+    answer.sort(key=lambda x: -x[1])
+    return [i[0] for i in answer]
