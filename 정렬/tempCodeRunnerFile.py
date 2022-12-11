f41a528f0c710e49bce2a2e5a73ae55a1e1df819
jung su young <j9972@naver.com>
2022-12-11 17:42:43 +0900

    시작점이랑 중간값을 합쳐진값을 세트로 보고 이게 투포인터고 그리고 나서 이분탐색을 사용하면 된다

--- 정렬/tempCodeRunnerFile.py
+++ 정렬/tempCodeRunnerFile.py
@@ -1,4 +1,4 @@
-# 세 용액
+# #  세 용액
 import sys
 input = sys.stdin.readline
 
@@ -8,16 +8,16 @@ data.sort()
 
 # 절댓값은 작을수록 0에 가깝다는것을 이용해서 문제 풀기
 # 투포인터 사용하기
+s = 0
+e = n-1
 res = 3e9 + 1
 ans = [0, 0, 0]
 
 for i in range(n-2):
-    s = i+1
-    e = n-1
     while s < e:
         sumValue = data[s] + data[e] + data[i]
 
-        if abs(sumValue) <= res:
+        if abs(sumValue) < res:
             res = min(res, abs(sumValue))
             ans = [data[s], data[i], data[e]]
 
@@ -27,6 +27,24 @@ for i in range(n-2):
             s += 1
         elif sumValue > 0:
             e -= 1
+# while s < e:
+#     m = (s+e) // 2
+#     if s == m or e == m or s == e:
+#         break
+
+#     sumValue = data[s] + data[e] + data[m]
+
+#     if abs(sumValue) < res:
+#         res = min(res, abs(sumValue))
+#         ans = [data[s], data[m], data[e]]
+
+#         if res == 0:
+#             break
+
+#     if sumValue < 0:
+#         s += 1
+#     elif sumValue > 0:
+#         e -= 1
 
 ans.sort()
 print(ans[0], ans[1], ans[2])
