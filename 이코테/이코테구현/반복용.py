67192488096f8919cf3081a95d74ba3468c4e053
jung su young <j9972@naver.com>
2022-12-13 11:25:13 +0900

    반반 치킨으로 나누기

--- 이코테/이코테구현/반복용.py
+++ 이코테/이코테구현/반복용.py
@@ -1,53 +1,17 @@
-# 게임 개발
+# 럭키
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-x, y, d = map(int, input().split())
+n = input().rstrip()
 
-data = []
-for i in range(n):
-    data.append(list(map(int, input().split())))
+cnt = 0
+for i in range(0, len(n)//2):
+    cnt += int(n[i])
+val = 0
+for i in range(len(n)//2, len(n)):
+    val += int(n[i])
 
-visited = [[0] * m for _ in range(n)]
-visited[x][y] = 1
-
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-
-def turing():
-    global d
-    d -= 1
-    if d == -1:
-        d = 3
-
-
-cnt = 1
-turingPoint = 0
-
-while True:
-    turing()
-    nx = x + dx[d]
-    ny = y + dy[d]
-
-    if visited[nx][ny] == 0 and data[nx][ny] == 0:  # data의 값을 변동시키지 않는 이유는 돌아갈수 있기 때문임
-        visited[nx][ny] = 1  # 방문 처리
-        x, y = nx, ny  # 이동
-        cnt += 1
-        turingPoint = 0
-        continue
-    else:
-        turingPoint += 1
-
-    if turingPoint == 4:
-        nx = x - dx[d]
-        ny = y - dy[d]
-
-        if data[nx][ny] == 0:
-            x, y = nx, ny
-        else:
-            break
-        turingPoint = 0
-
-print(cnt)
+if cnt == val:
+    print('LUCKY')
+else:
+    print('READY')
