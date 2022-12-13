e68f2315623c0ab62c3e9af37948c2c5679d5294
jung su young <j9972@naver.com>
2022-12-13 10:08:28 +0900

    상하좌우

--- 이코테/이코테구현/반복용.py
+++ 이코테/이코테구현/반복용.py
@@ -3,10 +3,23 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-data = [[0] * (n+1) for _ in range(n+1)]
-
 direction = list(map(str, input().split()))
 
+step = ['L', 'R', 'U', 'D']
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
 x, y = 1, 1
-for i in range(len(direction)):
-    data[x][y] = 1
+
+for d in direction:
+    for i in range(len(step)):
+        if d == step[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+    if 1 > nx or nx > n or 1 > ny or ny > n:
+        continue
+    x, y = nx, ny
+
+
+print(x, y)
