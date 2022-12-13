7503008e477dc5facd93d6ad3abe4d9c6bc726f9
jung su young <j9972@naver.com>
2022-12-13 10:21:10 +0900

    n**3 이라서 조심해야한다

--- 이코테/이코테구현/반복용.py
+++ 이코테/이코테구현/반복용.py
@@ -3,23 +3,11 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-direction = list(map(str, input().split()))
+count = 0
 
-step = ['L', 'R', 'U', 'D']
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-
-x, y = 1, 1
-
-for d in direction:
-    for i in range(len(step)):
-        if d == step[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-    if 1 > nx or nx > n or 1 > ny or ny > n:
-        continue
-    x, y = nx, ny
-
-
-print(x, y)
+for i in range(n+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(k) + str(j):
+                count += 1
+print(count)
