c16706a7d4adbeb76d826815ce6e31e62f6a4363
jung su young <j9972@naver.com>
2022-12-14 16:58:35 +0900

    press the sentense -> 규칙을 찾아야 해서 deque, heap 을 고민했는데, 간격을 기준으로 해서 접근해도 된다는점을 배움

--- 이코테/이코테구현/반복용.py
+++ 이코테/이코테구현/반복용.py
@@ -4,14 +4,25 @@ input = sys.stdin.readline
 
 s = input().rstrip()
 
-cnt = 0
-ans = []
-for i in range(len(s)):
-    if s[i].isalpha():
-        ans.append(s[i])
-    else:
-        cnt += int(s[i])
-ans.sort()
-ans.append(str(cnt))
 
-print(''.join(ans))
+def solution(s):
+    res = len(s)
+
+    for step in range(1, len(s)//2 + 1):
+        cnt = 1
+        ans = ""
+        prev = s[0:step]
+
+        for j in range(step, len(s), step):
+            if prev == s[j:j+step]:
+                cnt += 1
+            else:
+                ans += str(cnt) + prev if cnt >= 2 else prev
+                cnt = 1
+                prev = s[j:j+step]
+        ans += str(cnt) + prev if cnt >= 2 else prev
+        res = min(len(ans), res)
+    return res
+
+
+print(solution(s))
