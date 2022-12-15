f747311ce2486c5f6246c6ac36eb95ab409e0249
jung su young <j9972@naver.com>
2022-12-15 17:08:32 +0900

    원형 ( 시작과 끝이 같다면 ) 직선으로 풀자

--- 이코테/이코테구현/반복용.py
+++ 이코테/이코테구현/반복용.py
@@ -1,32 +1,29 @@
-# 치킨 배달
-from itertools import combinations
-import sys
-input = sys.stdin.readline
+from itertools import permutations
 
-n, m = map(int, input().split())
 
-data = []
-for i in range(n):
-    data.append(list(map(int, input().split())))
+def solution(n, weak, dist):
 
-home = []
-chicken = []
+    dist.sort(reverse=True)
+    length = len(weak)
+    ans = len(dist) + 1
 
-for i in range(n):
-    for j in range(n):
-        if data[i][j] == 2:
-            chicken.append([i, j])
-        elif data[i][j] == 1:
-            home.append([i, j])
+    for i in range(length):
+        weak.append(weak[i]+n)
 
-res = 1e9
+    for start in range(length):
+        for friends in list(permutations(dist, len(dist))):
+            cnt = 1
+            position = weak[start] + friends[cnt - 1]
 
-for ch in combinations(chicken, m):
-    temp = 0
-    for h in home:
-        ch_len = 999
-        for j in range(m):
-            ch_len = min(ch_len, abs(h[0]-ch[j][0]) + abs(h[1]-ch[j][1]))
-        temp += ch_len
-    res = min(res, temp)
-print(res)
+            for index in range(start, start+length):
+                if weak[index] > position:
+                    cnt += 1
+                    if cnt > len(dist):
+                        break
+                    position = weak[index] + friends[cnt - 1]
+
+            ans = min(ans, cnt)
+    if ans > len(dist):
+        return -1
+
+    return ans
