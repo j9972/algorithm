304c1ec99f3799049fca3a4cecaad79a923d28f4
jung su young <j9972@naver.com>
2022-12-22 14:38:46 +0900

    전형적인 신장트리

---
+++ 이코테/이코테그래프/어두운길.py
@@ -0,0 +1,45 @@
+# 신장트리
+from re import I
+import sys
+input = sys.stdin.readline
+
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+edge = []
+parent = [0] * (n+1)
+res = 0
+
+for i in range(1, n+1):
+    parent[i] = i
+
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    edge.append((cost, a, b))
+
+edge.sort()  # 비용순으로 정렬
+tot = 0
+
+for e in edge:  # 간선을 1개씩 확인
+    cost, a, b = e
+    tot += cost
+    if find(parent, a) != find(parent, b):
+        union(parent, a, b)
+        res += cost
+
+print(tot-res)
