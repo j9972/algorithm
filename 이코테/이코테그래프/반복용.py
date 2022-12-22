194e440aaeebdc0b950bac6fafff66c62bea6550
jung su young <j9972@naver.com>
2022-12-22 17:37:41 +0900

    위상정렬

--- 이코테/이코테그래프/반복용.py
+++ 이코테/이코테그래프/반복용.py
@@ -1,44 +1,41 @@
-# 도시분할 -> 신장트리
+# 커리큘럼 -> 위상정렬
+from collections import deque
+import copy
 import sys
 input = sys.stdin.readline
 
-
-def find(parent, x):
-    if parent[x] != x:
-        parent[x] = find(parent, parent[x])
-    return parent[x]
-
-
-def union(parent, a, b):
-    a = find(parent, a)
-    b = find(parent, b)
-
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
-
-n, m = map(int, input().split())
-parent = [0] * (n+1)
+n = int(input())
+inDegree = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
+time = [0] * (n+1)
 
 for i in range(1, n+1):
-    parent[i] = i
+    data = list(map(int, input().split()))
+    time[i] = data[0]
+    for x in data[1:-1]:
+        inDegree[i] += 1
+        graph[x].append(i)
 
-edges = []
-res = 0
-last = 0  # 가장 큰 비용
+
+def topo():
+    res = copy.deepcopy(time)
+    q = deque()
+
+    for i in range(1, n+1):
+        if inDegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            res[i] = max(res[i], res[now] + time[i])
+            inDegree[i] -= 1
+
+            if inDegree[i] == 0:
+                q.append(i)
+
+    for i in range(1, n+1):
+        print(res[i])
 
 
-for i in range(m):
-    a, b, cost = map(int, input().split())
-    edges.append((cost, a, b))
-edges.sort()
-
-for e in edges:
-    cost, a, b = e
-    if find(parent, a) != find(parent, b):
-        union(parent, a, b)
-        res += cost
-        last = cost
-print(res-last)
+topo()
