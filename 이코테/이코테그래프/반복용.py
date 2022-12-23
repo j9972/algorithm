f38028d0ed2eb24b97f05f17df297949762ce9c6
jung su young <j9972@naver.com>
2022-12-23 13:47:07 +0900

    신장트리 & 서로소

--- 이코테/이코테그래프/반복용.py
+++ 이코테/이코테그래프/반복용.py
@@ -1,41 +1,42 @@
-# 커리큘럼 -> 위상정렬
-from collections import deque
-import copy
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-inDegree = [0] * (n+1)
-graph = [[] for _ in range(n+1)]
-time = [0] * (n+1)
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+parent = [0] * (n+1)
+edges = []
+res = 0
+last = 0  # 비용이 가장 큰 도로 ( 두개의 도시로 분리하는데 있어서 최소비용을 만들어야 하므로 )
 
 for i in range(1, n+1):
-    data = list(map(int, input().split()))
-    time[i] = data[0]
-    for x in data[1:-1]:
-        inDegree[i] += 1
-        graph[x].append(i)
+    parent[i] = i
 
+for i in range(m):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+edges.sort()
 
-def topo():
-    res = copy.deepcopy(time)
-    q = deque()
+for e in edges:
+    cost, a, b = e
+    # 사이클 확인
+    if find(parent, a) != find(parent, b):
+        union(parent, a, b)
+        res += cost
+        last = cost
 
-    for i in range(1, n+1):
-        if inDegree[i] == 0:
-            q.append(i)
-
-    while q:
-        now = q.popleft()
-        for i in graph[now]:
-            res[i] = max(res[i], res[now] + time[i])
-            inDegree[i] -= 1
-
-            if inDegree[i] == 0:
-                q.append(i)
-
-    for i in range(1, n+1):
-        print(res[i])
-
-
-topo()
+print(res-last)
