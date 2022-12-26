a79e3c431a30da75008f0d45af442399277ac9b6
jung su young <j9972@naver.com>
2022-12-26 14:37:48 +0900

    특정거리에 접근

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -2,29 +2,34 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-data = []
-for i in range(n):
-    data.append(list(map(int, input().rstrip())))
+n, m, k, x = map(int, input().split())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+distance = [-1] * (n+1)
+distance[x] = 0
 
-def bfs(x, y):
-    q = deque()
-    q.append((x, y))
+data = [[] for i in range(n+1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    data[a].append(b)
 
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+q = deque([x])
 
-            if 0 <= nx < n and 0 <= ny < m and data[nx][ny] == 1:
-                data[nx][ny] = data[x][y] + 1
-                q.append((nx, ny))
-    return data[n-1][m-1] 
+while q:
+    now = q.popleft()
 
+    for next in data[now]:
+        if distance[next] == -1:
+            distance[next] = distance[now] + 1
+            q.append(next)
 
-print(bfs(0, 0))
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if check == False:
+    print(-1)
