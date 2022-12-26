5cc7a5c79b41c647e060f0628c0370d36ba66610
jung su young <j9972@naver.com>
2022-12-26 13:57:33 +0900

    음료수 얼려먹기

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,56 +1,26 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
 
-n, l, r = map(int, input().split())
-
+n, m = map(int, input().split())
 data = []
 for i in range(n):
-    data.append(list(map(int, input().split())))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+    data.append(list(map(int, input().rstrip())))
 
 
-def bfs(x, y, index):
-    united = []
-    united.append((x, y))
-
-    q = deque()
-    q.append((x, y))
-
-    union[x][y] = index
-    summary = data[x][y]
-    count = 1
-
-    while q:
-        x, y = q.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
-                if l <= abs(data[nx][ny] - data[x][y]) <= r:
-                    union[nx][ny] = index
-                    summary += data[nx][ny]
-                    count += 1
-                    united.append((nx, ny))
-                    q.append((nx, ny))
-    for x, y in united:
-        data[x][y] = summary // count
-    return count
+def dfs(x, y):
+    if 0 <= x < n and 0 <= y < m and data[x][y] == 0:
+        data[x][y] = 1
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    return False
 
 
-tot = 0
-while True:
-    union = [[-1] * (n+1) for _ in range(n+1)]
-    index = 0
-    for i in range(n):
-        for j in range(n):
-            if union[i][j] == -1:
-                bfs(i, j, index)
-                index += 1
-    if index == n*n:
-        break
-    tot += 1
-
-print(tot)
+res = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            res += 1
+print(res)
