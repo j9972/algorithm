db73bb4f5b4cdd2664c0025d3cdbc4edf4f59211
jung su young <j9972@naver.com>
2022-12-26 15:24:07 +0900

    경쟁적전염

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -2,62 +2,36 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n, k = map(int, input().split())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+
+virusData = []
+
 data = []
 for i in range(n):
     data.append(list(map(int, input().split())))
-res = 0
-temp = [[0] * (m+1) for _ in range(n+1)]
+    for j in range(n):
+        if data[i][j] != 0:
+            virusData.append((data[i][j], 0, i, j))
+virusData.sort()
+q = deque(virusData)
 
+tar_s, tar_x, tar_y = map(int, input().split())
 
-def virus(x, y):
+while q:
+    virus, s, x, y = q.popleft()
+
+    if s == tar_s:
+        break
+
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-
-        if 0 <= nx < n and 0 <= ny < m:
-            if temp[nx][ny] == 0:
-                temp[nx][ny] = 2
-                virus(nx, ny)
-
-
-def score():
-    score = 0
-    for i in range(n):
-        for j in range(m):
-            if temp[i][j] == 0:
-                score += 1
-    return score
-
-
-def dfs(count):
-    global res
-    if count == 3:
-        for i in range(n):
-            for j in range(m):
-                temp[i][j] = data[i][j]
-
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 2:
-                    virus(i, j)
-
-        res = max(res, score())
-        return
-
-    for i in range(n):
-        for j in range(m):
-            if data[i][j] == 0:
-                data[i][j] = 1
-                count += 1
-                dfs(count)
-                data[i][j] = 0
-                count -= 1
-
-
-dfs(0)
-print(res)
+        if 0 <= nx < n and 0 <= ny < n:
+            if data[nx][ny] == 0:
+                data[nx][ny] = virus
+                q.append((virus, s+1, nx, ny))
+print(data[tar_x-1][tar_y-1])
