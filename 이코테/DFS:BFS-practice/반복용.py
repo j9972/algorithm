3e4ee964818e19017847c1f4ff8d9fcfaf3615c6
jung su young <j9972@naver.com>
2022-12-26 15:01:28 +0900

    연구소

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -2,34 +2,62 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-n, m, k, x = map(int, input().split())
+n, m = map(int, input().split())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-distance = [-1] * (n+1)
-distance[x] = 0
+data = []
+for i in range(n):
+    data.append(list(map(int, input().split())))
+res = 0
+temp = [[0] * (m+1) for _ in range(n+1)]
 
-data = [[] for i in range(n+1)]
-for i in range(m):
-    a, b = map(int, input().split())
-    data[a].append(b)
 
-q = deque([x])
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-while q:
-    now = q.popleft()
+        if 0 <= nx < n and 0 <= ny < m:
+            if temp[nx][ny] == 0:
+                temp[nx][ny] = 2
+                virus(nx, ny)
 
-    for next in data[now]:
-        if distance[next] == -1:
-            distance[next] = distance[now] + 1
-            q.append(next)
 
-check = False
-for i in range(1, n+1):
-    if distance[i] == k:
-        print(i)
-        check = True
+def score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score += 1
+    return score
 
-if check == False:
-    print(-1)
+
+def dfs(count):
+    global res
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = data[i][j]
+
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i, j)
+
+        res = max(res, score())
+        return
+
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 0:
+                data[i][j] = 1
+                count += 1
+                dfs(count)
+                data[i][j] = 0
+                count -= 1
+
+
+dfs(0)
+print(res)
