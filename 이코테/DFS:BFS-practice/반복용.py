e03317f895b9f729131e237468eec700dff11986
jung su young <j9972@naver.com>
2022-12-27 20:13:34 +0900

    블록이동하기

--- 이코테/DFS:BFS-practice/반복용.py
+++ 이코테/DFS:BFS-practice/반복용.py
@@ -1,58 +1,64 @@
 from collections import deque
-from itertools import combinations as cb
 import sys
 input = sys.stdin.readline
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-n, l, r = map(int, input().split())
 
-data = []
-for i in range(n):
-    data.append(list(map(int, input().split())))
+def next_step(pos, board):
+    pos = list(pos)
+    posList = []
 
+    pos_x1 = pos[0][0]
+    pos_y1 = pos[0][1]
+    pos_x2 = pos[1][0]
+    pos_y2 = pos[1][1]
 
-def bfs(x, y, index):
-    q = deque()
-    q.append((x, y))
+    for i in range(4):
+        next_pos_x1 = pos_x1 + dx[i]
+        next_pos_y1 = pos_y1 + dy[i]
+        next_pos_x2 = pos_x2 + dx[i]
+        next_pos_y2 = pos_y2 + dy[i]
 
-    united = []
-    united.append((x, y))
+        if board[next_pos_x1][next_pos_y1] == 0 and board[next_pos_x2][next_pos_y2] == 0:
+            posList.append({(next_pos_x1, next_pos_y1),
+                           (next_pos_x2, next_pos_y2)})
 
-    union[x][y] = index
-    summary = data[x][y]
-    count = 1
-
-    while q:
-        x, y = q.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
-                if l <= abs(data[nx][ny] - data[x][y]) <= r:
-                    union[nx][ny] = index
-                    summary += data[nx][ny]
-                    united.append((nx, ny))
-                    q.append((nx, ny))
-                    count += 1
-    for x, y in united:
-        data[x][y] = summary // count
-    return count
+        if pos_x1 == pos_x2:
+            for i in [-1, 1]:
+                if board[pos_x1+i][pos_y1] == 0 and board[pos_x2+i][pos_y2] == 0:
+                    posList.append({(pos_x1, pos_y1), (pos_x1+i, pos_y1)})
+                    posList.append({(pos_x2, pos_y2), (pos_x2+i, pos_y2)})
+        if pos_y1 == pos_y2:
+            for i in [-1, 1]:
+                if board[pos_x1][pos_y1+i] == 0 and board[pos_x2][pos_y2+i] == 0:
+                    posList.append({(pos_x1, pos_y1), (pos_x1, pos_y1+i)})
+                    posList.append({(pos_x2, pos_y2), (pos_x2, pos_y2+i)})
+    return posList
 
 
-tot = 0
-while True:
-    union = [[-1] * (n+1) for _ in range(n+1)]
-    index = 0
+def bfs(board):
+    n = len(board)
+    newBoard = [[1]*(n+2) for _ in range(n+2)]
     for i in range(n):
         for j in range(n):
-            if union[i][j] == -1:
-                index += 1
-                bfs(i, j, index)
-    if index == n*n:
-        break
-    tot += 1
-print(tot)
+            newBoard[i+1][j+1] = board[i][j]
+
+    visited = []
+    pos = {(1, 1), (1, 2)}
+    visited.append(pos)
+    q = deque([])
+    q.append((pos, 0))
+
+    while q:
+        pos, cost = q.popleft()
+
+        if (n, n) in pos:
+            return cost
+
+        for next in next_step(pos, newBoard):
+            if next not in visited:
+                visited.append(next)
+                q.append((next, cost+1))
+    return cost
