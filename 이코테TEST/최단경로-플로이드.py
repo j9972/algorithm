ef571c7f538e9dcbccb28075ea0bcf9ab1bd36f6
jung su young <j9972@naver.com>
2023-01-04 14:45:10 +0900

    노드를 연결하는 간선의 개수가 많을 수 있음을 체크

---
+++ 이코테TEST/최단경로-플로이드.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if c > graph[a][b]:
+        c = graph[a][b]
+    graph[a][b] = c
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            graph[i][j] = 0
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] != INF:
+            print(graph[i][j], end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
