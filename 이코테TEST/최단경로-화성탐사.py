11056665a7e796eb5a82b5def4d6d043532cf675
jung su young <j9972@naver.com>
2023-01-04 16:13:15 +0900

    화성탐사

--- 이코테TEST/최단경로-화성탐사.py
+++ 이코테TEST/최단경로-화성탐사.py
@@ -14,16 +14,25 @@ for tc in range(int(input())):
         costData.append(list(map(int, input().split())))
 
     q = []
-    heapq.heappush(q, (costData[0][0], 0, 0))
+    x, y = 0, 0
+    heapq.heappush(q, (costData[x][y], x, y))
+    graph[x][y] = costData[x][y]  # 여기 체크 -> 난 graph[x][y] = 0
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
     while q:
         dist, x, y = heapq.heappop(q)
         if graph[x][y] < dist:
             continue
-        for i in graph[x][y]:
-            cost = dist + i
-            if cost < i:
-                i = cost
-                heapq.heappush(q, (cost, x, y))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:  # 이거 체크
+                # 여기도 체크  cost = dist + graph[nx][ny] 했었음
+                cost = dist + costData[nx][ny]
+                if cost < graph[nx][ny]:
+                    graph[nx][ny] = cost
+                    heapq.heappush(q, (cost, nx, ny))
 
-    print(graph[0][0])
+    print(graph[n-1][n-1])
