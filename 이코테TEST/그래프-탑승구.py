310b0fb6830f9dd72ea84a1624d6d0b4eaab3166
jung su young <j9972@naver.com>
2023-01-05 15:23:15 +0900

    어렵다...

---
+++ 이코테TEST/그래프-탑승구.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+g = int(input())  # node 개수
+p = int(input())
+
+parent = [0] * (g+1)
+
+for i in range(1, g+1):
+    parent[i] = i
+
+cnt = 0
+for i in range(p):
+    data = find(parent, int(input()))
+    if data == 0:
+        break
+    union(parent, data, data - 1)
+    cnt += 1
+print(cnt)
