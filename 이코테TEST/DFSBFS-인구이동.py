d93cdfbecdb83bc7869a5435b9db0f307da3271b
jung su young <j9972@naver.com>
2023-01-07 16:29:11 +0900

    인구이동

--- 이코테TEST/DFSBFS-인구이동.py
+++ 이코테TEST/DFSBFS-인구이동.py
@@ -4,12 +4,12 @@ input = sys.stdin.readline
 
 n, l, r = map(int, input().split())
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 data = []
 for i in range(n):
     data.append(list(map(int, input().split())))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
 
 def process(x, y, index):
@@ -19,9 +19,9 @@ def process(x, y, index):
     q = deque()
     q.append((x, y))
 
-    union[x][y] = index
+    summary = data[x][y]
     cnt = 1
-    population = data[x][y]
+    union[x][y] = index  # 연합의 번호를 할당
 
     while q:
         x, y = q.popleft()
@@ -32,25 +32,27 @@ def process(x, y, index):
 
             if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
                 if l <= abs(data[nx][ny] - data[x][y]) <= r:
-                    population += data[nx][ny]
-                    cnt += 1
                     union[nx][ny] = index
-                    united.append((nx, ny))
+                    summary += data[nx][ny]
                     q.append((nx, ny))
+                    united.append((nx, ny))
+                    cnt += 1
+    # 연합국가의 인구 분배 하는 부분
     for x, y in united:
-        data[x][y] = population // cnt
+        data[x][y] = summary // cnt
     return cnt
 
 
 time = 0
 while True:
-    union = [[-1]*n for i in range(n)]
-    index = 0  # 해당 연합 국가의 번호
+    # 연합의 해당 국가의 번호
+    union = [[-1] * (n+1) for _ in range(n+1)]
+    index = 0
 
     for i in range(n):
         for j in range(n):
-            if union[i][j] == -1:
-                index += 1
+            if union[i][j] == -1:  # 방문하지 않아서 해당 국가의 번호가 초기화 값 그대로인 경우는 방문처리 가능
+                index += 1  # 번호
                 process(i, j, index)
 
     if index == n*n:
