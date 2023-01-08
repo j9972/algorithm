eb7a41564fc21c7e49b4b364a6a571960d1d4258
jung su young <j9972@naver.com>
2023-01-08 21:39:00 +0900

    압축

---
+++ 이코테TEST/구현-문자열압축.py
@@ -0,0 +1,21 @@
+def solution(s):
+    length = len(s)
+    for step in range(1, len(s)//2 + 1):
+        cnt = 1
+        ans = ""
+        prev = s[:step]
+
+        for i in range(step, len(s), step):
+            if prev == s[i:i+step]:
+                cnt += 1
+            else:
+                ans += str(cnt) + prev if cnt >= 2 else prev
+                cnt = 0
+                prev = s[i:i+step]
+        ans += str(cnt) + prev if cnt >= 2 else prev
+        length = min(len(ans), length)
+
+    return length
+
+
+print(solution("ababcdcdababcdcd"))  # 9 가 정답
