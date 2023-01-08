23ffbccf48bd7e2e047c023c2101d9e070023671
jung su young <j9972@naver.com>
2023-01-08 17:18:17 +0900

    for문 돌릴때 range 지양하기

---
+++ 이코테TEST/삼성-청소년상어.py
@@ -0,0 +1,85 @@
+from collections import deque
+import copy
+import sys
+input = sys.stdin.readline
+
+dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+dy = [0, -1, -1, -1, 0, 1, 1, 1]
+
+data = [[None]*4 for _ in range(4)]
+for i in range(4):
+    info = list(map(int, input().split()))
+    for j in range(4):
+        data[i][j] = [info[j*2], info[j*2+1]-1]
+
+
+def turning(direction):
+    return (direction+1) % 8
+
+
+def findFish(array, index):
+    for i in range(4):
+        for j in range(4):
+            if array[i][j][0] == index:
+                return (i, j)
+    return None
+
+
+def moving(array, now_x, now_y):
+    for i in range(1, 17):
+        pos = findFish(array, i)
+
+        if pos != None:
+            x, y = pos[0], pos[1]
+            direction = array[x][y][1]
+
+            for i in range(8):
+                nx = x + dx[direction]
+                ny = y + dy[direction]
+
+                if 0 <= nx < 4 and 0 <= ny < 4:
+                    if not (nx == now_x and ny == now_y):
+                        array[x][y][1] = direction
+                        array[x][y], array[nx][ny] = array[nx][ny], array[x][y]
+                        break
+                direction = turning(direction)
+
+
+def eatingFish(array, x, y):
+    posList = []
+    direction = array[x][y][1]
+
+    for _ in range(4):
+        x += dx[direction]
+        y += dy[direction]
+
+        if 0 <= x < 4 and 0 <= y < 4:
+            if array[x][y][0] != -1:
+                posList.append((x, y))
+
+    return posList
+
+
+res = 0
+
+
+def dfs(array, x, y, tot):
+    global res
+    array = copy.deepcopy(array)
+
+    tot += array[x][y][0]
+    array[x][y][0] = -1
+
+    moving(array, x, y)
+    pos = eatingFish(array, x, y)
+
+    if len(pos) == 0:
+        res = max(res, tot)
+        return
+
+    for next_x, next_y in pos:
+        dfs(array, next_x, next_y, tot)
+
+
+dfs(data, 0, 0, 0)
+print(res)
