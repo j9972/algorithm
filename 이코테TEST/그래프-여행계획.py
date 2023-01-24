19bdbae0d4da448728d5c0447d6ccf29efc63e72
jung su young <j9972@naver.com>
2023-01-24 20:26:35 +0900

    여행계획 - 서로소집합

--- 이코테TEST/그래프-여행계획.py
+++ 이코테TEST/그래프-여행계획.py
@@ -24,11 +24,17 @@ for i in range(1, n+1):
     parent[i] = i
 
 data = []
-for i in range(n):
+for i in range(1, n+1):
     data.append(list(map(int, input().split())))
+    for j in range(1, n+1):
+        if data[i-1][j-1] == 1:
+            union(parent, i, j)
 
 info = list(map(int, input().split()))
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if data[i][j] == 1:
+for i in range(m-1):
+    if find(parent, info[i]) != find(parent, info[i+1]):
+        print('No')
+    else:
+        print('Yes')
+    break
