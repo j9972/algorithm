982e365d92f251cb7c3595eeee17f54b5a212c1b
jung su young <j9972@naver.com>
2023-01-24 13:54:32 +0900

    위상정렬 사용

--- 이코테TEST/1-check.py
+++ 이코테TEST/1-check.py
@@ -1,40 +1,5 @@
-from collections import deque
-import copy
-import heapq
 import sys
+import heapq
 input = sys.stdin.readline
 
 n, m, start = map(int, input().split())
-INF = int(1e9)
-graph = [[]*m for _ in range(n+1)]
-distance = [INF] * (n+1)
-
-for i in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-
-
-def dik(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if distance[i[0]] < cost:
-                cost = distance[i[0]]
-                heapq.heappush(q, (cost, i[0]))
-
-
-dik(start)
-cnt = 1
-max_distance = 0
-for d in distance:
-    if d != INF:
-        cnt += 1
-        max_distance = max(max_distance, d)
-print(cnt - 1, max_distance)
--- 이코테TEST/그래프-커리큘럼.py
+++ 이코테TEST/그래프-커리큘럼.py
@@ -4,36 +4,32 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-graph = [[] for _ in range(n+1)]
-indegree = [0] * (n+1)
-time = [0] * (n+1)
 
-for i in range(1,n+1):
+inDegree = [0] * (n+1)
+time = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
+
+for i in range(1, n+1):
     data = list(map(int, input().split()))
-    time[i] = data[0] # 시작하는 정보를 전부 넣어줘야 한다.
+    time[i] = data[0]
     for x in data[1:-1]:
         graph[x].append(i)
-        indegree[i] += 1
+        inDegree[i] += 1
 
-def topo():
-    res = copy.deepcopy(time)
-    q = deque()
+res = copy.deepcopy(time)
+q = deque()
 
-    for i in range(1, n+1):
-        if indegree[i] == 0:
+for i in range(1, n+1):
+    if inDegree[i] == 0:
+        q.append(i)
+
+while q:
+    now = q.popleft()
+
+    for i in graph[now]:
+        res[i] = max(res[i], res[now]+time[i])
+        inDegree[i] -= 1
+        if inDegree[i] == 0:
             q.append(i)
-
-    while q:
-        now = q.popleft()
-
-        for i in graph[now]:
-            res[i] = max(res[i], res[now] + time[i])
-            indegree[i] -= 1
-            if indegree[i] == 0:
-                q.append(i)
-    
-    for i in range(1,n+1):
-        print(res[i])
-
-
-topo()
+for i in range(1, n+1):
+    print(res[i])
