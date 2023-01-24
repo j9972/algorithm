de0c52dfb474ca506e16aafa9110d6d15e60c737
jung su young <j9972@naver.com>
2023-01-24 15:53:30 +0900

    개선된 다익스트라

--- 이코테TEST/1-check.py
+++ 이코테TEST/1-check.py
@@ -1,28 +1,37 @@
+import heapq
 import sys
 input = sys.stdin.readline
 
 INF = int(1e9)
-n, m = map(int, input().split())
-graph = [[INF]*(n+1) for _ in range(n+1)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-for i in range(1, n+1):
-    graph[i][i] = 0
+for tc in range(int(input())):
+    n = int(input())
 
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
+    data = []
+    for i in range(n):
+        data.append(list(map(int, input().split())))
 
-for k in range(1, n+1):
-    for a in range(1, n+1):
-        for b in range(1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+    distance = [[INF] * n for i in range(n)]
 
-res = 0
-for i in range(1, n+1):
-    cnt = 0
-    for j in range(1, n+1):
-        if graph[i][j] != INF or graph[j][i] != INF:
-            cnt += 1
-    if cnt == n:
-        res += 1
-print(res)
+    x, y = 0, 0
+    distance[x][y] = data[x][y]
+    q = [(data[x][y], x, y)]
+
+    while q:
+        dist, x, y = heapq.heappop(q)
+
+        if distance[x][y] < dist:
+            continue
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                cost = dist + data[nx][ny]
+                if cost < distance[nx][ny]:
+                    distance[nx][ny] = cost
+                    heapq.heappush(q, (cost, nx, ny))
+    print(distance[n-1][n-1])
