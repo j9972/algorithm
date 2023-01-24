3552ff99712155c502dbdbe1c3faf434a3672d0e
jung su young <j9972@naver.com>
2023-01-24 14:39:25 +0900

    플로이드 워셜

--- 이코테TEST/최단경로-정확한순위.py
+++ 이코테TEST/최단경로-정확한순위.py
@@ -1,29 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
 INF = int(1e9)
+n, m = map(int, input().split())
 graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    graph[i][i] = 0
 
 for i in range(m):
     a, b = map(int, input().split())
     graph[a][b] = 1
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if i == j:
-            graph[i][j] = 0
-
 for k in range(1, n+1):
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
 
 res = 0
-for i in range(1, n+1):
+for a in range(1, n+1):
     cnt = 0
-    for j in range(1, n+1):
-        if graph[i][j] != INF or graph[j][i] != INF:
+    for b in range(1, n+1):
+        if graph[a][b] != INF or graph[b][a] != INF:
             cnt += 1
     if cnt == n:
         res += 1
