b8b62c56d1f2ffb1de18906ab047018b9fed4e2d
jung su young <j9972@naver.com>
2023-01-25 10:38:40 +0900

    전입차수 채울때 범위 조심하기

--- 이코테TEST/1-check.py
+++ 이코테TEST/1-check.py
@@ -1,42 +1,37 @@
-import heapq
+import copy
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-INF = int(1e9)
-n, m, start = map(int, input().split())
+n = int(input())
+inDegree = [0] * (n+1)
+time = [0] * (n+1)
 graph = [[] for _ in range(n+1)]
-distance = [INF] * (n+1)
 
-for _ in range(m):
-    x, y, z = map(int, input().split())
-    graph[x].append((y, z))
-
-
-def dik(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-
-    while q:
-        dist, now = heapq.heappop(q)
-
-        if distance[now] < dist:
-            continue
-
-        for i in graph[now]:
-            cost = dist + i[1]
-            if distance[i[0]] > cost:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+for i in range(1, n+1):
+    data = list(map(int, input().split()))
+    time[i] = data[0]  # 도착 위치를 알려줌
+    for j in data[1:-1]:  # 출발 위치임
+        graph[j].append(i)
+        inDegree[i] += 1
 
 
-dik(start)
+q = deque()
+res = copy.deepcopy(time)
 
-cnt = 0
-max_distance = 0
+for i in range(1, n+1):
+    if inDegree[i] == 0:
+        q.append(i)
 
-for d in distance:
-    if d != INF:
-        max_distance = max(d, max_distance)
-        cnt += 1
-print(cnt-1, max_distance)
+while q:
+    now = q.popleft()
+
+    for i in graph[now]:
+        res[i] = max(res[i], res[now] + time[i])
+        inDegree[i] -= 1
+
+        if inDegree[i] == 0:
+            q.append(i)
+
+for i in range(1, n+1):
+    print(res[i])
