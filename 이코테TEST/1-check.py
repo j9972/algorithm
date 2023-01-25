c2b6bea5ec0e55a482cf5270a77e04d0f3f700a3
jung su young <j9972@naver.com>
2023-01-25 15:36:00 +0900

    그리디

--- 이코테TEST/1-check.py
+++ 이코테TEST/1-check.py
@@ -1,54 +1,62 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
+for tc in range(int(input())):
+    n = int(input())
+    data = list(map(int, input().split()))
+    graph = [[False] * (n+1) for _ in range(n+1)]
 
-def find(parent, x):
-    if parent[x] != x:
-        parent[x] = find(parent, parent[x])
-    return parent[x]
+    inDegree = [0] * (n+1)
+    q = deque()
 
+    for i in range(n):
+        for j in range(i+1, n):
+            graph[data[i]][data[j]] = True
+            inDegree[data[j]] += 1
 
-def union(parent, a, b):
-    b = find(parent, b)
-    a = find(parent, a)
+    m = int(input())
+    for i in range(m):
+        a, b = map(int, input().split())
+        if graph[a][b]:
+            graph[a][b] = False
+            graph[b][a] = True
+            inDegree[b] -= 1
+            inDegree[a] += 1
+        else:
+            graph[a][b] = True
+            graph[b][a] = False
+            inDegree[b] += 1
+            inDegree[a] -= 1
+    for i in range(1, n+1):
+        if inDegree[i] == 0:
+            q.append(i)
+    res = []
 
-    if a < b:
-        parent[b] = a
+    cycle = False
+    certain = True
+
+    for i in range(n):
+        if len(q) == 0:
+            cycle = True
+            break
+        if len(q) >= 2:
+            certain = False
+            break
+
+        now = q.popleft()
+        res.append(now)
+
+        for j in range(1, n+1):
+            if graph[now][j]:
+                inDegree[j] -= 1
+                if inDegree[j] == 0:
+                    q.append(j)
+    if cycle:
+        print('IMPOSSIBLE')
+    elif not certain:
+        print('?')
     else:
-        parent[a] = b
-
-
-n = int(input())
-
-parent = [0] * (n+1)
-for i in range(1, n+1):
-    parent[i] = i
-
-x = []
-y = []
-z = []
-edges = []
-res = 0
-
-for i in range(n):
-    a, b, c = map(int, input().split())
-    x.append((a, i))
-    y.append((b, i))
-    z.append((c, i))
-x.sort()
-y.sort()
-z.sort()
-
-for i in range(n-1):
-    edges.append((x[i+1][0] - x[i][0], x[i][1], x[i+1][1]))
-    edges.append((y[i+1][0] - y[i][0], y[i][1], y[i+1][1]))
-    edges.append((z[i+1][0] - z[i][0], z[i][1], z[i+1][1]))
-
-edges.sort()
-
-for e in edges:
-    cost, a, b = e
-    if find(parent, a) != find(parent, b):
-        union(parent, a, b)
-        res += cost
-print(res)
+        for i in res:
+            print(i, end=' ')
+        print()
--- 이코테TEST/그리디-만들수없는금액.py
+++ 이코테TEST/그리디-만들수없는금액.py
@@ -2,13 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-data = list(map(int, input().split()))
-data.sort()
+coin = list(map(int, input().split()))
+coin.sort()
 
-t = 1
-for i in data:  # 한번 돌때마다 전부 돌리면서 안되는게 있는지 여부 체크를 해서 하는것 ( 하나하나가 아니라 전부 체크 )
-    if t < i:
+maxCoin = 1
+for i in coin:
+    if maxCoin < i:
         break
-    t += i
+    maxCoin += i
 
-print(t)
+print(maxCoin)
