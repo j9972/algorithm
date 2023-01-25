fdc1209a0fd56c061ea34e1ba53b0f02cb11fc65
jung su young <j9972@naver.com>
2023-01-25 11:14:51 +0900

    서로소

--- 이코테TEST/1-check.py
+++ 이코테TEST/1-check.py
@@ -1,8 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-
 
 def find(parent, x):
     if parent[x] != x:
@@ -20,24 +18,18 @@ def union(parent, a, b):
         parent[a] = b
 
 
-parent = [0] * (n+1)
-for i in range(1, n+1):
+g = int(input())
+p = int(input())
+
+parent = [0] * (g+1)
+for i in range(1, g+1):
     parent[i] = i
+cnt = 0
+for i in range(p):
+    data = find(parent, int(input()))
+    if data == 0:
+        break
+    union(parent, data, data-1)
+    cnt += 1
 
-data = []
-for i in range(n):
-    data.append(list(map(int, input().split())))
-
-for i in range(n):
-    for j in range(n):
-        if data[i][j] == 1:
-            union(parent, i+1, j+1)
-
-info = list(map(int, input().split()))
-
-for i in range(m-1):
-    if find(parent, info[i]) != find(parent, info[i+1]):
-        print('NO')
-    else:
-        print('YES')
-    break
+print(cnt)
