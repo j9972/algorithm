41d84097a118d6c082508818ec5dca225fa24fe7
jung su young <j9972@naver.com>
2023-01-25 11:19:40 +0900

    신장트리

--- 이코테TEST/1-check.py
+++ 이코테TEST/1-check.py
@@ -18,18 +18,37 @@ def union(parent, a, b):
         parent[a] = b
 
 
-g = int(input())
-p = int(input())
+n = int(input())
 
-parent = [0] * (g+1)
-for i in range(1, g+1):
+parent = [0] * (n+1)
+for i in range(1, n+1):
     parent[i] = i
-cnt = 0
-for i in range(p):
-    data = find(parent, int(input()))
-    if data == 0:
-        break
-    union(parent, data, data-1)
-    cnt += 1
 
-print(cnt)
+x = []
+y = []
+z = []
+edges = []
+res = 0
+
+for i in range(n):
+    a, b, c = map(int, input().split())
+    x.append((a, i))
+    y.append((b, i))
+    z.append((c, i))
+x.sort()
+y.sort()
+z.sort()
+
+for i in range(n-1):
+    edges.append((x[i+1][0] - x[i][0], x[i][1], x[i+1][1]))
+    edges.append((y[i+1][0] - y[i][0], y[i][1], y[i+1][1]))
+    edges.append((z[i+1][0] - z[i][0], z[i][1], z[i+1][1]))
+
+edges.sort()
+
+for e in edges:
+    cost, a, b = e
+    if find(parent, a) != find(parent, b):
+        union(parent, a, b)
+        res += cost
+print(res)
