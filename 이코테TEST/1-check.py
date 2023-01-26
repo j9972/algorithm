7a809300bbab8703b414ae78c075b1a075b59430
jung su young <j9972@naver.com>
2023-01-26 10:47:05 +0900

    먹방라이브

--- 이코테TEST/1-check.py
+++ 이코테TEST/1-check.py
@@ -1,14 +1,32 @@
 from collections import deque
+import heapq
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-coin = list(map(int, input().split()))
-coin.sort()
 
-maxCoin = 1
-for c in coin:
-    if maxCoin < c:
-        break
-    maxCoin += c
-print(maxCoin)
+def sol(food, k):
+    length = len(food)
+
+    if sum(food) <= k:
+        return -1
+
+    q = []
+    res = []
+
+    for i in range(length):
+        heapq.heappush(q, (food[i], i+1))  # 음식번호, 음식 시간 순서
+
+    previous = 0
+    sum_value = 0
+
+    while sum_value + ((q[0][0] - previous) * length) < k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        previous = now
+        length -= 1
+
+    res = sorted(q, key=lambda x: x[1])
+    return res[(k-sum_value) % length][1]
+
+
+print(sol([3, 1, 2], 5))
