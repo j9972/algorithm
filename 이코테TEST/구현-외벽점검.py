3bb02f4844605651133148bfb59fa81bfef4bff0
jung su young <j9972@naver.com>
2023-01-26 17:56:42 +0900

    외벽체크

--- 이코테TEST/구현-외벽점검.py
+++ 이코테TEST/구현-외벽점검.py
@@ -1,39 +1,29 @@
-from itertools import permutations
+from itertools import permutations as pm
+import sys
+input = sys.stdin.readline
 
 
 def solution(n, weak, dist):
-    # 최솟값을 구해야하니까 초기화 시키기
-    answer = len(dist) + 1
+    ans = len(dist) + 1
 
-    # 내림 차순으로 해서 거리가 가장 긴 거리친구부터 배치
     dist.sort(reverse=True)
-
     length = len(weak)
 
-    # 배열 확장 -> 반시계 / 시계 방향 체크 할 필요가 없음
     for i in range(length):
         weak.append(weak[i]+n)
 
     for start in range(length):
-        for friends in list(permutations(dist, len(dist))):
-            # 친구 명수
-            count = 1
+        for friend in list(pm(dist, len(dist))):
+            friendCnt = 1
+            pos = weak[start] + friend[friendCnt-1]
 
-            # 점검 가능한 마지막 위치
-            position = weak[start] + friends[count - 1]
-
-            for index in range(start, start+length):
-                # 점검 범위를 벗어나면
-                if position < weak[index]:
-                    count += 1
-                    # 친구의 명수가 부족하면
-                    if count > len(dist):
+            for idx in range(start, start+length):
+                if pos < weak[idx]:
+                    friendCnt += 1
+                    if friendCnt > len(dist):
                         break
-                    position = weak[index] + friends[count - 1]
-
-            answer = min(answer, count)
-
-    # 취약점 모두를 찾을 수 없는 경우
-    if answer > len(dist):
+                    pos = weak[idx] + friend[friendCnt-1]
+            ans = min(ans, friendCnt)
+    if ans > len(dist):
         return -1
-    return answer
+    return ans
