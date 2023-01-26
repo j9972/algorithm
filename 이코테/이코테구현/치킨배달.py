807266e31ef03258de423764ddda26de1a89b04a
jung su young <j9972@naver.com>
2023-01-26 17:32:57 +0900

    치킨거리

--- 이코테/이코테구현/치킨배달.py
+++ 이코테/이코테구현/치킨배달.py
@@ -1,32 +1,29 @@
-# 치킨 배달
-from itertools import combinations
+from itertools import combinations as cb
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
+data = []
 home = []
 chicken = []
-
-res = 1e9
-
-data = []
 for i in range(n):
     data.append(list(map(int, input().split())))
-
-for i in range(n):
     for j in range(n):
         if data[i][j] == 1:
             home.append([i, j])
         elif data[i][j] == 2:
             chicken.append([i, j])
 
-for ch in combinations(chicken, m):
+res = 1e9
+
+
+for ch in cb(chicken, m):
     temp = 0
     for h in home:
         ch_len = 999
         for j in range(m):
-            ch_len = min(ch_len, abs(h[0] - ch[j][0]) + abs(h[1] - ch[j][1]))
+            ch_len = min(ch_len, abs(h[0]-ch[j][0]) + abs(h[1]-ch[j][1]))
         temp += ch_len
     res = min(res, temp)
 print(res)
--- 이코테TEST/구현.치킨배달.py
+++ 이코테TEST/구현.치킨배달.py
@@ -2,28 +2,27 @@ from itertools import combinations as cb
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-
-data = []
 home = []
 chicken = []
+
+n, m = map(int, input().split())
+
 for i in range(n):
-    data.append(list(map(int, input().split())))
+    data = list(map(int, input().split()))
     for j in range(n):
-        if data[i][j] == 1:
-            home.append([i, j])
-        elif data[i][j] == 2:
-            chicken.append([i, j])
-
-res = 1e9
+        if data[j] == 1:
+            home.append((i, j))
+        elif data[j] == 2:
+            chicken.append((i, j))
 
 
+distance = 1e9
 for ch in cb(chicken, m):
     temp = 0
     for h in home:
         ch_len = 999
-        for j in range(m):
-            ch_len = min(ch_len, abs(h[0]-ch[j][0]) + abs(h[1]-ch[j][1]))
+        for i in range(m):
+            ch_len = min(ch_len, abs(h[0] - ch[i][0]) + abs(h[1] - ch[i][1]))
         temp += ch_len
-    res = min(res, temp)
-print(res)
+    distance = min(distance, temp)
+print(distance)
