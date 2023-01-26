c1ad154fc57e1782457875a532f9aa3f8fb8b920
jung su young <j9972@naver.com>
2023-01-26 11:34:10 +0900

    key를 lock에 맞추기 전에 4 방향으로 key를 rotate 해보는게 중요하다

--- 이코테TEST/구현-자물쇠와열쇠.py
+++ 이코테TEST/구현-자물쇠와열쇠.py
@@ -1,12 +1,12 @@
-def rotate(key):
-    return list(zip(*key[::-1]))
+def rotate(arr):
+    return list(zip(*arr[::-1]))
 
 
-def check(newLock):
-    n = len(newLock) // 3
-    for i in range(n):
-        for j in range(n):
-            if newLock[i+n][j+n] != 1:
+def check(new_Lock):
+    n = len(new_Lock) // 3
+    for x in range(n, n*2):
+        for y in range(n, n*2):
+            if new_Lock[x][y] != 1:
                 return False
     return True
 
@@ -15,29 +15,26 @@ def solution(key, lock):
     n = len(lock)
     m = len(key)
 
-    newLock = [[0]*(n*3) for _ in range(n*3)]
-
+    new_Lock = [[0]*(n*3) for _ in range(n*3)]
     for i in range(n):
         for j in range(n):
-            newLock[i+n][j+n] = lock[i][j]
+            new_Lock[i+n][j+n] = lock[i][j]
 
     for _ in range(4):
         key = rotate(key)
+        for i in range(n*2):
+            for j in range(n*2):
 
-        for x in range(n*2):
-            for y in range(n*2):
-                # 껴 맞춰 보기
-                for i in range(m):
-                    for j in range(m):
-                        newLock[i+x][j+y] += key[i][j]
+                for x in range(m):
+                    for y in range(m):
+                        new_Lock[i+x][j+y] += key[x][y]
 
-                if check(newLock):
+                if check(new_Lock):
                     return True
 
-                # 껴 맞춰 본것 빼기
-                for i in range(m):
-                    for j in range(m):
-                        newLock[i+x][j+y] -= key[i][j]
+                for x in range(m):
+                    for y in range(m):
+                        new_Lock[i+x][j+y] -= key[x][y]
 
     return False
 
