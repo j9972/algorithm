484054873e4dd3d79d2d99696b573935ac269b1d
jung su young <j9972@naver.com>
2023-01-26 16:47:09 +0900

    범위체크 조심

--- 이코테TEST/구현-뱀.py
+++ 이코테TEST/구현-뱀.py
@@ -4,57 +4,64 @@ input = sys.stdin.readline
 n = int(input())
 k = int(input())
 
-board = [[0]*(n+1) for _ in range(n+1)]  # (1,1) 이 시작
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+board = [[0] * (n+1) for _ in range(n+1)]
 
 for i in range(k):
     x, y = map(int, input().split())
     board[x][y] = 1
 
-changingCount = int(input())
-info = []
-for i in range(changingCount):
-    x, c = map(str, input().split())
-    info.append((int(x), c))
+l = int(input())
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+info = []
+for i in range(l):
+    t, direction = input().split()
+    info.append((int(t), direction))
 
 
 def turning(direction, c):
-    if c == 'L':
-        direction = (direction-1) % 4
+    if c == 'D':
+        direction = (direction + 1) % 4
     else:
-        direction = (direction+1) % 4
+        direction = (direction - 1) % 4
     return direction
 
 
-x, y = 1, 1
-q = [(x, y)]
-board[x][y] = 2
-direction = 0
-time = 0
-cnt = 0
+def process():
+    x, y = 1, 1  # 처음 뱀 위치
+    board[x][y] = 2  # 뱀이 있는 의치
+    time = 0  # 이동 시간
+    direction = 0
+    cnt = 0  # 회전횟수 체크를 위함
 
-while True:
-    nx = x + dx[direction]
-    ny = y + dy[direction]
+    q = [(x, y)]
 
-    if 1 <= nx <= n and 1 <= ny <= n and board[nx][ny] != 2:
-        if board[nx][ny] == 0:
-            board[nx][ny] = 2
-            q.append((nx, ny))
-            px, py = q.pop(0)
-            board[px][py] = 0
-        elif board[nx][ny] == 1:
-            board[nx][ny] = 2
-            q.append((nx, ny))
-    else:
+    while True:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+
+        if 1 <= nx <= n and 1 <= ny <= n and board[nx][ny] != 2:
+            if board[nx][ny] == 1:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+
+            elif board[nx][ny] == 0:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+                px, py = q.pop(0)
+                board[px][py] = 0
+        else:
+            time += 1
+            break
         time += 1
-        break
-    time += 1
-    x, y = nx, ny
+        x, y = nx, ny
 
-    if cnt < changingCount and info[cnt][0] == time:
-        direction = turning(direction, info[cnt][1])
-        cnt += 1
-print(time)
+        if cnt < l and info[cnt][0] == time:
+            direction = turning(direction, info[cnt][1])
+            cnt += 1
+    return time
+
+
+print(process())
