90d25a910fbf13690dfc22fc2657c90630823112
jung su young <j9972@naver.com>
2023-01-26 15:21:59 +0900

    자물쇠 클리어

--- 이코테TEST/구현-자물쇠와열쇠.py
+++ 이코테TEST/구현-자물쇠와열쇠.py
@@ -2,11 +2,11 @@ def rotate(arr):
     return list(zip(*arr[::-1]))
 
 
-def check(new_Lock):
-    n = len(new_Lock) // 3
-    for x in range(n, n*2):
-        for y in range(n, n*2):
-            if new_Lock[x][y] != 1:
+def check(arr):
+    n = len(arr)//3
+    for i in range(n, n*2):
+        for j in range(n, n*2):
+            if arr[i][j] != 1:
                 return False
     return True
 
@@ -15,26 +15,25 @@ def solution(key, lock):
     n = len(lock)
     m = len(key)
 
-    new_Lock = [[0]*(n*3) for _ in range(n*3)]
+    newlock = [[0] * (n*3) for _ in range(n*3)]
+
     for i in range(n):
         for j in range(n):
-            new_Lock[i+n][j+n] = lock[i][j]
+            newlock[i+n][j+n] = lock[i][j]
 
     for _ in range(4):
         key = rotate(key)
         for i in range(n*2):
             for j in range(n*2):
-
                 for x in range(m):
                     for y in range(m):
-                        new_Lock[i+x][j+y] += key[x][y]
-
-                if check(new_Lock):
+                        newlock[i+x][j+y] += key[x][y]
+                if check(newlock):
                     return True
 
                 for x in range(m):
                     for y in range(m):
-                        new_Lock[i+x][j+y] -= key[x][y]
+                        newlock[i+x][j+y] -= key[x][y]
 
     return False
 
