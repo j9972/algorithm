4ae35072e6d70c3d346276e85c6add170d5a2d6c
jung su young <j9972@naver.com>
2023-01-27 17:21:31 +0900

    뱀

--- 이코테TEST/1-check.py
+++ 이코테TEST/1-check.py
@@ -1,4 +1,66 @@
-from collections import deque
-import heapq
+
 import sys
 input = sys.stdin.readline
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+n = int(input())
+k = int(input())
+
+board = [[0] * (n+1) for _ in range(n+1)]
+
+for i in range(k):
+    x, y = map(int, input().split())
+    board[x][y] = 1
+
+l = int(input())
+info = []
+for i in range(l):
+    t, direction = input().split()
+    info.append((int(t), direction))
+
+
+def turning(direction, c):
+    if c == 'L':
+        direction = (direction - 1) % 4
+    else:
+        direction = (direction + 1) % 4
+    return direction
+
+
+def process():
+    x, y = 1, 1
+    board[x][y] = 2
+    q = [(x, y)]
+    direction = 0
+    time = 0
+    cnt = 0
+
+    while True:
+        nx = x + dx[direction]
+        ny = y + dy[direction]
+
+        if 1 <= nx <= n and 1 <= ny <= n and board[nx][ny] != 2:
+            if board[nx][ny] == 0:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+                hx, hy = q.pop(0)
+                board[hx][hy] = 0
+
+            elif board[nx][ny] == 1:
+                board[nx][ny] = 2
+                q.append((nx, ny))
+        else:
+            time += 1
+            break
+        time += 1
+        x, y = nx, ny
+
+        if cnt < l and info[cnt][0] == time:
+            direction = turning(direction, info[cnt][1])
+            cnt += 1
+    return time
+
+
+print(process())
