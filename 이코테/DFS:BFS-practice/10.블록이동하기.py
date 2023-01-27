8a913da04574be36bdf13b38411ae7d334b8bddf
jung su young <j9972@naver.com>
2023-01-27 16:36:52 +0900

    블록이동

--- 이코테/DFS:BFS-practice/10.블록이동하기.py
+++ 이코테/DFS:BFS-practice/10.블록이동하기.py
@@ -1,14 +1,15 @@
 from collections import deque
 
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-def move_robot(pos: set, board):
+
+def move_robot(pos, board):
     pos_list = []  # 로봇이 이동가능한 좌표들 담을 리스트
     # 현재 로봇 위치 좌표 값
     pos = list(pos)
     pos_x1, pos_y1, pos_x2, pos_y2 = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
-    # 3-1.이동하는 경우
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
+
     for i in range(4):
         pos_next_x1, pos_next_y1, pos_next_x2, pos_next_y2 = pos_x1 + dx[i], pos_y1 + dy[i], pos_x2 + dx[i], pos_y2 + \
             dy[i]
@@ -60,64 +61,3 @@ def solution(board):
                 visited.append(next_pos)
                 robot_queue.append((next_pos, cost + 1))
     return cost
-
-
-def next_pos_func(pos, new_one):
-    next_pos = []
-    pos = list(pos)
-    pos1_x = pos[0][0]
-    pos1_y = pos[0][1]
-    pos2_x = pos[1][0]
-    pos2_y = pos[1][1]
-
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-
-    for i in range(4):
-        pos1_next_x = pos1_x + dx[i]
-        pos1_next_y = pos1_y + dy[i]
-        pos2_next_x = pos2_x + dx[i]
-        pos2_next_y = pos2_y + dy[i]
-
-        if new_one[pos1_next_x][pos1_next_y] == 0 and new_one[pos2_next_x][pos2_next_y] == 0:
-            next_pos.append({(pos1_next_x, pos1_next_y),
-                            (pos2_next_x, pos2_next_y)})
-
-    if pos1_x == pos2_x:
-        if i in [-1, 1]:
-            if new_one[pos1_x + i][pos1_y] == 0 and new_one[pos2_x + i][pos2_y] == 0:
-                next_pos.append({(pos1_x, pos1_y), (pos1_x+i, pos1_y)})
-                next_pos.append({(pos2_x, pos2_y), (pos2_x+i, pos2_y)})
-    if pos1_y == pos2_y:
-        if i in [-1, 1]:
-            if new_one[pos1_x][pos1_y + i] == 0 and new_one[pos2_x][pos2_y + i] == 0:
-                next_pos.append({(pos1_x, pos1_y), (pos1_x, pos1_y+i)})
-                next_pos.append({(pos2_x, pos2_y), (pos2_x, pos2_y+i)})
-    return next
-
-
-def solution(board):
-    n = len(board)
-    newBoard = [[1]*(n+2) for _ in range(n+2)]
-
-    for i in range(n):
-        for j in range(n):
-            newBoard[i+1][j+1] = board[i][j]
-
-    q = deque()
-    pos = {(1, 1), (1, 2)}
-    visited = []
-
-    visited.append(pos)
-    q.append((pos, 0))
-
-    while q:
-        pos, cost = q.popleft()
-        for (n, n) in pos:
-            return cost
-
-        for next in next_pos_func(pos, new_one):
-            if next not in visited:
-                visited.append(next)
-                q.append((next, cost+1))
-    return 0
