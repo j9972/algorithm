b38ddcd0c965e42880093e4cacaf00b359c409e3
jung su young <j9972@naver.com>
2023-01-30 17:38:09 +0900

    합의 개수

--- 소마대비기출/체크용.py
+++ 소마대비기출/체크용.py
@@ -1,19 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-data = [0]
-for i in range(n):
-    data.append(int(input().rstrip()))
+for tc in range(int(input())):
+    n = int(input())
 
-if n == 1:
-    print(data[1])
-else:
-    d = [0] * (n+1)
-    d[1] = data[1]
-    d[2] = data[1] + data[2]
-
-    for i in range(3, n+1):
-        d[i] = max(d[i-3] + data[i] + data[i-1], d[i-2] + data[i])
+    d = [0] * 11
+    d[1] = 1
+    d[2] = 2
+    d[3] = 4
+    for i in range(4, n+1):
+        d[i] = d[i-1] + d[i-2] + d[i-3]
 
     print(d[n])
