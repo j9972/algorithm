23239544facb587b8df3b883dea50b4523823740
jung su young <j9972@naver.com>
2023-01-31 19:49:37 +0900

    바닥공사

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,14 +1,15 @@
-# 개미 전사
+# 바닥 공사
 import sys
 input = sys.stdin.readline
 
 x = int(input())
 
-dp = [0] * 100
-data = list(map(int, input().split()))
-dp[0] = data[0]
-dp[1] = max(data[0], data[1])
+dp = [0] * 1001
 
-for i in range(2, x):
-    dp[i] = max(dp[i-1], dp[i-2]+data[i])
-print(dp[x-1])
+dp[1] = 1
+dp[2] = 3
+#dp[3] = 5
+
+for i in range(3, x+1):
+    dp[i] = (dp[i-1] + dp[i-2]*2) % 796796
+print(dp[x])
