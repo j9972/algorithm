11b6433ed7f484aca05412bc94926ac412b9b51d
jung su young <j9972@naver.com>
2023-01-31 21:41:24 +0900

    정수 삼각형

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -2,36 +2,27 @@
 import sys
 input = sys.stdin.readline
 
-for tc in range(int(input())):
-    n, m = map(int, input().split())
-    data = list(map(int, input().split()))
 
-    dp = []
-    idx = 0
-    for i in range(n):
-        dp.append(data[idx:idx+m])
-        idx += m
+n = int(input())
+dp = []
+for i in range(n):
+    dp.append(list(map(int, input().split())))
 
-    for j in range(1, m):
-        for i in range(n):
-            # 왼쪽 위에서 오는 경우
-            if i == 0:
-                left_up = 0
-            else:
-                left_up = dp[i-1][j-1]
+for i in range(1, n):
+    for j in range(i+1):
+        # 왼쪽 위에서 오는 경우
+        if j == 0:
+            left_up = 0
+        else:
+            left_up = dp[i-1][j-1]
 
-            # 왼쪽 아래에서 오는 경우
-            if i == n-1:
-                left_down = 0
-            else:
-                left_down = dp[i+1][j-1]
+        # 위
+        if i == j:
+            up = 0
+        else:
+            up = dp[i-1][j]
 
-            left = dp[i][j-1]
+        # dp[i][j] 왼쪽에서 오기전까지의 최대값
+        dp[i][j] = dp[i][j] + max(up, left_up)
 
-            # dp[i][j] 왼쪽에서 오기전까지의 최대값
-            dp[i][j] = dp[i][j] + max(left, left_down, left_up)
-    res = 0
-    for i in range(n):
-        res = max(res, dp[i][m-1])
-
-    print(res)
+print(max(dp[n-1]))
