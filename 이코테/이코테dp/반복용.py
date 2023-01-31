62740500d2164cd5d7b2dc34808e80d55831c3f9
jung su young <j9972@naver.com>
2023-01-31 21:03:01 +0900

    항상 왼쪽에서 오는 경우로 생각하고 문제를 풀자 제에발발

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,22 +1,37 @@
-# 바닥 공사
+# 금광
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-coin = []
-for i in range(n):
-    coin.append(int(input()))
-coin.sort()
+for tc in range(int(input())):
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
 
-d = [10001] * (m+1)  # 1<=m<=10000
-d[0] = 0
+    dp = []
+    idx = 0
+    for i in range(n):
+        dp.append(data[idx:idx+m])
+        idx += m
 
-for i in range(n):
-    for j in range(coin[i], m+1):
-        if d[j-coin[i]] != 10001:
-            d[j] = min(d[j], d[j-coin[i]]+1)
+    for j in range(1, m):
+        for i in range(n):
+            # 왼쪽 위에서 오는 경우
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i-1][j-1]
 
-if d[m] != 10001:
-    print(d[m])
-else:
-    print(-1)
+            # 왼쪽 아래에서 오는 경우
+            if i == n-1:
+                left_down = 0
+            else:
+                left_down = dp[i+1][j-1]
+
+            left = dp[i][j-1]
+
+            # dp[i][j] 왼쪽에서 오기전까지의 최대값
+            dp[i][j] = dp[i][j] + max(left, left_down, left_up)
+    res = 0
+    for i in range(n):
+        res = max(res, dp[i][m-1])
+
+    print(res)
