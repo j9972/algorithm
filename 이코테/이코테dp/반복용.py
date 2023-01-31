39e415202919a4b6348e6141dc21d23c31c0c2a4
jung su young <j9972@naver.com>
2023-01-31 19:33:35 +0900

    개미전사

--- 이코테/이코테dp/반복용.py
+++ 이코테/이코테dp/반복용.py
@@ -1,21 +1,14 @@
-# 1로 만들기
+# 개미 전사
 import sys
 input = sys.stdin.readline
 
 x = int(input())
 
-dp = [0] * (x+1)
-dp[1] = 0
-dp[2] = 1
-dp[3] = 1
+dp = [0] * 100
+data = list(map(int, input().split()))
+dp[0] = data[0]
+dp[1] = max(data[0], data[1])
 
-for i in range(4, x+1):
-    dp[i] = dp[i-1] + 1
-    if i % 5 == 0:
-        dp[i] = min(dp[i], dp[i//5] + 1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-
-print(dp[x])
+for i in range(2, x):
+    dp[i] = max(dp[i-1], dp[i-2]+data[i])
+print(dp[x-1])
