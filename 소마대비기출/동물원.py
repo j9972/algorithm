9a958cd91a3b9c3661f31eef087e0b8e52aeeec9
jung su young <j9972@naver.com>
2023-02-01 15:26:54 +0900

    동전

--- 소마대비기출/동물원.py
+++ 소마대비기출/동물원.py
@@ -4,3 +4,12 @@ input = sys.stdin.readline
 
 n = int(input())
 
+d = [0] * 100001
+d[0] = 1
+d[1] = 3
+d[2] = 7
+d[3] = 17
+
+for i in range(4, n+1):
+    d[i] = (d[i-1]*2 + d[i-2]) % 9901
+print(d[n])
--- 소마대비기출/동전1.py
+++ 소마대비기출/동전1.py
@@ -6,3 +6,13 @@ n, k = map(int, input().split())
 coin = []
 for i in range(n):
     coin.append(int(input()))
+
+d = [0] * (k+1)
+d[0] = 1
+
+for i in coin:
+    for j in range(1, k+1):
+        if j - i >= 0:
+            d[j] += d[j-i]
+
+print(d[k])
