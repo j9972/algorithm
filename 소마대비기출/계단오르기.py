c7312bb7510272063d299918a2420afd78aaa69b
jung su young <j9972@naver.com>
2023-02-01 11:44:00 +0900

    계단오르기

--- 소마대비기출/계단오르기.py
+++ 소마대비기출/계단오르기.py
@@ -3,3 +3,18 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+stair = [0]
+for i in range(n):
+    stair.append(int(input()))
+
+if n == 1:
+    print(stair[1])
+else:
+    d = [0] * 301
+    d[0] = 0
+    d[1] = stair[1]
+    d[2] = stair[1] + stair[2]
+    for i in range(3, n+1):
+        d[i] = max(d[i-3] + stair[i-1], d[i-2]) + stair[i]
+
+    print(d[n])
--- 소마대비기출/단지번호붙이기.py
+++ 소마대비기출/단지번호붙이기.py
@@ -2,3 +2,7 @@
 import sys
 input = sys.stdin.readline
 
+n = int(input())
+data = []
+for i in range(n):
+    data.append(list(map(int, input().strip())))
