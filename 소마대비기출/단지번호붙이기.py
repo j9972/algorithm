d6bc29afcb6e16f28739befe800bac5edd35fa73
jung su young <j9972@naver.com>
2023-02-01 14:47:39 +0900

    단지번호 붙이기

--- 소마대비기출/단지번호붙이기.py
+++ 소마대비기출/단지번호붙이기.py
@@ -6,3 +6,30 @@ n = int(input())
 data = []
 for i in range(n):
     data.append(list(map(int, input().strip())))
+
+ans = 0
+
+
+def dfs(x, y):
+    global ans
+    if 0 <= x < n and 0 <= y < n and data[x][y] != 0:
+        ans += 1
+        data[x][y] = 0
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    return False
+
+
+res = []
+for i in range(n):
+    for j in range(n):
+        if dfs(i, j) == True:
+            res.append(ans)
+            ans = 0
+res.sort()
+print(len(res))
+for i in res:
+    print(i)
