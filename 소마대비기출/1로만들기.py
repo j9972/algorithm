a1c302a54b0f07d57bfcff99ecd30d598c7614ea
jung su young <j9972@naver.com>
2023-02-01 10:13:00 +0900

    bottom-up, indexError

--- 소마대비기출/1로만들기.py
+++ 소마대비기출/1로만들기.py
@@ -5,13 +5,11 @@ input = sys.stdin.readline
 x = int(input())
 d = [0] * (x+1)
 d[1] = 0
-d[2] = 1
-d[3] = 1
 
-for i in range(4, x+1):
+for i in range(2, x+1):
     d[i] = d[i-1] + 1
     if i % 2 == 0:
         d[i] = min(d[i], d[i//2] + 1)
     if i % 3 == 0:
         d[i] = min(d[i], d[i//3] + 1)
-print(d[x-1])
+print(d[x])
