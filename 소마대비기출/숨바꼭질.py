0463157fc521ec50788b2bf61567f723a7bc4c31
jung su young <j9972@naver.com>
2023-02-01 16:40:00 +0900

    정수 삼각형

--- 소마대비기출/숨바꼭질.py
+++ 소마대비기출/숨바꼭질.py
@@ -1,3 +1,17 @@
 # 실버1 - 1697
+from collections import deque
 import sys
 input = sys.stdin.readline
+
+n, k = map(int, input().split())
+d = [0] * 100001
+d[1] = 1
+
+for i in range(2, n+1):
+    d[i] = d[i-1] + 1
+    if n <= k:
+        d[i] = min(d[i], d[i*2]+1)
+    else:
+        d[i] = min(d[i], d[i-1])
+
+print(d[n])
--- 소마대비기출/정수삼각형.py
+++ 소마대비기출/정수삼각형.py
@@ -2,3 +2,25 @@
 import sys
 input = sys.stdin.readline
 
+n = int(input())
+d = []
+for i in range(n):
+    d.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(i):
+        # left_up
+        if j == 0:
+            left_up = 0
+        else:
+            left_up = d[i-1][j-1]
+
+        # up
+        if i == j:
+            up = 0
+        else:
+            up = d[i-1][j]
+
+        d[i][j] += max(up, left_up)
+
+print(max(d[n-1]))
