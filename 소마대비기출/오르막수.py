e3779011af4995fc36a58a21c7c60665f0fb2868
jung su young <j9972@naver.com>
2023-02-01 17:22:55 +0900

    오르막수

--- 소마대비기출/오르막수.py
+++ 소마대비기출/오르막수.py
@@ -3,8 +3,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [0] * (n+1)
+dp = [[0] * 10 for _ in range(1001)]
 
-dp[1] = 10
-dp[2] = 55
-dp[3] = 220
+for i in range(10):
+    dp[1][i] = 1
+
+for i in range(2, n+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = 1
+        else:
+            dp[i][j] = dp[i][j-1] + dp[i-1][j]
+print(sum(dp[n]) % 10007)
