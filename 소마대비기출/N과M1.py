8e8d840c597b100e7671811fd30421dcf83c93b5
jung su young <j9972@naver.com>
2023-02-02 15:21:46 +0900

    백트래킹

--- 소마대비기출/N과M1.py
+++ 소마대비기출/N과M1.py
@@ -1,3 +1,31 @@
-# 실버 3 - 15649
+# 실버 3 - 15649 - 다시
 import sys
 input = sys.stdin.readline
+
+n = int(input())
+
+res = 0
+row = [0] * (n)
+
+
+def promising(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x]-row[i]) == abs(x-i):
+            return False
+    return True
+
+
+def dfs(queenCnt):
+    global res
+    if queenCnt == n:
+        res += 1
+    else:
+        for i in range(n):
+            # [queenCnt, i] 에 퀸을 놓겠다
+            row[queenCnt] = i
+            if promising(queenCnt):
+                dfs(queenCnt+1)
+
+
+dfs(0)
+print(res)
