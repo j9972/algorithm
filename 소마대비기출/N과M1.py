6f46c6402f5d9b8aff70f782ccad46a798eaa97e
jung su young <j9972@naver.com>
2023-02-02 15:29:18 +0900

    permutation으로 푼 n과m question

--- 소마대비기출/N과M1.py
+++ 소마대비기출/N과M1.py
@@ -1,3 +1,16 @@
 # 실버 3 - 15649 - 다시
+from itertools import permutations as pm
 import sys
 input = sys.stdin.readline
+
+n, m = map(int, input().split())
+num = []
+for i in range(n):
+    num.append(i+1)
+
+data = list(pm(num, m))
+
+for i in data:
+    for j in i:
+        print(j, end=' ')
+    print()
