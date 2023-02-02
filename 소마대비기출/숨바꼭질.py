12180d2206d9da49804a87696c4783854a01bca0
jung su young <j9972@naver.com>
2023-02-02 11:07:04 +0900

    숨바꼭질

--- 소마대비기출/숨바꼭질.py
+++ 소마대비기출/숨바꼭질.py
@@ -4,14 +4,33 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-d = [0] * 100001
-d[1] = 1
+length = 100001
 
-for i in range(2, n+1):
-    d[i] = d[i-1] + 1
-    if n <= k:
-        d[i] = min(d[i], d[i*2]+1)
-    else:
-        d[i] = min(d[i], d[i-1])
+board = [[] for _ in range(length)]
+for i in range(length):
+    board[i] = [i+1, i-1, i*2]
 
-print(d[n])
+visit = [0] * length
+
+
+def bfs(n):
+    q = deque([n])
+
+    if visit[n] > 0:  # 이미 방문된 곳
+        return False
+
+    visit[n] = 1  # 방문처리 된거면 1로 바꾸기 ( 처음에 )
+
+    while q:
+        now = q.popleft()
+
+        if now == k:
+            return visit[k] - 1  # 처음에 방문처리한다고 1을 올려나서 -1 필요
+        for i in board[now]:
+            if 0 <= i < length and visit[i] == 0:
+                visit[i] = visit[now] + 1
+                q.append(i)
+    return True
+
+
+print(bfs(n))
