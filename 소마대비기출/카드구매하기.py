4138bc84f5cb5636eec8ca040933d2f091114c20
jung su young <j9972@naver.com>
2023-02-02 11:27:54 +0900

    카드 구매하기

--- 소마대비기출/카드구매하기.py
+++ 소마대비기출/카드구매하기.py
@@ -3,4 +3,14 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-data = list(map(int, input().split()))
+data = [0] + list(map(int, input().split()))
+d = [0] * (n+1)
+d[0] = 0
+d[1] = data[1]
+
+for i in range(2, n+1):
+    for j in range(1, i+1):
+        if d[i] < d[i-j] + data[j]:
+            d[i] = d[i-j] + data[j]
+
+print(d[n])
