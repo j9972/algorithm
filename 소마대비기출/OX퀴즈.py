c1b8ad82d026f454c50a2a31eb6c079e78ccbc2b
jung su young <j9972@naver.com>
2023-02-03 10:18:38 +0900

    OX를 DP 로 풀어보기

--- 소마대비기출/OX퀴즈.py
+++ 소마대비기출/OX퀴즈.py
@@ -4,23 +4,24 @@ input = sys.stdin.readline
 
 
 for tc in range(int(input())):
-    s = input()
-    d = [0] * (len(s)+1)
+    s = input().rstrip()
+    n = len(s)
+    d = [0] * (n+1)
 
     if s[0] == 'O':
         d[0] = 1
     else:
         d[0] = 0
 
-    for i in range(1, len(s)+1):
-        if s[i-1] == 'O':
-            if s[i] == 'O':
+    for i in range(1, n):
+        if s[i] == 'O':
+            if d[i-1] != 0:
                 d[i] = d[i-1] + 1
             else:
-                d[i] = 0
-        else:
-            if s[i] == 'O':
                 d[i] = 1
-            else:
-                d[i] = 0
-    print(sum(d[len(s)]))
+        else:
+            d[i] = 0
+    res = 0
+    for i in range(n):
+        res += d[i]
+    print(res)
