ef70e1b0569b0a4446c1223d3d83933fe010ff0c
jung su young <j9972@naver.com>
2023-02-07 16:21:24 +0900

    정수삼각형

--- 소마대비기출/0repeat.py
+++ 소마대비기출/0repeat.py
@@ -4,13 +4,21 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
 n = int(input())
-data = list(map(int, input().split()))
-
-d = [1] * (n+1)
-
+d = []
 for i in range(n):
-    for j in range(i):
-        if data[i] > data[j]:
-            d[i] = max(d[i], d[j] + 1)
+    d.append(list(map(int, input().split())))
 
-print(max(d))
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            leftup = 0
+        else:
+            leftup = d[i-1][j-1]
+
+        if j == i:
+            up = 0
+        else:
+            up = d[i-1][j]
+
+        d[i][j] += max(leftup, up)
+print(max(d[n-1]))
