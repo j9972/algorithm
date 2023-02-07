6c27ae1692167def6c98a3b9657101e01694da49
jung su young <j9972@naver.com>
2023-02-07 16:02:08 +0900

    rgb거리

--- 소마대비기출/0repeat.py
+++ 소마대비기출/0repeat.py
@@ -4,13 +4,13 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
 n = int(input())
-d = [0] * 1001
+d = []
+for i in range(n):
+    d.append(list(map(int, input().split())))
 
-d[0] = 0
-d[1] = 1
-d[2] = 2
+for i in range(1, n):
+    d[i][0] += min(d[i-1][1], d[i-1][2])
+    d[i][1] += min(d[i-1][0], d[i-1][2])
+    d[i][2] += min(d[i-1][1], d[i-1][0])
 
-for i in range(3, n+1):
-    d[i] = (d[i-1] + d[i-2]) % 10007
-
-print(d[n])
+print(min(d[n-1][0], d[n-1][1], d[n-1][2]))
