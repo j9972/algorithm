d65f1e0b0cd253074306b075dc538bdd26661210
jung su young <j9972@naver.com>
2023-02-07 11:09:09 +0900

    LIS

--- 소마대비기출/0repeat.py
+++ 소마대비기출/0repeat.py
@@ -1,15 +1,15 @@
-# 실버2 - 11053 - 다시
+# 다시 부분
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 
 n = int(input())
 data = list(map(int, input().split()))
 
 d = [1] * (n+1)
 
-for i in range(n):  # 현재 위치
-    for j in range(i):  # 이전을 의미
+for i in range(n):
+    for j in range(i):
         if data[i] > data[j]:
             d[i] = max(d[i], d[j]+1)
-
 print(max(d))
