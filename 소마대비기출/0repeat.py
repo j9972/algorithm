2a49429737026417214a9f5525002f3889413254
jung su young <j9972@naver.com>
2023-02-07 16:07:48 +0900

    계단오르기

--- 소마대비기출/0repeat.py
+++ 소마대비기출/0repeat.py
@@ -4,13 +4,16 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
 n = int(input())
-d = []
+data = [0]
 for i in range(n):
-    d.append(list(map(int, input().split())))
+    data.append(int(input()))
+if n == 1:
+    print(data[1])
+else:
+    d = [0] * (n+1)
+    d[1] = data[1]
+    d[2] = data[1] + data[2]
+    for i in range(3, n+1):
+        d[i] = max(d[i-3]+data[i-1], d[i-2]) + data[i]
 
-for i in range(1, n):
-    d[i][0] += min(d[i-1][1], d[i-1][2])
-    d[i][1] += min(d[i-1][0], d[i-1][2])
-    d[i][2] += min(d[i-1][1], d[i-1][0])
-
-print(min(d[n-1][0], d[n-1][1], d[n-1][2]))
+    print(d[n])
