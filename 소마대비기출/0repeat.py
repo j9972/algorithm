e90d322f1af6f16c7b7358d5733c23b0d535d023
jung su young <j9972@naver.com>
2023-02-07 16:39:48 +0900

    연속합

--- 소마대비기출/0repeat.py
+++ 소마대비기출/0repeat.py
@@ -4,21 +4,9 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
 n = int(input())
-d = []
-for i in range(n):
-    d.append(list(map(int, input().split())))
-
+data = list(map(int, input().split()))
+d = [0] * n
+d[0] = data[0]
 for i in range(1, n):
-    for j in range(i+1):
-        if j == 0:
-            leftup = 0
-        else:
-            leftup = d[i-1][j-1]
-
-        if j == i:
-            up = 0
-        else:
-            up = d[i-1][j]
-
-        d[i][j] += max(leftup, up)
-print(max(d[n-1]))
+    d[i] = max(data[i], d[i-1] + data[i])
+print(max(d))
