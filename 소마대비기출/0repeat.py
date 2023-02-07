fe069676716a9a88901765cd5ae111677ce6bceb
jung su young <j9972@naver.com>
2023-02-07 16:13:51 +0900

    LIS

--- 소마대비기출/0repeat.py
+++ 소마대비기출/0repeat.py
@@ -4,16 +4,13 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
 n = int(input())
-data = [0]
-for i in range(n):
-    data.append(int(input()))
-if n == 1:
-    print(data[1])
-else:
-    d = [0] * (n+1)
-    d[1] = data[1]
-    d[2] = data[1] + data[2]
-    for i in range(3, n+1):
-        d[i] = max(d[i-3]+data[i-1], d[i-2]) + data[i]
+data = list(map(int, input().split()))
 
-    print(d[n])
+d = [1] * (n+1)
+
+for i in range(n):
+    for j in range(i):
+        if data[i] > data[j]:
+            d[i] = max(d[i], d[j] + 1)
+
+print(max(d))
