a6cb3576cfa0812e4609972a7af213d0553bc36c
jung su young <j9972@naver.com>
2023-02-08 23:52:34 +0900

    그래프를 그리면서 하는것이 이해하기 좋다

---
+++ 소마대비기출/기타리스트.py
@@ -0,0 +1,22 @@
+# 실버 1 - 1495
+import sys
+input = sys.stdin.readline
+n, start, m = map(int, input().split())
+v = list(map(int, input().split()))
+
+d = [[False] * (m+1) for _ in range(n+1)]
+d[0][start] = True
+
+for song in range(n):
+    for vol in range(1, m+1):
+        if d[song][vol] == True:
+            for next in [vol + v[song], vol - v[song]]:
+                if 0 <= next <= m:
+                    d[song+1][next] = True
+
+res = -1
+for vol in range(m, -1, -1):
+    if d[n][vol] == True:
+        res = vol
+        break
+print(res)
