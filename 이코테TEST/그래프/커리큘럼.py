0dfb05f201075d021d89dc5d53de22133f637515
jung su young <j9972@naver.com>
2023-02-10 16:32:22 +0900

    위상정렬

---
+++ 이코테TEST/그래프/커리큘럼.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+import copy
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = [[] for _ in range(n+1)]
+inDegree = [0] * (n+1)
+time = [0] * (n+1)
+
+for i in range(1, n+1):
+    data = list(map(int, input().split()))
+    time[i] = data[0]
+    for x in data[1:-1]:
+        inDegree[i] += 1
+        graph[x].append(i)
+
+res = copy.deepcopy(time)
+q = deque()
+
+for i in range(1, n+1):
+    if inDegree[i] == 0:
+        q.append(i)
+
+while q:
+    now = q.popleft()
+
+    for i in graph[now]:
+        res[i] = max(res[i], res[now] + time[i])
+        inDegree[i] -= 1
+        if inDegree[i] == 0:
+            q.append(i)
+for i in range(1, n+1):
+    print(res[i])
