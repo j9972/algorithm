2e14cf04238c9732cd531af9a6fb689d9d08f7fa
jung su young <j9972@naver.com>
2023-02-11 01:10:25 +0900

    퇴사

--- 이코테TEST/DP/퇴사.py
+++ 이코테TEST/DP/퇴사.py
@@ -5,7 +5,6 @@ sys.setrecursionlimit(10**9)
 n = int(input())
 t = []
 p = []
-
 for i in range(n):
     a, b = map(int, input().split())
     t.append(a)
@@ -13,12 +12,8 @@ for i in range(n):
 
 d = [0] * (n+1)
 
-mv = 0
-
 for i in range(n-1, -1, -1):
     time = t[i] + i
     if time <= n:
-        d[i] = max(p[i] + d[time], mv)
-    else:
-        mv = d[i]
+        d[i] = max(p[i]+d[time], d[i])
 print(max(d))
