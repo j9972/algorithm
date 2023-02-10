ab37df02958afb291ef2447880b5a6dee35a9a9a
jung su young <j9972@naver.com>
2023-02-10 15:20:12 +0900

    바닥공사

--- 이코테TEST/DP/1로만들기.py
+++ 이코테TEST/DP/1로만들기.py
@@ -2,13 +2,18 @@ import sys
 input = sys.stdin.readline
 
 x = int(input())
-data = list(map(int, input().split()))
-
 d = [0] * (x+1)
 
-d[0] = data[0]
-d[1] = max(data[0], data[1])
+d[0] = 0
+d[1] = 0
 
-for i in range(2, x):
-    d[i] = max(d[i-1], d[i-2] + data[i])
-print(d[x-1])
+for i in range(2, x+1):
+    d[i] = d[i-1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2]+1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3]+1)
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i//5]+1)
+
+print(d[x])
