e084ab9638c902542ed30966f4496fdf3b22e139
jung su young <j9972@naver.com>
2023-02-12 11:26:28 +0900

    이동 가능 범위에 따른 최댓값

--- 소마대비기출/자두나무.py
+++ 소마대비기출/자두나무.py
@@ -1,12 +1,30 @@
+# 골드 5 - 2240
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 
 t, w = map(int, input().split())
-d = [[0] * w for _ in range(t)]
+d = [[0] * (w+1) for _ in range(t+1)]
 
+data = [0]
 for i in range(t):
-    data = int(input())
-    d[i][data-1] = 1
+    data.append(int(input()))
 
-for i in range(t):
+for i in range(t+1):
+    # 이동 없음
+    if data[i] == 1:
+        d[i][0] = d[i-1][0] + 1
+    else:
+        d[i][0] = d[i-1][0]
+
+    # 1번 이상 움직이는 경우
+    for j in range(1, w+1):
+        # 내가 2에 있는데 2에서 자두가 있는 경우
+        if data[i] == 2 and j % 2 == 1:
+            d[i][j] = max(d[i-1][j], d[i-1][j-1]) + 1
+        elif data[i] == 1 and j % 2 == 0:
+            d[i][j] = max(d[i-1][j], d[i-1][j-1]) + 1
+        else:
+            d[i][j] = max(d[i-1][j], d[i-1][j-1])
+
+print(max(d[t]))
---
+++ 이코테TEST/DFSBFS/연구소.py
@@ -0,0 +1,62 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+n, m = map(int, input().split())
+
+graph = [[0]*m for _ in range(n)]
+data = []
+for i in range(n):
+    data.append(list(map(int, input().split())))
+
+
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m:
+            if graph[nx][ny] == 0:  # 0인가 2인가?
+                virus(nx, ny)
+
+
+def score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 0:
+                score += 1
+    return score
+
+
+res = 0
+
+
+def dfs(count):
+    global res
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                graph[i][j] = data[i][j]
+
+        for i in range(n):
+            for j in range(m):
+                if graph[i][j] == 2:
+                    virus(i, j)
+        res = max(res, score())
+        return
+
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 0:
+                data[i][j] = 1
+                count += 1
+                dfs(count)
+                data[i][j] = 0
+                count -= 1
+
+
+dfs(0)
+print(res)
