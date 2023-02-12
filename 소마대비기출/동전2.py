67d8604d20a9a2b064d8c4dd477fbff6bcb6fb2e
jung su young <j9972@naver.com>
2023-02-12 16:06:50 +0900

    bfs, 최단경로

--- 소마대비기출/동전2.py
+++ 소마대비기출/동전2.py
@@ -7,3 +7,16 @@ n, k = map(int, input().split())
 coin = []
 for i in range(n):
     coin.append(int(input()))
+
+d = [10001] * (k+1)
+d[0] = 0
+
+for i in range(len(coin)):
+    for j in range(coin[i], k+1):
+        d[i] = min(d[i], d[j-coin[i]] + 1)
+
+
+if d[n-1] != 10001:
+    print(d[n-1])
+else:
+    print(-1)
---
+++ 소마대비기출/스타트링크.py
@@ -0,0 +1,24 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, start, end, up, down = map(int, input().split())
+
+d = [-1] * (n+1)
+d[start] = 0
+q = deque()
+q.append(start)
+
+while q:
+    now = q.popleft()
+
+    if now == end:
+        print(d[now])
+        exit(0)
+
+    for nx in [now - down, now + up]:
+        if 0 < nx <= n and d[nx] == -1:
+            q.append(nx)
+            d[nx] = d[now] + 1
+
+print('use the stairs')
