23dc2b1fd6e570342077a16879ba23d9fbd448f8
jung su young <j9972@naver.com>
2023-02-13 23:48:37 +0900

    using combination for backTracking question

--- 소마대비기출/감소하는수.py
+++ 소마대비기출/감소하는수.py
@@ -1,2 +1,20 @@
 # 골드 5 - 1038
+from itertools import combinations as cb
+import sys
+input = sys.stdin.readline
+
 n = int(input())
+res = []
+nums = [i for i in range(10)]
+
+for i in range(1, 11):
+    info = list(cb(nums, i))
+    for j in info:
+        num = ''.join(list(map(str, reversed(j))))
+        res.append(int(num))
+res.sort()
+
+if n >= len(res):
+    print(-1)
+else:
+    print(res[n])
