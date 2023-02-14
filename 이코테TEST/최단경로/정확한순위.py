067f8625c8507809ad1ae92a58d1d491831dc997
jung su young <j9972@naver.com>
2023-02-14 23:10:18 +0900

    플로이드

---
+++ 이코테TEST/최단경로/정확한순위.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+INF = int(1e9)
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    graph[i][i] = 0
+
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+res = 0
+for i in range(1, n+1):
+    cnt = 0
+    for j in range(1, n+1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            cnt += 1
+    if cnt == n:
+        res += 1
+print(res)
