870a10f6eb609bb492a1e23318635f7bf2fc9cd7
jung su young <j9972@naver.com>
2023-02-15 15:28:29 +0900

    1이될때까지

---
+++ 이코테TEST/그리디/1이될때까지.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+cnt = 0
+while True:
+    if n == 1:
+        break
+    if n % k == 0:
+        n //= k
+        cnt += 1
+    else:
+        n -= 1
+        cnt += 1
+print(cnt)
