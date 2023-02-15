20ced3e997604f9adb297105ee1c516609c9346c
jung su young <j9972@naver.com>
2023-02-15 17:45:38 +0900

    못생긴 수

--- 이코테TEST/DP/퇴사.py
+++ 이코테TEST/DP/퇴사.py
@@ -1,20 +1,22 @@
 import sys
 input = sys.stdin.readline
 
+n = int(input())
 t = []
 p = []
-n = int(input())
 
+d = [0] * (n+1)
 for i in range(n):
     a, b = map(int, input().split())
     t.append(a)
     p.append(b)
 
-d = [0] * (n+1)
-
-for i in range(n-1, -1, -1):
+mv = 0
+for i in range(1, n):
     time = t[i] + i
     if time <= n:
-        d[i] = max(d[i], p[i] + d[time])
-        
-print(d[n])
+        d[i] = max(p[i]+d[time], mv)
+        mv = d[i]
+    else:
+        d[i] = mv
+print(mv)
