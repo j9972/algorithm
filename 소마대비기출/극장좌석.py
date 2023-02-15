e6516ecca3502f832c79641c7bfe847d9450906b
jung su young <j9972@naver.com>
2023-02-15 22:00:05 +0900

    극장좌석

--- 소마대비기출/극장좌석.py
+++ 소마대비기출/극장좌석.py
@@ -4,10 +4,36 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-d = [0 for _ in range(1, n+1)]
 
-for i in range(m):
-    data = int(input())
-    d[data-1] = 1
+if n >= 2:
+    d = [i for i in range(1, n+1)]
 
-# print(d) -> [0,0,0,1,0,0,1,0,0]
+    for i in range(m):
+        data = int(input())
+        d[data-1] = 0
+
+    res = []
+    cnt = 0
+    for i in range(n):
+        if d[i] != 0:
+            cnt += 1
+            if i == n-1:
+                res.append(cnt)
+        else:
+            res.append(cnt)
+            cnt = 0
+
+    dp = [1] * (n+1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    ans = 1
+    for i in res:
+        ans *= dp[i]
+    print(ans)
+else:
+    for _ in range(m):
+        v = int(input())
+    print(1)
