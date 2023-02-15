d67406a853967dbfec9d47e65f62bb0cc40e792d
jung su young <j9972@naver.com>
2023-02-15 15:58:33 +0900

    바닥공사

---
+++ 이코테TEST/DP/바닥공사.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+d = [0] * (n+1)
+d[0] = 0
+d[1] = 1
+d[2] = 3
+
+for i in range(3, n+1):
+    d[i] = (d[i-1] + d[i-2] * 2) % 796796
+print(d[n])
