31ef723c11c6a9e3c9f98495686dd8df774df85f
jung su young <j9972@naver.com>
2023-02-15 17:58:43 +0900

    모험가 길드

--- 이코테TEST/그리디/모험가길드.py
+++ 이코테TEST/그리디/모험가길드.py
@@ -1,15 +1,17 @@
 import sys
-from tokenize import group
 input = sys.stdin.readline
 
 n = int(input())
 data = list(map(int, input().split()))
+
 data.sort()
 
-cnt, group = 0, 0
+
+cnt = 0
+g = 0
 for i in data:
     cnt += 1
     if cnt >= i:
-        group += 1
+        g += 1
         cnt = 0
-print(group)
+print(g)
