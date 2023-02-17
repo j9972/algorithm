6e67b38be31aa1b52173a4aca2c31d76d2bce426
jung su young <j9972@naver.com>
2023-02-18 01:28:12 +0900

    블록이동하기

--- 이코테TEST/DFSBFS/블록이동하기.py
+++ 이코테TEST/DFSBFS/블록이동하기.py
@@ -1,2 +1,63 @@
-import sys
-input = sys.stdin.readline
+
+from collections import deque
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def nextMove(pos,board):
+    pos = list(pos)
+    posList = []
+
+    pos_x1 = pos[0][0]
+    pos_y1 = pos[0][1]
+    pos_x2 = pos[1][0]
+    pos_y2 = pos[1][1]
+
+    for i in range(4):
+        next_pos_x1 = pos_x1 + dx[i]
+        next_pos_y1 = pos_y1 + dy[i]
+        next_pos_x2 = pos_x2 + dx[i]
+        next_pos_y2 = pos_y2 + dy[i]
+
+        if board[next_pos_x1][next_pos_y1] == 0 and board[next_pos_x2][next_pos_y2] == 0:
+            posList.append({(next_pos_x1,next_pos_y1), (next_pos_x2,next_pos_y2)})
+    
+    if pos_x1 == pos_x2:
+        for i in [-1,1]:
+            if board[pos_x1+i][pos_y1] == 0 and board[pos_x2+i][pos_y2] == 0:
+                posList.append({(pos_x1,pos_y1),(pos_x1+i,pos_y1)})
+                posList.append({(pos_x2,pos_y2),(pos_x2+i,pos_y2)})
+    elif pos_y1 == pos_y2:
+        for i in [-1,1]:
+            if board[pos_x1][pos_y1+i] == 0 and board[pos_x2][pos_y2+i] == 0:
+                posList.append({(pos_x1,pos_y1),(pos_x1,pos_y1+i)})
+                posList.append({(pos_x2,pos_y2),(pos_x2,pos_y2+i)})
+    return posList
+
+
+def solution(board):
+
+    n = len(board)
+    newBoard= [[1]*(n+2) for _ in range(n+2)]
+    for i in range(n):
+        for j in range(n):
+            newBoard[i+1][j+1] = board[i][j]
+    
+    q = deque([])
+    pos = {(1,1),(1,2)}
+    visited = []
+    visited.append(pos)
+    q.append((pos,0))
+
+    while q:
+        pos,cost = q.popleft()
+
+        if (n,n) in pos:
+            return cost
+        
+        for next in nextMove(pos, newBoard):
+            if next not in visited:
+                visited.append(next)
+                q.append((next, cost+1))
+
+    return cost
--- 이코테TEST/DFSBFS/인구이동.py
+++ 이코테TEST/DFSBFS/인구이동.py
@@ -1,2 +1,56 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
+
+n,l,r = map(int,input().split())
+
+data = []
+for i in range(n):
+    data.append(list(map(int,input().split())))
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def process(x,y,index):
+    q= deque()
+    q.append((x,y))
+
+    united = []
+    united.append((x,y))
+
+    summary = data[x][y]
+    union[x][y] = index
+    cnt = 1
+
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0<=nx<n and 0<=ny<n and union[nx][ny] == -1:
+                if l <= abs(data[nx][ny] - data[x][y]) <= r:
+                    summary += data[nx][ny]
+                    union[nx][ny] = index
+                    q.append((nx,ny))
+                    united.append((nx,ny))
+                    cnt += 1
+    for x,y in united:
+        data[x][y] = summary // cnt
+    return cnt
+
+
+
+days = 0
+while True:
+    union = [[-1] * (n+1) for _ in range(n+1)]
+    index = 0
+    for i in range(n):
+        for j in range(n):
+            if union[i][j] == -1:
+                index += 1
+                process(i,j,index)
+    if index == n*n:
+        break
+    days += 1
+print(days)
