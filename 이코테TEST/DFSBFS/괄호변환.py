59042b65c862f84026fb0d0f3620a885867a1dfb
jung su young <j9972@naver.com>
2023-02-18 01:56:01 +0900

    괄호변환

--- 이코테TEST/DFSBFS/괄호변환.py
+++ 이코테TEST/DFSBFS/괄호변환.py
@@ -1,2 +1,48 @@
 import sys
 input = sys.stdin.readline
+
+def balance(p):
+    count = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            return i
+def proper(p):
+    count = 0
+    for i in p:
+        if i == '(':
+            count += 1
+        else:
+            if count == 0:
+                return False
+            else:
+                count -= 1
+    return True
+
+def sol(p):
+    ans = ""
+    if p == "":
+        return ""
+    
+    idx = balance(p)
+    v = p[idx+1:]
+    u = p[:idx+1]
+
+    if proper(p):
+        ans = u + sol(v)
+    else:
+        ans += '('
+        ans += sol(v)
+        ans += ')'
+
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        ans += ''.join(u)
+    return ans
