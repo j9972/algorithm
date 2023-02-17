2e688e5565d2df64ddcd3d0b3bc71bbc28a5c3dc
jung su young <j9972@naver.com>
2023-02-18 01:33:10 +0900

    연산자 끼워넣기

--- 이코테TEST/DFSBFS/연산자끼워넣기.py
+++ 이코테TEST/DFSBFS/연산자끼워넣기.py
@@ -1,2 +1,35 @@
 import sys
 input = sys.stdin.readline
+
+n = int(input())
+data = list(map(int,input().split()))
+
+add,minus,multi,div = map(int,input().split())
+mxv = int(-1e9)
+miv = int(1e9)
+
+def dfs(i,now):
+    global add,minus,multi,div,mxv,miv
+    if i == n:
+        mxv = max(mxv, now)
+        miv = min(miv, now)
+    else:
+        if add > 0:
+            add -= 1
+            dfs(i+1, now + data[i])
+            add += 1
+        if minus > 0:
+            minus -= 1
+            dfs(i+1, now - data[i])
+            minus += 1
+        if multi > 0:
+            multi -= 1
+            dfs(i+1, now * data[i])
+            multi += 1
+        if div > 0:
+            div -= 1
+            dfs(i+1, int(now / data[i]))
+            div += 1
+dfs(1,data[0])
+print(mxv)
+print(miv)
