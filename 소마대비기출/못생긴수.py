aa5840e84eb529cd8a7cae75de4e78ea441074ef
jung su young <j9972@naver.com>
2023-02-18 21:12:29 +0900

    못생긴수

---
+++ 소마대비기출/못생긴수.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+next2 ,next3, next5 = 2,3,5
+i2=i3=i5=0
+
+d = [0] * (n+1)
+d[0] = 1
+for i in range(1,n+1):
+    d[i] = min(next2,next3,next5)
+    if d[i] == next2:
+        i2 += 1
+        next2 = d[i2] * 2
+    if d[i] == next3:
+        i3 += 1
+        next3 = d[i3] * 3
+    if d[i] == next5:
+        i5 += 1
+        next5 = d[i5] * 5
+print(d[n-1])
