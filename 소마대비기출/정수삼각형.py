3186e526d9ba6fc31bc28cf30b387526f2c2f697
jung su young <j9972@naver.com>
2023-02-18 20:56:29 +0900

    정수삼각형

--- 소마대비기출/정수삼각형.py
+++ 소마대비기출/정수삼각형.py
@@ -1,26 +1,23 @@
-# 실버 - 1932
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 d = []
 for i in range(n):
-    d.append(list(map(int, input().split())))
+    d.append(list(map(int,input().split())))
 
-for i in range(1, n):
-    for j in range(i):
-        # left_up
+for i in range(1,n):
+    for j in range(i+1):
         if j == 0:
             left_up = 0
         else:
             left_up = d[i-1][j-1]
-
-        # up
-        if i == j:
+        
+        if j == i:
             up = 0
         else:
             up = d[i-1][j]
 
-        d[i][j] += max(up, left_up)
+        d[i][j] += max(up,left_up)
 
 print(max(d[n-1]))
