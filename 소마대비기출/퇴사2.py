5ffa7943befd950e13e0a3d88fb44baf714da641
jung su young <j9972@naver.com>
2023-02-18 15:18:42 +0900

    xhltk2

---
+++ 소마대비기출/퇴사2.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+t = []
+p = []
+
+d = [0] * (n+1)
+
+for i in range(n):
+    a,b = map(int,input().split())
+    t.append(a)
+    p.append(b)
+
+mv = 0 
+for i in range(n-1,-1,-1):
+    time = t[i] + i
+    if time <= n:
+        d[i] = max(d[time]+p[i], mv)
+        mv = d[i]
+    else:
+        d[i] = mv
+print(mv)
