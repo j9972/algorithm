77ecc794353ca19ae545b142333be0207af15d3c
jung su young <j9972@naver.com>
2023-02-19 15:26:41 +0900

    N과M으로 back tracking

--- 소마대비기출/N과M_3.py
+++ 소마대비기출/N과M_3.py
@@ -1,4 +1,4 @@
-# 실버 3 - 15649
+#실버 3 - 15651
 import sys
 input = sys.stdin.readline
 
@@ -7,14 +7,12 @@ n,m = map(int,input().split())
 res = []
 
 def dfs():
-    # 종료시점
     if len(res) == m:
         print(' '.join(map(str,res)))
         return
     else:
         for i in range(1,n+1):
-            if i not in res:
-                res.append(i)
-                dfs()
-                res.pop()
+            res.append(i)
+            dfs()
+            res.pop()
 dfs()
--- 소마대비기출/N과M_4.py
+++ 소마대비기출/N과M_4.py
@@ -1,18 +1,18 @@
-#실버 3 - 15650
+#실버 3 - 15652
 import sys
 input = sys.stdin.readline
 
 n,m = map(int,input().split())
 
 res = []
+
 def dfs(start):
     if len(res) == m:
         print(' '.join(map(str,res)))
         return
     else:
         for i in range(start,n+1):
-            if i not in res:
-                res.append(i)
-                dfs(i)
-                res.pop()
+            res.append(i)
+            dfs(i)
+            res.pop()
 dfs(1)
