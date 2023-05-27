624d1d3f04398d8e4b44d4d10633dac4263da572
jung su young <j9972@naver.com>
2023-05-27 11:00:26 +0900

    5/27

--- 구현( 이코테 + @ )/올바른 배열.py
+++ 구현( 이코테 + @ )/올바른 배열.py
@@ -9,3 +9,18 @@ for i in range(n):
 
 data.sort()
 
+maxVal = 0
+for i in range(n):
+    cnt = 0
+    res = []
+    for j in range(data[i]+1,data[i]+5):
+        res.append(j)
+    
+    for d in data:
+        if d in res:
+            cnt += 1
+    
+    if cnt > maxVal:
+        maxVal = cnt
+print(4-maxVal)
+#data의 모든 원소들을 기준으로 +4까지 전부 체크해서 그 안에 들어오는 data의 개수 체크해서 가장 많은거 고르기
