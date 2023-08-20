282a5ff32869092bb42db638c4a0d7e881e91875
정수영 <j9972@naver.com>
2023-08-20 19:49:27 +0900

    배열 회전

---
+++ 프로그래머스/test.py
@@ -0,0 +1,39 @@
+def solution(rows, cols, queries):
+    ans = []
+    
+    cnt = 1
+    board = [[0]*(cols+1) for _ in range(rows+1)]
+    for i in range(1,rows+1):
+        for j in range(1,cols+1):
+            board[i][j] = cnt
+            cnt += 1
+    
+    for x1,y1,x2,y2 in queries:
+        tmp = board[x1][y1]
+        mini = tmp 
+        
+        for k in range(x1,x2):
+            test = board[k+1][y1]
+            board[k][y1] = test
+            mini = min(mini, test)
+
+        for k in range(y1,y2):
+            test = board[x2][k+1]
+            board[x2][k] = test
+            mini = min(mini, test)
+
+        for k in range(x2,x1,-1):
+            test = board[k-1][y2]
+            board[k][y2] = test
+            mini = min(mini, test)
+
+        for k in range(y2,y1,-1):
+            test = board[x1][k-1]
+            board[x1][k] = test
+            mini = min(mini, test)
+
+        board[x1][y1+1] = tmp
+        ans.append(mini)
+    print(ans)   
+    return ans
+solution(3, 3, [[1, 1, 2, 2], [1, 2, 2, 3], [2, 1, 3, 2], [2, 2, 3, 3]])
---
+++ 프로그래머스/행렬 테두리 회전하기.py
@@ -0,0 +1,91 @@
+# row = 행, col = 열 
+# 1번째 방법
+# def solution(rows, columns, queries):
+
+#     board = [[0]*(columns+1) for _ in range(rows+1)]
+#     ans = []
+    
+#     cnt = 1
+#     for i in range(1,rows+1):
+#         for j in range(1,columns+1):
+#             board[i][j] = cnt
+#             cnt += 1
+
+#     for x1, y1, x2, y2 in queries:
+#         tmp = board[x1][y1]
+#         mini = tmp
+        
+#         # 왼쪽 세로
+#         for k in range(x1,x2):
+#             test = board[k+1][y1]
+#             board[k][y1] = test
+#             mini = min(mini, test)
+
+#         # 아래쪽 가로
+#         for k in range(y1,y2):
+#             test = board[x2][k+1]
+#             board[x2][k] = test
+#             mini = min(mini, test)
+
+#         # 오른쪽 세로
+#         for k in range(x2,x1,-1):
+#             test = board[k-1][y2]
+#             board[k][y2] = test
+#             mini = min(mini, test)
+
+#         # 위쪽 가로
+#         for k in range(y2,y1,-1):
+#             test = board[x1][k-1]
+#             board[x1][k] = test
+#             mini = min(mini, test)
+
+#         board[x1][y1+1] = tmp
+#         #print(board)
+#         ans.append(mini)
+#     #print(ans)
+#     return ans
+
+# 2번째 방법
+from collections import deque
+def solution(rows, columns, queries):
+    ans = deque()
+    res = []
+
+    board = [[i+columns*j for i in range(1,columns+1)] for j in range(rows)]
+
+    # cnt = 1
+    # for i in range(1,rows+1):
+    #     for j in range(1,columns+1):
+    #         board[i][j] = cnt
+    #         cnt += 1
+    
+    for i in queries:
+        x1, y1, x2, y2 = i[0]-1,i[1]-1,i[2]-1,i[3]-1
+        for x in range(y2-y1):
+            ans.append(board[x1][x2+x])
+        for y in range(x2-x1):
+            ans.append(board[x1+y][y2])
+        for z in range(y2-y1):
+            ans.append(board[x2][y2-z])
+        for k in range(x2-x1):
+            ans.append(board[x2-k][y1])
+        
+        ans.rotate(1)
+        res.append(min(ans))
+
+        for x in range(y2-y1):
+            board[x1][y1+x] = ans[0]
+            ans.popleft()
+        for y in range(x2-x1):
+            board[x1+y][y2] = ans[0]
+            ans.popleft()
+        for z in range(y2-y1):
+            board[x2][y2-z] = ans[0]
+            ans.popleft()
+        for k in range(x2-x1):  
+            board[x2-k][y1] = ans[0]
+            ans.popleft()  
+    print(res)
+    return res
+
+solution(3, 3, [[1, 1, 2, 2], [1, 2, 2, 3], [2, 1, 3, 2], [2, 2, 3, 3]])
