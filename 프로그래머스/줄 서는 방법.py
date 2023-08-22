c6c70111c4bfbb7365d2a709c850521fe5f832ec
정수영 <j997@naver.com>
2023-08-22 14:43:20 +0900

    change user name

--- 프로그래머스/줄 서는 방법.py
+++ 프로그래머스/줄 서는 방법.py
@@ -29,3 +29,23 @@ def solution(n, k):
     
     print(res)
 solution(3, 5)
+
+def solution(n, k):
+    res = []
+    data = [i for i in range(1,n+1)]
+
+    # fact를 dp로 표현
+    d = [1] * (n+1)
+    d[1] = 1
+
+    for i in range(2, n+1):
+	    d[i] = d[i-1] * i
+    
+    while n > 0:
+        res.append(data[(k-1) // d[n-1]])
+        data.remove(data[(k-1) // d[n-1]])
+
+        k = k % d[n-1]
+        n -= 1
+    
+    return res
