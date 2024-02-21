1db15da224f03bd569e4bfebe9220ecf56c34d6a
j9972 <j9972@naver.com>
2024-02-22 01:30:44 +0900

    sql null part

---
+++ SQL/업그레이드 할 수없는 아이템구하기.sql
@@ -0,0 +1,9 @@
+select 
+ITEM_ID, ITEM_NAME, RARITY
+from ITEM_INFO
+where ITEM_ID not in (
+    select PARENT_ITEM_ID
+    from ITEM_TREE
+    where PARENT_ITEM_ID is not null
+)
+order by ITEM_ID desc
