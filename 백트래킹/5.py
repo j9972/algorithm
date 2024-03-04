b85b6d13354efdcf244a6c1863f48f14d86e4217
j9972 <j9972@naver.com>
2024-03-04 21:47:51 +0900

    5

---
+++ 백트래킹/5.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+visited = [False] * (n+1)
+
+ans = list()
+
+def Print():
+    for i in ans:
+        print(i,end=' ')
+    print()
+
+def choose(idx):
+    if idx == n:
+        Print()
+        return
+    
+    for i in range(n,0,-1):
+        if not visited[i]:
+            visited[i] = True
+            ans.append(i)
+
+            choose(idx+1)
+
+            ans.pop()
+            visited[i] = False
+
+choose(0)
