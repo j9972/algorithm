ea64ee56999d6f0c2e4cc5a60a0dd72c464529a2
j9972 <j9972@naver.com>
2024-03-06 16:55:51 +0900

    스타트와 링크 2

---
+++ 삼성SW역량/스타트와링크_2.py
@@ -0,0 +1,27 @@
+import sys
+from itertools import combinations as cb
+
+n = int(input())
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+# print("arr: {}".format(arr))
+# print("*arr: {} , zip(*arr) : {}".format(*arr, zip(*arr)))
+
+mem = [sum(i) + sum(j) for i,j in zip(arr, zip(*arr))]
+
+# for i,j in zip(arr, zip(*arr)):
+#     print(i,j)
+#     print(sum(i) , sum(j))
+
+print(mem)
+
+tot = sum(mem) // 2
+
+res = sys.maxsize
+
+for i in cb(mem, n//2):
+    res = min(res, abs(tot - sum(i)))
+print(res)
