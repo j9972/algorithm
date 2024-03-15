f66255259125ea5dbcaf5a16ed753985d2b51025
j9972 <j9972@naver.com>
2024-03-15 16:37:03 +0900

    감시

--- 삼성SW역량/감시.py
+++ 삼성SW역량/감시.py
@@ -1,5 +1,5 @@
 import sys
-import copy
+from itertools import product
 
 n,m = map(int,input().split())
 arr = [
@@ -7,75 +7,69 @@ arr = [
     for _ in range(n)
 ]
 
-maps = []
-
-# 동 남 서 북 ( 시계 방향 )
-dxs,dys = [0,1,0,-1],[1,0,-1,0]
+temp = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
 
 min_val = n * m
-cctv_cnt = 0
+
 cctv = []
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] in [1,2,3,4,5]:
+            cctv.append((i,j,arr[i][j]))
+
+dxs,dys = [0,1,0,-1],[1,0,-1,0]
 
 cctv_direction = [
     [],
-    [[0], [1], [2], [3]],
-    [[0, 2], [1, 3]],
-    [[0, 1], [1, 2], [2, 3], [3, 0]],
-    [[0, 1, 2], [1, 2, 3], [0, 2, 3], [1, 0, 3]],
-    [[0, 1, 2, 3]]
+    [[0],[1],[2],[3]],
+    [[0,2],[1,3]],
+    [[0,1],[1,2],[2,3],[3,0]],
+    [[0,1,2],[1,2,3],[2,3,0],[3,0,1]],
+    [[0,1,2,3]]
 ]
-
-for i in range(n):
-    data = list(map(int, input().split()))
-    maps.append(data)
-    for j in range(m):
-        if data[j] in [1, 2, 3, 4, 5]:
-            cctv.append((i,j,data[j]))
-            cctv_cnt += 1
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
+
+def init_temp():
+    for i in range(n):
+        for j in range(m):
+            temp[i][j] = arr[i][j]
 
 def getSize():
     cnt = 0
     for i in range(n):
         for j in range(m):
-            if maps[i][j] == 0:
+            if temp[i][j] == 0:
                 cnt += 1
     return cnt
 
-def watching(maps, direct, x, y):
-    
-    for d in direct:
-        cur_x, cur_y = x, y
+tmp = list()
+for _, _, val in cctv:
+    tmp.append(cctv_direction[val])
 
-        while True:
-            nx,ny = cur_x + dxs[d], cur_y + dys[d]
+for prod in list(product(*tmp)):
+    init_temp()
 
-            if not in_range(nx,ny):
-                break
+    for i in range(len(cctv)):
+        for d in prod[i]:
+            x,y,_ = cctv[i][0],cctv[i][1],cctv[i][2]
 
-            if maps[nx][ny] == 6:
-                break
+            while True:
+                nx,ny = x + dxs[d],y + dys[d]
 
-            if maps[nx][ny] == 0:
-                maps[nx][ny] = 1
+                if not in_range(nx,ny):
+                    break
+                if temp[nx][ny] == 6:
+                    break
+                elif temp[nx][ny] == 0:
+                    temp[nx][ny] = -1
 
+                x,y = nx,ny
+            
+    min_val = min(min_val, getSize())
 
-
-def choose(idx, cnt):
-    global min_val, cctv_cnt
-
-    if cnt == cctv_cnt:
-        min_val = min(min_val, watching())
-        return
-    
-    temp = copy.deepcopy(cnt)
-    x,y,num = cctv[idx]
-    for d in cctv_direction[num]:
-        watching(temp, d, x, y)
-        choose(idx+1, temp)
-        temp = copy.deepcopy(cnt)
-
-choose(0, maps)
 print(min_val)
