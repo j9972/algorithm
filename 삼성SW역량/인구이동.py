18a651060cdc3838a173bc9946f2d0eb55ac0562
j9972 <j9972@naver.com>
2024-03-20 15:46:25 +0900

    인구이도 2

--- 삼성SW역량/인구이동.py
+++ 삼성SW역량/인구이동.py
@@ -3,64 +3,66 @@ from collections import deque
 
 n,L,R = map(int,input().split())
 
+tot_days = 0
+
 arr = [
     list(map(int,input().split()))
     for _ in range(n)
 ]
 
-# 상 우 하 좌
-dxs,dys = [-1,0,1,0], [0,1,0,-1]
+visited = [
+    [False for _ in range(n)]
+    for _ in range(n)
+]
 
-tot_days = 0
+dxs,dys = [-1,1,0,0], [0,0,-1,1]
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
 
-def can_go(nx,ny):
-    return in_range(nx,ny) and not visited[nx][ny]
+def init_visited():
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = False
 
-def process(x,y):
+def can_go(x,y):
+    return in_range(x,y) and not visited[x][y]
+
+def bfs(x,y):
     q = deque()
     q.append((x,y))
 
     united = []
     united.append((x,y))
 
-    poplutaion = arr[x][y]
+    population = arr[x][y]
     visited[x][y] = True
 
-    while q:
-        x,y = q.popleft()
+    for dx,dy in zip(dxs,dys):
+        nx,ny = x + dx, y + dy
 
-        for dx,dy in zip(dxs,dys):
-            nx,ny = x + dx, y +dy
-
-            if can_go(nx,ny):
-                if L <= abs(arr[nx][ny] - arr[x][y]) <= R:
-                    poplutaion += arr[nx][ny]
-                    visited[nx][ny] = True
-                    q.append((nx,ny))
-                    united.append((nx,ny))
-            
+        if can_go(nx,ny):
+            if L <= abs(arr[nx][ny] - arr[x][y]) <= R:
+                population += arr[nx][ny]
+                visited[nx][ny] = True
+                q.append((nx,ny))
+                united.append((nx,ny))
+    
     for x,y in united:
-        arr[x][y] = poplutaion // len(united)
-
+        arr[x][y] = population // len(united)
 
 while True:
-    visited = [
-        [False for _ in range(n)]
-        for _ in range(n)
-    ]
+    init_visited()
 
-    idx = 0
+    cnt = 0
 
     for i in range(n):
         for j in range(n):
             if not visited[i][j]:
-                idx += 1
-                process(i,j)
+                cnt += 1
+                bfs(i,j)
     
-    if idx == n**2:
+    if cnt == n**2:
         break
     tot_days += 1
 
