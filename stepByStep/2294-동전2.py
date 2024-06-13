854e741ba1bcf47f87eaf27d8ca4af3e1ec2d5be
j9972 <j9972@naver.com>
2024-06-13 23:39:44 +0900

    #19 동전2

--- stepByStep/2294-동전2.py
+++ stepByStep/2294-동전2.py
@@ -8,11 +8,15 @@ coin = [
     for _ in range(n)
 ]
 
-d = [0] * (k+1)
-d[0] = 1
+d = [sys.maxsize] * (k+1)
+d[0] = 0
 
 for i in coin:
     for j in range(1,k+1):
         if j - i >= 0:
-            d[j] += d[j-i]
-print(d[k])
+            d[j] = min(d[j], d[j-i] + 1)
+
+if d[k] == sys.maxsize:
+    print(-1)
+else:
+    print(d[k])
