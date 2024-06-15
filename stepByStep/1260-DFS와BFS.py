23c664b4b7b0a3df84bf9313e008a4a2ee14c931
j9972 <j9972@naver.com>
2024-06-15 13:30:40 +0900

    #24 DFS와  BFS

---
+++ stepByStep/1260-DFS와BFS.py
@@ -0,0 +1,47 @@
+from collections import deque
+import sys
+sys.setrecursionlimit(10**9)
+
+n,m,v = map(int,input().split())
+
+graph = [
+    [] * (n+1)
+    for _ in range(n+1)
+]
+
+for i in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(n):
+    graph[i].sort()
+
+visited1 = [False] * (n+1)
+visited2 = [False] * (n+1)
+
+def dfs(start):
+    visited1[start] = True
+    print(start, end=' ')
+
+    for i in graph[start]:
+        if not visited1[i]:
+            dfs(i)
+
+def bfs(start):
+    visited2[start] = True
+    q = deque([start])
+
+    while q:
+        data = q.popleft()
+        print(data, end=' ')
+
+        for i in graph[data]:
+            if not visited2[i]:
+                visited2[i] = True
+                q.append(i)
+
+
+dfs(v)
+print()
+bfs(v)
--- stepByStep/17070-파이프 옮기기1.py
+++ stepByStep/17070-파이프 옮기기1.py
@@ -13,7 +13,7 @@ d = [
     for _ in range(3)
 ]
 
-# 0가로, 1세로, 2대각
+# 0가로, 1세로, 2대각 -> d[방향][행][열]
 d[0][0][1] = 1
 for i in range(2,n):
     if arr[0][i] == 0:
@@ -22,13 +22,11 @@ for i in range(2,n):
 for i in range(1,n):
     for j in range(1,n):
 
-        # 대각
-        if arr[i][j] == 0 and arr[i][j-1] == 0 and arr[i-1][j] == 0:
-            d[2][i][j] = d[0][i-1][j-1] + d[1][i-1][j-1] + d[2][i-1][j-1]
-
-        # 세로, 가로
+        if arr[i][j] == 0 and arr[i-1][j] == 0 and arr[i][j-1] == 0:
+            d[2][i][j] = d[2][i-1][j-1] + d[1][i-1][j-1] + d[0][i-1][j-1]
+        
         if arr[i][j] == 0:
-            d[0][i][j] = d[0][i][j-1] + d[2][i][j-1]
             d[1][i][j] = d[1][i-1][j] + d[2][i-1][j]
-
+            d[0][i][j] = d[0][i][j-1] + d[2][i][j-1]
+        
 print(d[0][n-1][n-1] + d[1][n-1][n-1] + d[2][n-1][n-1])
