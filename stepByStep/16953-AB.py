9589001078a548777c9c4b3a700a2385059a0cc0
j9972 <j9972@naver.com>
2024-06-16 15:10:28 +0900

    #29 A->B [2]

--- stepByStep/16953-AB.py
+++ stepByStep/16953-AB.py
@@ -1,30 +1,48 @@
+# a,b = map(int,input().split())
+
+# min_val = 0
+# flag = False
+
+# while not flag:
+
+#     if b % 10 == 1:
+#         b = int(str(b)[:-1])
+#         min_val += 1
+#     elif b % 2 == 0:
+#         b //= 2
+#         min_val += 1
+#     else:
+#         flag = True
+#         break
+
+#     if a > b:
+#         flag = True
+#         break
+
+#     if a == b:
+#         min_val += 1
+#         break
+
+
+# if flag:
+#     print(-1)
+# else:
+#     print(min_val)
+
 a,b = map(int,input().split())
+min_val = 1
 
-min_val = 0
-flag = False
-
-while not flag:
+while b!=a:
+    min_val += 1
+    temp = b
 
     if b % 10 == 1:
-        b = int(str(b)[:-1])
-        min_val += 1
+        b //= 10
     elif b % 2 == 0:
         b //= 2
-        min_val += 1
-    else:
-        flag = True
+    
+    if temp == b:
+        print(-1)
         break
-
-    if a > b:
-        flag = True
-        break
-
-    if a == b:
-        min_val += 1
-        break
-
-
-if flag:
-    print(-1)
 else:
     print(min_val)
