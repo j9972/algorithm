7c26c930246275511a07639e486f653a9dec36d2
j9972 <j9972@naver.com>
2024-06-19 14:00:05 +0900

    #33 이모티콘_2

--- stepByStep/14226-이모티콘.py
+++ stepByStep/14226-이모티콘.py
@@ -3,13 +3,13 @@ from collections import deque
 s = int(input())
 
 d = [[0] * (1001) for _ in range(1001)]
-d[1][0] = True
+d[1][0] = 1
 
 q = deque()
-q.append([1,0,0]) # 화면, 클립, 연산수
+q.append([1,0,0])
 
-def in_range(monitor, clip):
-    return 0<=monitor<1001 and 0<=clip<1001
+def in_range(a,b):
+    return 0<=a<1001 and 0<=b<1001
 
 while q:
     monitor, clip, cnt = q.popleft()
@@ -17,20 +17,17 @@ while q:
     if monitor == s:
         print(cnt)
         break
-    
+
     for i in range(3):
         if i == 0:
-            new_monitor , new_clip = monitor, monitor
+            new_monitor, new_clip = monitor, monitor
         elif i == 1:
-            new_monitor , new_clip = clip + monitor, clip
+            new_monitor, new_clip = clip + monitor, clip
         else:
-            new_monitor , new_clip = monitor - 1, clip
+            new_monitor, new_clip = monitor - 1, clip
         
-        if not in_range(new_monitor,new_clip) or d[new_monitor][new_clip] == 1:
+        if not in_range(new_monitor, new_clip) or d[new_monitor][new_clip] == 1:
             continue
 
         d[new_monitor][new_clip] = 1
         q.append([new_monitor, new_clip, cnt + 1])
-        
-
-
