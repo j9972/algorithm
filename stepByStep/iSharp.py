75603b9ee049659c83b05b13915077f2fdd42192
j9972 <j9972@naver.com>
2024-06-27 14:59:19 +0900

    #39 iSharp

---
+++ stepByStep/iSharp.py
@@ -0,0 +1,33 @@
+iSharp = list(map(str, input().split()))
+
+iSharp_list = []
+
+for i in range(1,len(iSharp)):
+
+    var = ''
+    temp = iSharp[0]
+    idx = 0
+
+    #print("iSharp[i] : {}, len(iSharp[i])-1 : {}".format(iSharp[i], len(iSharp[i])-1))
+    for j in range(len(iSharp[i]) - 1):
+        if not iSharp[i][j] in ['[',']','*','&']:
+            var += iSharp[i][j]
+            idx = j
+        else:
+            break
+    #print("var : {}, idx : {}".format(var, idx))
+
+    for j in iSharp[i][idx+1:-1][::-1]:
+        if j == ']':
+            continue
+        elif j == '[':
+            temp += '[]'
+        else:
+            temp += j
+
+    temp += ' ' + var
+
+    iSharp_list.append(temp)
+
+for i in iSharp_list:
+    print(i+';')
