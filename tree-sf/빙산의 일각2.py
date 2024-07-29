c23199535ef81e18e0d1aa397a00ce22b55b4a94
j9972 <j9972@naver.com>
2024-07-29 15:30:36 +0900

    boolean으로 매번 체크하는 방법

---
+++ tree-sf/빙산의 일각2.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+arr = [
+    int(input())
+    for _ in range(n)
+]
+
+max_cnt = 0
+
+for h in range(1,1001):
+    flag = False
+    cnt = 0
+    for i in range(n):
+        if arr[i] - h > 0:
+            if not flag:
+                cnt += 1
+                flag = True
+        else:
+            flag = False
+
+    max_cnt = max(max_cnt ,cnt)
+
+print(max_cnt)
