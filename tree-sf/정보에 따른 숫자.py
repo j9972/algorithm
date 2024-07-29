745b080648eba74f829b8c03809295a2792792fb
j9972 <j9972@naver.com>
2024-07-29 14:45:54 +0900

    새로운 1차원 배열 생성없이 계산 하기

--- tree-sf/정보에 따른 숫자.py
+++ tree-sf/정보에 따른 숫자.py
@@ -29,3 +29,29 @@ for i in range(a,b+1):
     if min_dist_s <= min_dist_n:
         cnt += 1
 print(cnt)
+
+import sys
+
+t,a,b = map(int,input().split())
+
+arr = [
+    list(input().split())
+    for _ in range(t)
+]
+
+cnt = 0
+
+for i in range(a,b+1):
+    min_dist_n, min_dist_s = sys.maxsize, sys.maxsize
+
+    for alpha, idx in arr:
+        idx = int(idx)
+
+        if alpha == 'S':
+            min_dist_s = min(min_dist_s, abs(idx - i))
+        if alpha == 'N':
+            min_dist_n = min(min_dist_n, abs(idx - i))
+
+    if min_dist_s <= min_dist_n:
+        cnt += 1
+print(cnt)
