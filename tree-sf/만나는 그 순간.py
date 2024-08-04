532b6e51d2d322c1a71da48336cb1e09c34ae5f5
j9972 <j9972@naver.com>
2024-08-04 10:58:50 +0900

    + 가 아닌 += 인지 잘 체크 하기

---
+++ tree-sf/만나는 그 순간.py
@@ -0,0 +1,44 @@
+n,m = map(int,input().split())
+
+A = [0] * 1000001
+B = [0] * 1000001
+
+a_idx = 1
+time = 0
+
+for i in range(n):
+    d, t = input().split()
+    t = int(t)
+    time += t
+
+    if d == 'R':
+        for _ in range(t):
+            A[a_idx] = A[a_idx - 1] + 1
+            a_idx += 1
+    else:
+        for _ in range(t):
+            A[a_idx] = A[a_idx - 1] - 1
+            a_idx += 1
+
+
+a_idx = 1
+
+for i in range(m):
+    d, t = input().split()
+    t = int(t)
+
+    if d == 'R':
+        for _ in range(t):
+            B[a_idx] = B[a_idx - 1] + 1
+            a_idx += 1
+    else:
+        for _ in range(t):
+            B[a_idx] = B[a_idx - 1] - 1
+            a_idx += 1
+
+cnt = -1
+for i in range(1,min(1000000, time)+1):
+    if A[i] == B[i]:
+        cnt = i
+        break
+print(cnt)
