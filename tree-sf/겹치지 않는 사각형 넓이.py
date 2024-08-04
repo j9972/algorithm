af9974cbe533fd086e2000b969099cb3290de849
j9972 <j9972@naver.com>
2024-08-04 09:36:46 +0900

    번호를 선정해서 넓이 구하는 방법

--- tree-sf/겹치지 않는 사각형 넓이.py
+++ tree-sf/겹치지 않는 사각형 넓이.py
@@ -20,3 +20,27 @@ for x in range(m_x1+1000, m_x2+1000):
         temp[x][y] = 0
 
 print(sum(sum(row) for row in temp))
+
+
+# 2번
+
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+for i, (x1, y1, x2, y2) in enumerate(arr, start=1):
+    x1, y1 = x1 + 100, y1 + 100
+    x2, y2 = x2 + 100, y2 + 100
+    
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            temp[x][y] = i
+
+area = 0
+for x in range(0, 2001):
+    for y in range(0, 2001):
+        if temp[x][y] == 1 or temp[x][y] == 2:
+            area += 1
+
+print(area)
