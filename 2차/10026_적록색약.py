a58ca6513a3c286e85ef1b38eb2d622f4b57ff7b
j9972 <j9972@naver.com>
2026-04-08 21:56:28 +0900

    도착 위치에서 정렬하는 것이 아니라 bfs로 모든 경우의수 확인

--- 2차/10026_적록색약.py
+++ 2차/10026_적록색약.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 
 n = int(input())
 
---
+++ 2차/16928_뱀과 사다리 게임.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n,m = map(int,input().split())
+
+arr = [i for i in range(101)]
+visited = [False for i in range(101)]
+
+for _ in range(n+m):
+    s,e = map(int,input().split())
+    arr[s] = e
+
+q = deque()
+q.append((1,0))
+visited[1] = True
+
+while q:
+    now, cnt = q.popleft()
+
+    if now == 100:
+        print(cnt)
+        break
+
+    for i in range(1,7):
+        nx = now + i
+
+        if nx > 100:
+            continue
+
+        nx = arr[nx]
+
+        if not visited[nx]:
+            visited[nx] = True
+            q.append((nx, cnt+1))
