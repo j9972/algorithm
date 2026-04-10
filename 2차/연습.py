ce52e8fa6d303294427b235e32070f140534abda
j9972 <j9972@naver.com>
2026-04-10 17:36:13 +0900

    위치 이동은 bfs

--- 2차/연습.py
+++ 2차/연습.py
@@ -1,72 +1,39 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
-
 from collections import deque
 
-n = int(input())
-arr = [
-    list(input().rstrip())
-    for _ in range(n)
-]
-visited = [
-    [False] * n
-    for _ in range(n)
-]
+n,m = map(int,input().split())
 
-dxs = [-1,1,0,0]
-dys = [0,0,-1,1]
+arr = [i for i in range(101)]
+visited = [False for i in range(101)]
 
-def in_range(x,y):
-    return 0<=x<n and 0<=y<n and not visited[x][y]
+for _ in range(n+m):
+    s,e = map(int,input().split())
 
-def dfs(x,y):
-    for dx,dy in zip(dxs, dys):
-        nx = x + dx
-        ny = y + dy
+    arr[s] = e
 
-        if in_range(nx,ny) and arr[x][y] == arr[nx][ny]:
-            visited[nx][ny] = True
-            dfs(nx,ny)
+q = deque()
+q.append((1,0))
+visited[1] = True
 
-cnt = 0
-for i in range(n):
-    for j in range(n):
-        if not visited[i][j]:
-            visited[i][j] = True
-            cnt += 1    
-            dfs(i,j)
+while q:
+    now, cnt = q.popleft()
+
+    if now == 100:
+        print(cnt)
+        break
+
+    for i in range(1,7):
+        nx = now + i
+
+        if nx > 100:
+            continue
+
+        nx = arr[nx]
+
+        if not visited[nx]:
+            visited[nx] = True
+            q.append((nx, cnt + 1))
 
 
-for i in range(n):
-    for j in range(n):
-        if arr[i][j] == 'R':
-            arr[i][j] = 'G'
-
-visited2 = [
-    [False] * n
-    for _ in range(n)
-]
-
-def in_range2(x,y):
-    return 0<=x<n and 0<=y<n and not visited2[x][y]
-
-cnt2 = 0
-def dfs2(x,y):
-    for dx,dy in zip(dxs, dys):
-        nx = x + dx
-        ny = y + dy
-
-        if in_range2(nx,ny) and arr[x][y] == arr[nx][ny]:
-            visited2[nx][ny] = True
-            dfs2(nx,ny)
-
-cnt2 = 0
-for i in range(n):
-    for j in range(n):
-        if not visited2[i][j]:
-            visited2[i][j] = True
-            cnt2 += 1    
-            dfs2(i,j)
-
-print(cnt,cnt2)
