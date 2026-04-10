3ed9a7f6877d0b99b11313727acf070c41c044f6
j9972 <j9972@naver.com>
2026-04-10 18:21:29 +0900

    플로이드 워셜

--- 2차/연습.py
+++ 2차/연습.py
@@ -3,23 +3,19 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 from collections import deque
 
-n,k = map(int,input().split())
+n = int(input())
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
 
-dp = [0] * 100001
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] or (arr[i][k] and arr[k][j]):
+                arr[i][j] = 1
 
-def bfs(start):
-    q = deque()
-    q.append(start)
-
-    while q:
-        cur = q.popleft()
-
-        if cur == k:
-            return dp[k]
-
-        for i in (cur+1, cur-1, cur*2):
-            if 0<=i<=100000 and dp[i] == 0:
-                dp[i] = dp[cur] + 1
-                q.append(i)
-print(bfs(n))
-
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j],end=' ')
+    print()
